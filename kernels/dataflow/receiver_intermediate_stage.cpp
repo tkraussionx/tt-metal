1162ae93c7256d93e2cce80e68b04fb42ccd6c5c
Davor Capalija <dcapalija@tenstorrent.com>
2023-02-25 09:23:40 -0800

    #31: initial set of measurements

--- kernels/dataflow/receiver_intermediate_stage.cpp
+++ kernels/dataflow/receiver_intermediate_stage.cpp
@@ -11,6 +11,7 @@ void kernel_main() {
     uint32_t num_tiles               = get_arg_val<uint32_t>(2);
     uint32_t sender_semaphore_addr   = get_arg_val<uint32_t>(3);
     uint32_t receiver_semaphore_addr = get_arg_val<uint32_t>(4);
+    uint32_t num_repetitions         = get_arg_val<uint32_t>(5);
 
     volatile uint32_t* receiver_semaphore_addr_ptr = reinterpret_cast<volatile uint32_t*>(receiver_semaphore_addr);
 
@@ -19,20 +20,22 @@ void kernel_main() {
 
     uint32_t block_size_bytes = get_tile_size(cb_id) * block_size_tiles;
 
-    for (uint32_t i = 0; i<num_tiles ; i += block_size_tiles) {
-        cb_reserve_back(cb_id, block_size_tiles);
+    for (uint32_t j = 0; j < num_repetitions; j++) {
+        for (uint32_t i = 0; i<num_tiles ; i += block_size_tiles) {
+            cb_reserve_back(cb_id, block_size_tiles);
 
-        // Reset receiver's own semaphore value to INVALID
-        noc_semaphore_set(receiver_semaphore_addr_ptr, INVALID);
+            // Reset receiver's own semaphore value to INVALID
+            noc_semaphore_set(receiver_semaphore_addr_ptr, INVALID);
 
-        // Tell sender we're ready -- atomic increment sender's semaphore
-        uint64_t sender_semaphore_noc_addr = get_noc_addr(sender_noc_x, sender_noc_y, sender_semaphore_addr);
-        noc_semaphore_inc(sender_semaphore_noc_addr, 1);
+            // Tell sender we're ready -- atomic increment sender's semaphore
+            uint64_t sender_semaphore_noc_addr = get_noc_addr(sender_noc_x, sender_noc_y, sender_semaphore_addr);
+            noc_semaphore_inc(sender_semaphore_noc_addr, 1);
 
-        // Wait on receiver's own semaphore value to become VALID (set by sender after it sends the data)
-        noc_semaphore_wait(receiver_semaphore_addr_ptr, VALID);
+            // Wait on receiver's own semaphore value to become VALID (set by sender after it sends the data)
+            noc_semaphore_wait(receiver_semaphore_addr_ptr, VALID);
 
-        cb_push_back(cb_id, block_size_tiles);
+            cb_push_back(cb_id, block_size_tiles);
+        }
     }
 
     kernel_profiler::mark_time(6);
--- kernels/dataflow/sender_intermediate_stage.cpp
+++ kernels/dataflow/sender_intermediate_stage.cpp
@@ -10,6 +10,7 @@ void kernel_main() {
     uint32_t num_tiles               = get_arg_val<uint32_t>(2);
     uint32_t sender_semaphore_addr   = get_arg_val<uint32_t>(3);
     uint32_t receiver_semaphore_addr = get_arg_val<uint32_t>(4);
+    uint32_t num_repetitions         = get_arg_val<uint32_t>(5);
 
     // initialized by the host to 0 before program launch
     volatile uint32_t* sender_semaphore_addr_ptr = reinterpret_cast<volatile uint32_t*>(sender_semaphore_addr);
@@ -19,27 +20,29 @@ void kernel_main() {
 
     uint32_t block_size_bytes = get_tile_size(cb_id) * block_size_tiles;
 
-    for (uint32_t i = 0; i < num_tiles; i += block_size_tiles) {
-        cb_wait_front(cb_id, block_size_tiles);
-        uint32_t l1_addr = get_read_ptr(cb_id);
+    for (uint32_t j = 0; j < num_repetitions; j++) {
+        for (uint32_t i = 0; i < num_tiles; i += block_size_tiles) {
+            cb_wait_front(cb_id, block_size_tiles);
+            uint32_t l1_addr = get_read_ptr(cb_id);
 
-        // wait until receiver has set the sender's semaphore_addr value to 1, which means receiver has reserved space in the CB
-        noc_semaphore_wait(sender_semaphore_addr_ptr, 1);
-        // set the semaphore value back to zero for the next block
-        noc_semaphore_set(sender_semaphore_addr_ptr, 0);
+            // wait until receiver has set the sender's semaphore_addr value to 1, which means receiver has reserved space in the CB
+            noc_semaphore_wait(sender_semaphore_addr_ptr, 1);
+            // set the semaphore value back to zero for the next block
+            noc_semaphore_set(sender_semaphore_addr_ptr, 0);
 
-        // Now we have the block in the CB (at l1_addr), we can send to receiver
-        uint64_t receiver_data_noc_addr = get_noc_addr(receiver_noc_x, receiver_noc_y, l1_addr);
-        noc_async_write(l1_addr, receiver_data_noc_addr, block_size_bytes);
+            // Now we have the block in the CB (at l1_addr), we can send to receiver
+            uint64_t receiver_data_noc_addr = get_noc_addr(receiver_noc_x, receiver_noc_y, l1_addr);
+            noc_async_write(l1_addr, receiver_data_noc_addr, block_size_bytes);
 
-        // We also set the receiver's semaphore, so that it knows that the data has been written to the CB
-        uint64_t receiver_semaphore_noc_addr = get_noc_addr(receiver_noc_x, receiver_noc_y, receiver_semaphore_addr);
-        noc_semaphore_inc(receiver_semaphore_noc_addr, 1);
+            // We also set the receiver's semaphore, so that it knows that the data has been written to the CB
+            uint64_t receiver_semaphore_noc_addr = get_noc_addr(receiver_noc_x, receiver_noc_y, receiver_semaphore_addr);
+            noc_semaphore_inc(receiver_semaphore_noc_addr, 1);
 
-        // do we need the barrier? to make sure we've sent the data before we pop the CB? or does sempahore inter-lock already guarantee that?
-        noc_async_write_barrier();
+            // do we need the barrier? to make sure we've sent the data before we pop the CB? or does sempahore inter-lock already guarantee that?
+            noc_async_write_barrier();
 
-        cb_pop_front(cb_id, block_size_tiles);
+            cb_pop_front(cb_id, block_size_tiles);
+        }
     }
 
     kernel_profiler::mark_time(4);
