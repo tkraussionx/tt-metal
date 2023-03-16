8e5939f8133d2b1202c534a603e444bea7f1ea96
srajab-tt <109368398+srajab-tt@users.noreply.github.com>
2023-03-16 16:20:03 -0400

    Working and tested version, PCC = 0.9999

--- python_api_testing/fused_ops/batchnorm.py
+++ python_api_testing/fused_ops/batchnorm.py
@@ -1,3 +1,4 @@
+# Only tested for Batchsize = 1, Channels = 1
 
 import math
 from pathlib import Path
@@ -6,13 +7,16 @@ f = f"{Path(__file__).parent}"
 sys.path.append(f"{f}/..")
 
 import torch
+from torch import nn
 
 from gpai import gpai
-from python_api_testing.models.utility_functions import pad_activation, pad_weight, tilize, untilize, tilize_to_list, print_diff_argmax, pad_weight
+from python_api_testing.models.utility_functions import pad_activation, pad_weight, tilize, untilize, tilize_to_list, pad_weight
 torch.set_printoptions(threshold=10_000)
 
+from python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+
 #v1_ assume input is always 32x32
-def Batchnorm(mean_run, var_run, gamma, beta, C, device): #
+def Batchnorm(mean_run, var_run, gamma, beta, C, device): 
     # gamma, beta, epsilon should be vectors of size C
     mean_run = gpai.tensor.Tensor(
         mean_run, 
@@ -46,40 +50,25 @@ def Batchnorm(mean_run, var_run, gamma, beta, C, device): #
         device
     )
 
-    BCHW = gpai.tensor.BcastOpDim.HW
-    BCMUL = gpai.tensor.BcastOpMath.MUL
-    BCSUB = gpai.tensor.BcastOpMath.SUB
-    BCADD = gpai.tensor.BcastOpMath.ADD
-
     def batchnorm_(x):
         # first subtract running mean
-        x_minus_mean = gpai.tensor.bcast(x, mean_run, BCSUB, BCHW)
-        print ('xxxxxxxxxx')
-        #x
-        print (x_minus_mean)
-
-        hostx = gpai.device.GetHost()
-        y=x_minus_mean.to(hostx).data()
-        z=torch.Tensor(y).reshape((1,C,H,W))
-        print(z[0,0,:,:])
-
-        print ('xxxxxxxxxx')
-        breakpoint()
-
+        x_minus_mean = gpai.tensor.sub(x, mean_run)
         # take sqrt of running_var+eps
         var_sqrt = gpai.tensor.sqrt(var_run)
         # reciprocal
-        inv_sqrt = gpai.tensor.recip(var_sqrt)
-        x_div_sqrt = gpai.tensor.bcast(x_minus_mean, inv_sqrt, BCMUL, BCHW)
-        x_gamma = gpai.tensor.bcast(x_div_sqrt, gamma, BCMUL, BCHW)
-        x_result = gpai.tensor.bcast(x_gamma, beta, BCADD, BCHW)
+        inv_sqrt = gpai.tensor.recip(var_sqrt)       
+        #mulitply by reciprocal
+        x_div_sqrt = gpai.tensor.mul(x_minus_mean, inv_sqrt)
+        #multiply by gamma
+        x_gamma = gpai.tensor.mul(x_div_sqrt, gamma)
+        # add beta
+        x_result = gpai.tensor.add(x_gamma, beta)
 
         return x_result
 
     return batchnorm_
 
-
-def ref_batchnorm(x, eps, gamma, beta, mean_run, var_run):
+def ref_batchnorm_torch(x, eps, gamma, beta, mean_run, var_run):
     bnorm = torch.nn.BatchNorm2d(x.shape[1], eps=eps, momentum=1)
     bnorm.weight       = torch.nn.Parameter(torch.tensor([gamma] * x.shape[1]))
     bnorm.bias         = torch.nn.Parameter(torch.tensor([beta] * x.shape[1]))
@@ -88,6 +77,28 @@ def ref_batchnorm(x, eps, gamma, beta, mean_run, var_run):
     
     with torch.no_grad():
         result = bnorm(x)
+    return result
+
+def ref_batchnorm(x, eps, gamma, beta, mean_run, var_run):
+    # N = 1, C = 1, batchnorm inference
+    result = gamma*(x - mean_run)/math.sqrt(var_run + eps) + beta
+
+    return result
+
+def ref_batchnorm_torch(x, eps, gamma, beta, mean_run, var_run):
+
+    bnorm = torch.nn.BatchNorm2d(x.shape[1], eps=eps, momentum=0)
+
+    bnorm.weight       = torch.nn.Parameter(torch.tensor([gamma] * x.shape[1]))
+    bnorm.bias         = torch.nn.Parameter(torch.tensor([beta] * x.shape[1]))
+    bnorm.running_mean = torch.nn.Parameter(torch.tensor([mean_run] * x.shape[1]))
+    bnorm.running_var  = torch.nn.Parameter(torch.tensor([var_run] * x.shape[1]))
+
+    model = nn.Sequential(bnorm)
+    model.eval()
+    
+    with torch.no_grad():
+        result = model(x)
     return result
 
 if __name__ == "__main__":
@@ -102,29 +113,23 @@ if __name__ == "__main__":
     epsf = 1e-4
     betaf = 0.345
     gammaf = 0.123
-    mean_runf = 0. # 0.789
+    mean_runf = 0.789
     var_runf = 0.567 
     torch.manual_seed(123)
     x = torch.randn((1,C,H,W))
 
-    print ('THIS IS XXXXX')
-    print (x)
-
-    #breakpoint()
-
     ref_bnorm = ref_batchnorm(x, epsf, gammaf, betaf, mean_runf, var_runf)
 
-    gamma = pad_weight(torch.full((1,C,H,W), gammaf))
-    beta = pad_weight(torch.full((1,C,H,W), betaf))
-    mean_run = pad_weight(torch.full((1,C,H,W), mean_runf))
-    var_run = pad_weight(torch.full((1,C,H,W), var_runf + epsf))
+    gamma = pad_weight(torch.full((1,C,32,32), gammaf))
+    beta = pad_weight(torch.full((1,C,32,32), betaf))
+    mean_run = pad_weight(torch.full((1,C,32,32), mean_runf))
+    var_run = pad_weight(torch.full((1,C,32,32), var_runf + epsf))
     
     t0 = gpai.tensor.Tensor(tilize_to_list(x), [1, C, H, W], gpai.tensor.DataType.BFLOAT16, gpai.tensor.Layout.TILE, device)
     ttgamma = tilize_to_list(gamma)
     ttbeta = tilize_to_list(beta)
     ttmean_run = tilize_to_list(mean_run)
     ttvar_run = tilize_to_list(var_run)
-
 
     func = Batchnorm(ttmean_run, ttvar_run, ttgamma, ttbeta, C, device)
 
@@ -135,12 +140,7 @@ if __name__ == "__main__":
     tt_got_back = untilize(tt_got_back)
 
     print ('======================')
-    print (tt_got_back)
-    print ( ref_bnorm[0,0,:,:])
-    #print (list(torch.Tensor(ref_bnorm).reshape((H*W))))
-    print ('-------++++_------------')
-    
-    print("Batchnorm max absdiff=")
-    print_diff_argmax(tt_got_back, ref_bnorm)
+    print ("GOLDEN PCC TEST")
+    print (comp_pcc(ref_bnorm, tt_got_back, pcc=.99))
 
     gpai.device.CloseDevice(device)
