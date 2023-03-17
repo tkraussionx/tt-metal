dc7501837b2f51a545257eda24709edecb5e7730
Mo Memarian <mmemarian@tenstorrent.com>
2023-03-17 21:35:07 +0000

    #88: Prototyping gantt charts

--- tt_metal/tools/profiler/process_device_log.py
+++ tt_metal/tools/profiler/process_device_log.py
@@ -6,6 +6,9 @@ import csv
 
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output
+from rich import print
+import plotly.express as px
+import pandas as pd
 
 import plot_setup
 
@@ -185,6 +188,39 @@ def print_help():
     print("e.g. : process_device_log.py")
 
 
+def import_device_profile_log (logPath):
+    deviceData = {}
+    with open(logPath) as csvFile:
+        csvReader = csv.reader(csvFile, delimiter=",")
+        for lineCount,row in enumerate(csvReader):
+            if lineCount > 1:
+                chipID = int(row[0])
+                core = (int(row[1]), int(row[2]))
+                risc = row[3].strip()
+                timerID = int(row[4])
+                timeData = int(row[5])
+
+                if chipID in deviceData.keys():
+                    if core in deviceData[chipID].keys():
+                        if risc in deviceData[chipID][core].keys():
+                            deviceData[chipID][core][risc]["timeSeries"].append(
+                                (timerID, timeData)
+                            )
+                        else:
+                            deviceData[chipID][core][risc] = {
+                                "timeSeries": [(timerID, timeData)]
+                            }
+                    else:
+                        deviceData[chipID][core] = {
+                            risc: {"timeSeries": [(timerID, timeData)]}
+                        }
+                else:
+                    deviceData[chipID] = {
+                        core: {risc: {"timeSeries": [(timerID, timeData)]}}
+                    }
+    return deviceData
+
+
 def main(args):
     if len(args) == 1:
         try:
@@ -207,217 +243,45 @@ def main(args):
         print_help()
         return
 
-    timerVals = {}
+    deviceData = import_device_profile_log(DEVICE_TIME_CSV)
+
+    ganttData = []
+    for device in deviceData.keys():
+        for core in deviceData[device].keys():
+            for risc in deviceData[device][core].keys():
+                timeSeries = deviceData[device][core][risc]['timeSeries']
+                timeSeries.sort(key=lambda x: x[1])
+                for startTime, endTime in zip(timeSeries[:-1],timeSeries[1:]):
+                    ganttData.append(
+                        dict(core=f"{core[0]}-{core[1]}-{risc}", Start=startTime[1], Finish=endTime[1], Resource=f"{startTime[0]}-{endTime[0]}"))
+
+    df = pd.DataFrame(ganttData)
+    df['delta'] = df['Finish'] - df['Start']
+    minCycle = min(df.Start)
+    df['Start'] = df['Start'] - minCycle
+    df['Finish'] = df['Finish'] - minCycle
+
+    print(df)
+    fig = px.timeline(df, x_start="Start", x_end="Finish", y="core", color="Resource")
+    fig.update_yaxes(autorange="reversed")
+    fig.layout.xaxis.type = 'linear'
+    for i in range(len(fig.data)):
+        fig.data[i].x = (df.delta[i],)
+
     external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
     app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-    freq_text = ""
-    with open(DEVICE_TIME_CSV, "r") as csvfile:
-        freq_text = csvfile.readline().split('at ')[-1].split(' ')[0].strip()
-        fields = csvfile.readline()
-        for line in csvfile.readlines():
-            vals = line.split(",")
-            if len(vals) == 6:
-                pcie_slot = vals[0].strip()
-                col = vals[1].strip()
-                row = vals[2].strip()
-                risc = vals[3].strip()
-                timerID = vals[4].strip()
-                cycleCount = int(vals[5].strip())
+    app.layout = html.Div(
+        [
+            html.H1("Device Performance"),
+            html.Br(),
+            html.H3("Stats Table"),
+            # generate_analysis_table(timerStats),
+            dcc.Graph(figure=fig),
+        ]
+    )
 
-                core = f"{col},{row}"
-                if core in timerVals.keys():
-                    if risc in timerVals[core].keys():
-                        timerVals[core][risc][timerID] = cycleCount
-                    else:
-                        timerVals[core][risc] = {timerID: cycleCount}
-                else:
-                    timerVals[core] = {risc: {timerID: cycleCount}}
-
-    print_arranged_csv(timerVals, setup.timerIDLabels, pcie_slot, freq_text)
-
-    if timerVals:
-        maxTime = 0
-        minTime = (1 << 64) - 1
-        for core in timerVals.keys():
-            for risc in timerVals[core].keys():
-                for timerID, cycleCount in timerVals[core][risc].items():
-                    if cycleCount > maxTime:
-                        maxTime = cycleCount
-                    if cycleCount < minTime:
-                        minTime = cycleCount
-        timerStats = {"RunTime" : {"Total" : maxTime - minTime}}
-        timerStatsCores = {}
-        setupMaxStrLen = 0
-        for timerAnalysisSetup in setup.timerAnalysis.keys():
-            if len(timerAnalysisSetup) > setupMaxStrLen:
-                setupMaxStrLen = len(timerAnalysisSetup)
-
-        for timerAnalysisSetup in setup.timerAnalysis.keys():
-            analysisTimeMax = 0
-            analysisTimeMin = (1 << 64) - 1
-            analysisTimeSum = 0
-
-            countAnalysisTime = 0
-            for core in timerVals.keys():
-                analysisTime = -1
-                setupType = setup.timerAnalysis[timerAnalysisSetup]["type"]
-                if setupType == "single":
-                    risc = setup.timerAnalysis[timerAnalysisSetup]["risc"]
-                    timerID = setup.timerAnalysis[timerAnalysisSetup]["timerID"]
-                    if timerID in timerVals[core][risc].keys():
-                        analysisTime = timerVals[core][risc][timerID] - minTime
-                elif setupType == "diff":
-                    startRisc = setup.timerAnalysis[timerAnalysisSetup]["start"]["risc"]
-                    startTimerID = setup.timerAnalysis[timerAnalysisSetup]["start"][
-                        "timerID"
-                    ]
-                    endRisc = setup.timerAnalysis[timerAnalysisSetup]["end"]["risc"]
-                    endTimerID = setup.timerAnalysis[timerAnalysisSetup]["end"][
-                        "timerID"
-                    ]
-                    if (
-                        startRisc in timerVals[core].keys()
-                        and endRisc in timerVals[core].keys()
-                        and startTimerID in timerVals[core][startRisc].keys()
-                        and endTimerID in timerVals[core][endRisc].keys()
-                    ):
-                        analysisTime = (
-                            timerVals[core][endRisc][endTimerID]
-                            - timerVals[core][startRisc][startTimerID]
-                        )
-
-                if analysisTime != -1:
-                    if core in timerStatsCores.keys():
-                        timerStatsCores[core][timerAnalysisSetup] = analysisTime
-                    else:
-                        timerStatsCores[core] = {timerAnalysisSetup: analysisTime}
-                    countAnalysisTime += 1
-                    analysisTimeSum += analysisTime
-                    if analysisTime > analysisTimeMax:
-                        analysisTimeMax = analysisTime
-                    if analysisTime < analysisTimeMin:
-                        analysisTimeMin = analysisTime
-
-            analysisTimeAverage = 0
-            if countAnalysisTime:
-                analysisTimeAverage = analysisTimeSum / countAnalysisTime
-
-            timerStats[timerAnalysisSetup] = {
-                "Average": analysisTimeAverage,
-                "Min": analysisTimeMin,
-                "Max": analysisTimeMax,
-            }
-
-        print_stats(timerStats, timerStatsCores)
-        print_stats_outfile(timerStats, timerStatsCores)
-        analyze_stats(timerStats, timerStatsCores)
-
-        yVals = sorted(timerVals.keys(), key=coreCompare, reverse=True)
-        xVals = {}
-
-        for yVal in yVals:
-            for risc in setup.riscTimerCombo.keys():
-                if risc in timerVals[yVal].keys():
-                    for combo in setup.riscTimerCombo[risc]:
-                        xVal = 0
-                        if (
-                            combo[0] == "START"
-                            and combo[1] in timerVals[yVal][risc].keys()
-                        ):
-                            xVal = timerVals[yVal][risc][combo[1]] - (minTime - 5)
-                        elif (
-                            combo[1] == "END"
-                            and combo[0] in timerVals[yVal][risc].keys()
-                        ):
-                            xVal = maxTime - timerVals[yVal][risc][combo[0]]
-                        elif (
-                            combo[0] in timerVals[yVal][risc].keys()
-                            and combo[1] in timerVals[yVal][risc].keys()
-                        ):
-                            xVal = (
-                                timerVals[yVal][risc][combo[1]]
-                                - timerVals[yVal][risc][combo[0]]
-                            )
-
-                            # TODO: Add drop down for time / cycles display
-                            # xVal = xVal / CYCLE_COUNT_TO_MILISECS
-
-                        if risc in xVals.keys():
-                            if combo in xVals[risc].keys():
-                                xVals[risc][combo].append(xVal)
-                            else:
-                                xVals[risc][combo] = [xVal]
-                        else:
-                            xVals[risc] = {combo: [xVal]}
-
-        # TODO: Add drop down for time / cycles display
-        # layout = go.Layout(xaxis=dict(title="Time [ms]"), yaxis=dict(title="Cores"))
-
-        layout = go.Layout(xaxis=dict(title="Cycle count"), yaxis=dict(title="Cores"))
-        fig = go.Figure(layout=layout)
-
-        fig.add_trace(
-            go.Bar(
-                y=[yVals, [" "] * len(yVals)],
-                x=[0] * len(yVals),
-                orientation="h",
-                showlegend=False,
-                marker=dict(color="rgba(255, 255, 255, 0.0)"),
-            )
-        )
-        for risc in setup.riscTimerCombo.keys():
-            if risc in xVals.keys():
-                for combo in setup.riscTimerCombo[risc]:
-                    color = setup.colors[combo[2]]
-
-                    showlegend = True
-                    if combo[2] == "blank":
-                        showlegend = False
-
-                    fig.add_trace(
-                        go.Bar(
-                            y=[yVals, [risc] * len(yVals)],
-                            x=xVals[risc][combo],
-                            orientation="h",
-                            name=combo[3],
-                            showlegend=showlegend,
-                            marker=dict(color=color),
-                        )
-                    )
-        fig.add_trace(
-            go.Bar(
-                y=[yVals, [""] * len(yVals)],
-                x=[0] * len(yVals),
-                orientation="h",
-                showlegend=False,
-                marker=dict(color="rgba(255, 255, 255, 0.0)"),
-            )
-        )
-
-        fig.update_layout(
-            barmode="stack", height=BASE_HEIGHT + PER_CORE_HEIGHT * len(yVals)
-        )
-
-        fig.write_html(DEVICE_PERF_HTML)
-
-        os.system(
-            f"tar -czf {DEVICE_PERF_RESULTS} {DEVICE_TIME_CSV} {DEVICE_STATS_TXT} {DEVICE_PERF_HTML} {REARRANGED_TIME_CSV}"
-        )
-
-        app.layout = html.Div(
-            [
-                html.H1("Device Performance"),
-                html.Br(),
-                html.H3("Stats Table"),
-                generate_analysis_table(timerStats),
-                dcc.Graph(figure=fig),
-            ]
-        )
-
-        app.run_server(host="0.0.0.0", debug=True)
-    else:
-        print(f"Empty {DEVICE_TIME_CSV}")
+    app.run_server(host="0.0.0.0", debug=True)
 
 
 if __name__ == "__main__":
