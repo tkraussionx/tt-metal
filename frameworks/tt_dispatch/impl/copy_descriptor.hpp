b543e956c89184e0cdc7c01553f4f8d9fe4ae4e2
agrebenisan <agrebenisan@tenstorrent.com>
2023-05-02 15:08:45 +0000

    #0: WIP commit that specs out the new dispatch copy descriptor table

---
+++ frameworks/tt_dispatch/impl/copy_descriptor.hpp
@@ -0,0 +1,44 @@
+#include <cstdint>
+#include <array>
+
+template <size_t T>
+struct CopyDescriptor {
+
+    static_assert((T % 2) == 0, "Size of copy descriptor must be divisible by 2");
+
+    // These two attributes are only used by host to know where
+    // to write this copy descriptor
+    uint32_t l1_addr;
+    const uint32_t read_base = 0;
+    const uint32_t write_base = T / 2;
+    uint32_t read_ptr = read_base + 1;
+    uint32_t write_ptr = write_base + 1;
+
+    std::array<uint32_t, T> data;
+
+    void add_read(uint64_t src, uint32_t dst, uint32_t size) {
+        this->data[read_base]++; // Increment num_reads
+
+        uint32_t upper = src >> 32;
+        uint32_t lower = src & 0xffffffff;
+        this->data[this->read_ptr++] = lower;
+        this->data[this->read_ptr++] = upper;
+        this->data[this->read_ptr++] = dst;
+        this->data[this->read_ptr++] = size;
+    }
+
+    void add_write(uint32_t src, uint64_t dst, uint32_t) {
+        this->data[write_base]++; // Increment num_writes
+
+        this->data[this->write_ptr++] = src;
+        uint32_t upper = dst >> 32;
+        uint32_t lower = dst & 0xffffffff;
+        this->data[this->write_ptr++] = lower;
+        this->data[this->write_ptr++] = upper;
+        this->data[this->write_ptr++] = size;
+    }
+
+    void clear_table() {
+        memset(this->data, 0, sizeof(this->data));
+    }
+};
