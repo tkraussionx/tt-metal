6c85b3902da8fac8833f491f3870b58736e7f4db
Austin Ho <aho@tenstorrent.com>
2023-05-08 22:25:09 +0000

    #545: Refactor conversion wrapper code

--- libs/tt_lib/fallback_ops/conversion_wrapper.py
+++ libs/tt_lib/fallback_ops/conversion_wrapper.py
@@ -1,6 +1,102 @@
 from .. import tensor as ttl_tensor, device as ttl_device
+from collections.abc import Sequence, Mapping
 import torch
 from functools import wraps
+
+
+def convert_tt_tensor_to_pt_tensor(tt_tensor, host, output_format):
+    # Update output_format with format of first encountered arg
+    if not output_format:
+        if tt_tensor.on_host():
+            output_format["device"] = host
+        else:
+            output_format["device"] = tt_tensor.device()
+        output_format["layout"] = tt_tensor.layout()
+        output_format["dtype"] = tt_tensor.dtype()
+
+    # Convert to PT Tensor
+    return torch.Tensor(
+        tt_tensor.to(host).to(ttl_tensor.Layout.ROW_MAJOR).data()
+    ).reshape(tt_tensor.shape())
+
+
+def convert_pt_tensor_to_tt_tensor(pt_tensor, output_format):
+    tt_tensor = ttl_tensor.Tensor(
+        pt_tensor.reshape(-1).tolist(),
+        pt_tensor.shape,
+        output_format["dtype"],
+        ttl_tensor.Layout.ROW_MAJOR,
+    )
+    if output_format["layout"] == ttl_tensor.Layout.TILE:
+        if (
+            tt_tensor.shape()[2] % 32 == 0 and tt_tensor.shape()[3] % 32 == 0
+        ):  # Restore tile layout only if legal or else leave as RM
+            tt_tensor = tt_tensor.to(ttl_tensor.Layout.TILE)
+    else:
+        tt_tensor = tt_tensor.to(output_format["layout"])
+    tt_tensor = tt_tensor.to(output_format["device"])
+    return tt_tensor
+
+
+def convert_tt_tensors_to_pt_tensors(args, host, output_format):
+    if args is None:
+        return None
+    elif isinstance(args, ttl_tensor.Tensor):
+        return convert_tt_tensor_to_pt_tensor(args, host, output_format)
+    elif isinstance(args, Mapping):
+        outputs = {}
+        for key, value in args.items():
+            if isinstance(value, ttl_tensor.Tensor):
+                outputs[key] = convert_tt_tensor_to_pt_tensor(
+                    value, host, output_format
+                )
+            elif isinstance(value, Sequence):
+                outputs[key] = convert_tt_tensors_to_pt_tensors(
+                    value, host, output_format
+                )
+            else:
+                outputs[key] = value
+        return outputs
+    elif isinstance(args, Sequence):
+        outputs = []
+        for arg in args:
+            if isinstance(arg, ttl_tensor.Tensor):
+                outputs.append(convert_tt_tensor_to_pt_tensor(arg, host, output_format))
+            elif isinstance(arg, (Sequence)):
+                outputs.append(
+                    convert_tt_tensors_to_pt_tensors(arg, host, output_format)
+                )
+            else:
+                outputs.append(arg)
+        return outputs
+
+
+def convert_pt_tensors_to_tt_tensors(args, output_format):
+    if args is None:
+        return None
+    elif isinstance(args, torch.Tensor):
+        return convert_pt_tensor_to_tt_tensor(args, output_format)
+    elif isinstance(args, Mapping):
+        outputs = []
+        for key, value in args.items():
+            if isinstance(value, torch.Tensor):
+                outputs[key] = convert_pt_tensor_to_tt_tensor(value, output_format)
+            elif isinstance(value, Sequence):
+                outputs[key] = convert_pt_tensors_to_tt_tensors(value, output_format)
+            else:
+                outputs[key] = value
+        return outputs
+    elif isinstance(args, Sequence):
+        outputs = []
+        for arg in args:
+            if isinstance(arg, torch.Tensor):
+                outputs.append(convert_pt_tensor_to_tt_tensor(arg, output_format))
+            elif isinstance(arg, Sequence):
+                outputs.append(convert_pt_tensors_to_tt_tensors(arg, output_format))
+            else:
+                outputs.append(arg)
+        return outputs
+
 
 def convert_tt_tensors_wrapper(func):
     host = ttl_device.GetHost()
@@ -9,105 +105,21 @@ def convert_tt_tensors_wrapper(func):
     def wrap(*args, **kwargs):
         output_format = {}
 
-        new_args = []
-        for i, arg in enumerate(args):
-            if isinstance(arg, ttl_tensor.Tensor):
-                if not output_format:
-                    if arg.on_host():
-                        output_format["device"] = host
-                    else:
-                        output_format["device"] = arg.device()
-                    output_format["layout"] = arg.layout()
-                    output_format["dtype"] = arg.dtype()
-                # Convert to PT Tensor
-                new_args.append(
-                    torch.Tensor(
-                        arg.to(host).to(ttl_tensor.Layout.ROW_MAJOR).data()
-                    ).reshape(arg.shape())
-                )
-            elif isinstance(arg, list) or isinstance(arg, tuple):
-                new_a = []
-                for j, a in enumerate(arg):
-                    if isinstance(a, ttl_tensor.Tensor):
-                        if not output_format:
-                            if a.on_host():
-                                output_format["device"] = host
-                            else:
-                                output_format["device"] = a.device()
-                            output_format["layout"] = a.layout()
-                            output_format["dtype"] = a.dtype()
-                        # Convert to PT Tensor
-                        new_a.append(
-                            torch.Tensor(
-                                a.to(host).to(ttl_tensor.Layout.ROW_MAJOR).data()
-                            ).reshape(a.shape())
-                        )
-                    else:
-                        new_a.append(a)
-                new_args.append(new_a)
-            else:
-                new_args.append(arg)
+        new_args = convert_tt_tensors_to_pt_tensors(args, host, output_format)
 
-        for key, value in kwargs.items():
-            if isinstance(value, ttl_tensor.Tensor):
-                if not output_format:
-                    if value.on_host():
-                        output_format["device"] = host
-                    else:
-                        output_format["device"] = value.device()
-                    output_format["layout"] = value.layout()
-                    output_format["dtype"] = value.dtype()
-                # Convert to PT Tensor
-                kwargs[key] = torch.Tensor(
-                    value.to(host).to(ttl_tensor.Layout.ROW_MAJOR).data()
-                ).reshape(value.shape())
+        new_kwargs = convert_tt_tensors_to_pt_tensors(kwargs, host, output_format)
 
         # Set default output format
         if not output_format:
-            output_format = {"device": host, "layout": ttl_tensor.Layout.ROW_MAJOR, "dtype": ttl_tensor.DataType.BFLOAT16}
+            output_format = {
+                "device": host,
+                "layout": ttl_tensor.Layout.ROW_MAJOR,
+                "dtype": ttl_tensor.DataType.BFLOAT16,
+            }
 
-        outputs = func(*new_args, **kwargs)
+        outputs = func(*new_args, **new_kwargs)
 
-        # CONVERT TO TT TENSOR
-        if outputs is None:
-            return outputs
-        elif isinstance(outputs, torch.Tensor):
-            output = ttl_tensor.Tensor(
-                outputs.reshape(-1).tolist(),
-                outputs.shape,
-                output_format["dtype"],
-                ttl_tensor.Layout.ROW_MAJOR,
-            )
-            if output_format["layout"] == ttl_tensor.Layout.TILE:
-                if (
-                    output.shape()[2] % 32 == 0 and output.shape()[3] % 32 == 0
-                ):  # Restore tile layout only if legal or else leave as RM
-                    output = output.to(ttl_tensor.Layout.TILE)
-            else:
-                output = output.to(output_format["layout"])
-            output = output.to(output_format["device"])
-            return output
-
-        new_outputs = []
-        if isinstance(outputs, list) or isinstance(outputs, tuple):
-            for i, output in enumerate(outputs):
-                # Convert to TT Tensor
-                if isinstance(output, torch.Tensor):
-                    output = ttl_tensor.Tensor(
-                        output.reshape(-1).tolist(),
-                        output.shape,
-                        output_format["dtype"],
-                        ttl_tensor.Layout.ROW_MAJOR,
-                    )
-                    if output_format["layout"] == ttl_tensor.Layout.TILE:
-                        if (
-                            output.shape()[2] % 32 == 0 and output.shape()[3] % 32 == 0
-                        ):  # Restore tile layout only if legal or else leave as RM
-                            output = output.to(ttl_tensor.Layout.TILE)
-                    else:
-                        output = output.to(output_format["layout"])
-                    output = output.to(output_format["device"])
-                    new_outputs.append(output)
-        return new_outputs
+        # Convert pt tensors in outputs to tt tensors
+        return convert_pt_tensors_to_tt_tensors(outputs, output_format)
 
     return wrap
