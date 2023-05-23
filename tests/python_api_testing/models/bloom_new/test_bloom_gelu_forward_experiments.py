de8070cfb75b6a3a29232771d828461200f74ec9
Nenad Petrovic <npetrovic@tenstorrent.com>
2023-05-23 18:04:57 +0000

    #752: Gelu tests fixed

--- tests/python_api_testing/models/bloom_new/test_bloom_gelu_forward_experiments.py
+++ tests/python_api_testing/models/bloom_new/test_bloom_gelu_forward_experiments.py
@@ -21,6 +21,11 @@ def gen_input(a,b,c,d,range1,range2):
     test_in = (range1-range2)*torch.rand(a, b, c, d) + range2
     return test_in
 
+def gen_input_2(a,b,c,d):
+    test_in = torch.rand(a, b, c, d)
+    return test_in
+
+
 def run_bloom_gelu_forward_test(device):
     # Prepare input
     torch.manual_seed(0)
@@ -31,19 +36,21 @@ def run_bloom_gelu_forward_test(device):
         dim2 = random.randint(1,65536)
 
         print('DIM:------------------------')
-        print(dim1)
-        print(dim2)
+        #print(dim1)
+        #print(dim2)
 
         # test 1
-        test_in = gen_input(1,1,dim1,dim2,-1000,100)
+        test_in = gen_input(1,1,1024,1024, -1000, 100)
+        print(test_in)
 
-        pt_out = bloom_gelu_forward.bloom_gelu_forward(test_in)
+        pt_out = torch.nn.functional.gelu(test_in)
+
         tt_test_in = bloom_utils.torch2tt_tensor(test_in, device)
 
+
+        print("TTLIB!-------")
+
         tt_out = tt_lib.tensor.gelu(tt_test_in)
-
-        tt_out_2 = bloom_gelu_forward.tt_bloom_gelu_forward(tt_test_in, device)
-
 
         tt_out_converted = bloom_utils.tt2torch_tensor(tt_out)
 
@@ -52,7 +59,6 @@ def run_bloom_gelu_forward_test(device):
         print(comp_allclose(pt_out, tt_out_converted))
         print(pcc_message)
 
-        print("TTLIB!-----")
 
         if does_pass:
             logger.info("bloom_gelu_forward: Passed!")
@@ -61,12 +67,34 @@ def run_bloom_gelu_forward_test(device):
 
         assert does_pass
 
-        print("APPROX!-----")
+        print("TT LIB APPROX!-----")
+
+        tt_out_2 = bloom_gelu_forward.tt_bloom_gelu_forward(tt_test_in, device)
+
         tt_out_converted_2 = bloom_utils.tt2torch_tensor(tt_out_2)
 
         does_pass, pcc_message = comp_pcc(pt_out, tt_out_converted_2, 0.98)
 
         print(comp_allclose(pt_out, tt_out_converted_2))
+        print(pcc_message)
+
+        if does_pass:
+            logger.info("bloom_gelu_forward: Passed!")
+        else:
+            logger.warning("bloom_gelu_forward: Failed!")
+
+        assert does_pass
+
+
+
+        print("PT APPROX!-----")
+
+        pt_out_3 = bloom_gelu_forward.bloom_gelu_forward(test_in)
+
+
+        does_pass, pcc_message = comp_pcc(pt_out, pt_out_3, 0.98)
+
+        print(comp_allclose(pt_out, pt_out_3))
         print(pcc_message)
 
         if does_pass:
