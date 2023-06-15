a9a95640721983aed0070ae647e719aae7050b9b
Nitika Shanker <nshanker@tenstorrent.com>
2023-06-15 18:31:52 +0000

    #0: bmm test

--- tests/python_api_testing/unit_testing/test_bmm_tilize_untilize.py
+++ tests/python_api_testing/unit_testing/test_bmm_tilize_untilize.py
@@ -22,18 +22,31 @@ TILE_HEIGHT = TILE_WIDTH = 32
 
 ## parameters
 # matrix sizes as number of blocks along h and w:
-a_height_nblocks = [1, 7]
-a_width_nblocks = [1, 7]
-b_width_nblocks = [1, 7]
+# a_height_nblocks = [1, 7]
+# a_width_nblocks = [1, 7]
+# b_width_nblocks = [1, 7]
+# # block sizes as number of tiles along h and w:
+# a_block_height_ntiles = [4]
+# a_block_width_ntiles = [4]
+# b_block_width_ntiles = [16]
+# # output sublobcking per block:
+# out_subblock_height_ntiles = [4] ## == a_block_height_ntiles, <= 8
+# out_subblock_width_ntiles = [2]  ## == b_block_width_ntiles, <= 8
+# tilize_a = [True, False]
+# untilize_out = [True, False]
+
+a_height_nblocks = [2]
+a_width_nblocks = [135]
+b_width_nblocks = [10]
 # block sizes as number of tiles along h and w:
 a_block_height_ntiles = [4]
 a_block_width_ntiles = [4]
-b_block_width_ntiles = [16]
+b_block_width_ntiles = [4]
 # output sublobcking per block:
 out_subblock_height_ntiles = [4] ## == a_block_height_ntiles, <= 8
 out_subblock_width_ntiles = [2]  ## == b_block_width_ntiles, <= 8
-tilize_a = [True, False]
-untilize_out = [True, False]
+tilize_a = [True]
+untilize_out = [True]
 
 
 @pytest.mark.parametrize(
