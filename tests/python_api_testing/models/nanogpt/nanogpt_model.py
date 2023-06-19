07823acba992479133d390410de53813856a30fb
Nenad Petrovic <npetrovic@tenstorrent.com>
2023-06-19 06:49:28 +0000

    #492: Fixed model weights shapes

--- tests/python_api_testing/models/nanogpt/nanogpt_model.py
+++ tests/python_api_testing/models/nanogpt/nanogpt_model.py
@@ -94,6 +94,10 @@ class TtGPT(nn.Module):
             self.lm_weight, device
         )
 
+        self.tt_weight_lm_head = tt_lib.tensor.transpose(self.tt_weight_lm_head)
+
+        self.wte.weight = nn.Parameter(self.lm_weight) # https://paperswithcode.com/method/weight-tying
+
 
     def forward(self, idx, device):
         b, t = idx.shape
@@ -129,3 +133,37 @@ class TtGPT(nn.Module):
         loss = None
 
         return logits, loss
+
+def generate(self, idx, device, max_new_tokens, temperature=1.0, top_k=None):
+        """
+        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+        the sequence max_new_tokens times, feeding the predictions back into the model each time.
+        Most likely you'll want to make sure to be in model.eval() mode of operation for this.
+        """
+        for _ in range(max_new_tokens):
+            # if the sequence context is growing too long we must crop it at block_size
+            idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
+            # forward the model to get the logits for the index in the sequence
+            tt_logits, _ = self.forward(idx_cond, device)
+            # pluck the logits at the final step and scale by desired temperature
+
+            logits = nanogpt_utils.tt2torch_tensor(tt_logits)
+
+            logits = logits[:, -1, :] / temperature
+            # optionally crop the logits to only the top k options
+            tt_logits = nanogpt_utils.torch2tt_tensor(logits, device)
+
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = -float('Inf')
+            # apply softmax to convert logits to (normalized) probabilities
+
+            tt_probs = fallback_ops.softmax(tt_logits, dim=-1)
+            probs = nanogpt_utils.tt2torch_tensor(tt_probs)
+
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)
+            # append sampled index to the running sequence and continue
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
