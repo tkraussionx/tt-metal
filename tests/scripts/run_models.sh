3dc5c3497b55149267fa072a76baa42b1b233547
Branko Arsic <barsic@e12cs05.tenstorrent.net>
2023-06-21 08:16:11 +0000

    #491: Add llama pytest to regression

--- tests/scripts/run_models.sh
+++ tests/scripts/run_models.sh
@@ -48,10 +48,7 @@ env pytest tests/python_api_testing/models/synthetic_gradients -k linear_test
 env pytest tests/python_api_testing/models/synthetic_gradients -k block_test
 env pytest tests/python_api_testing/models/synthetic_gradients -k full_inference
 
-env pytest tests/python_api_testing/models/llama -k llama_layer_norm
-env pytest tests/python_api_testing/models/llama -k llama_mlp
-env pytest tests/python_api_testing/models/llama -k llama_attention
-env pytest tests/python_api_testing/models/llama -k llama_decoder
+env pytest tests/python_api_testing/models/llama_split/tests -k llama_split_perf
 
 env pytest tests/python_api_testing/models/whisper -k whisper_attention
 env pytest tests/python_api_testing/models/whisper -k WhipserEncoderLayer_inference
