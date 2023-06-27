9016e7a1d6c715a2e3abe6a95f49ce77ec369f1c
Aaron Shayesteh <ashayestehmanesh@tenstorrent.com>
2023-06-27 16:32:44 +0000

    #0: replace linear helperfuncs with a class

--- tests/python_api_testing/models/mini_graphs.py
+++ tests/python_api_testing/models/mini_graphs.py
@@ -1,6 +1,6 @@
 from typing import List, Union, Optional
 from tt_lib import tensor
-
+from torch import nn
 from loguru import logger
 
 def Linear(in_features: int, out_features: int, weight: tensor.Tensor, bias: Optional[tensor.Tensor] = None):
@@ -29,3 +29,29 @@ def Linear(in_features: int, out_features: int, weight: tensor.Tensor, bias: Opt
         return output
 
     return linear_
+
+
+class Linear(nn.Module):
+
+    def __init__(self, in_features: int, out_features: int, weight: tensor.Tensor, bias: Optional[tensor.Tensor] = None, transpose_weights=True):
+
+        assert weight.shape() == [1, 1, out_features, in_features], "weight does not have the expected shape"
+
+        self.in_features = in_features
+        self.out_features = out_features
+        self.bias = bias
+
+        if transpose_weights:
+            self.weight_T = tensor.transpose(weight)
+        else:
+            self.weight_T = weight
+
+    def forward(self, activation: tensor.Tensor) -> tensor.Tensor:
+        assert activation.shape()[-1] == self.in_features, "activation tensor do not have the expected shape"
+        output = tensor.matmul(activation, self.weight_T)
+
+        if self.bias is not None:
+            output_plus_bias = tensor.bcast(output, self.bias, tensor.BcastOpMath.ADD, tensor.BcastOpDim.H)
+            return output_plus_bias
+
+        return output
