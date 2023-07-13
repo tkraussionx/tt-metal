ad0c27ab611e8725ea3f9ef1bdc8e3d39570a46d
Nenad Petrovic <npetrovic@tenstorrent.com>
2023-07-13 08:43:50 +0000

    #1543: Added attention subgraphs fixed

--- tests/python_api_testing/models/codegen/tests/test_codegen_fixed_pos_embed.py
+++ tests/python_api_testing/models/codegen/tests/test_codegen_fixed_pos_embed.py
@@ -38,11 +38,11 @@ def run_codegen_fixed_pos_embed_test(device, pcc):
 
     pt_out = codegen_fixed_pos_embed.pt_fixed_pos_embed(test_in, seq_dim=1, seq_len=None)
 
-    tt_out = codegen_fixed_pos_embed.tt_fixed_pos_embed(tt_test_in, seq_dim=1, seq_len=None)
+    tt_out = codegen_fixed_pos_embed.tt_fixed_pos_embed(tt_test_in, device, seq_dim=1, seq_len=None)
 
-    tt_out_converted = tt2torch_tensor(tt_out)
+    tt_out_converted = tt2torch_tensor(tt_out[0])
 
-    does_pass, pcc_message = comp_pcc(pt_out, tt_out_converted, 0.99)
+    does_pass, pcc_message = comp_pcc(pt_out[0], tt_out_converted, 0.99)
     logger.info(pcc_message)
 
     if does_pass:
@@ -64,5 +64,6 @@ def test_codegen_merge_heads(pcc):
     device = tt_lib.device.CreateDevice(tt_lib.device.Arch.GRAYSKULL, 0)
     tt_lib.device.InitializeDevice(device)
     tt_lib.device.SetDefaultDevice(device)
+
     run_codegen_fixed_pos_embed_test(device, pcc)
     tt_lib.device.CloseDevice(device)
