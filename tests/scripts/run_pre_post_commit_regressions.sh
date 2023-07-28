7c5b44d0a820231f91fe6b57013c967f02246ae4
Nitika Shanker <nshanker@tenstorrent.com>
2023-07-28 00:05:58 +0000

    #0: test sweep conv test only (no other tests) on CI

--- tests/scripts/run_pre_post_commit_regressions.sh
+++ tests/scripts/run_pre_post_commit_regressions.sh
@@ -18,26 +18,26 @@ export PYTHONPATH=$TT_METAL_HOME
 
 if [ "$ARCH_NAME" == "grayskull" ]; then
   ./tests/scripts/run_python_api_unit_tests.sh
-  env python tests/scripts/run_tt_metal.py
-else
-  ./build/test/tt_metal/test_bcast --arch $ARCH_NAME
-  ./build/test/tt_metal/test_reduce_hw --arch $ARCH_NAME
-  ./build/test/tt_metal/test_reduce_w --arch $ARCH_NAME
-  ./build/test/tt_metal/test_reduce_h --arch $ARCH_NAME
+  #env python tests/scripts/run_tt_metal.py
+# else
+#   ./build/test/tt_metal/test_bcast --arch $ARCH_NAME
+#   ./build/test/tt_metal/test_reduce_hw --arch $ARCH_NAME
+#   ./build/test/tt_metal/test_reduce_w --arch $ARCH_NAME
+#   ./build/test/tt_metal/test_reduce_h --arch $ARCH_NAME
 fi
 
-./build/test/tt_metal/unit_tests
-./build/test/tt_metal/gtest_unit_tests
+# ./build/test/tt_metal/unit_tests
+# ./build/test/tt_metal/gtest_unit_tests
 
-echo "Checking docs build..."
+# echo "Checking docs build..."
 
-cd $TT_METAL_HOME/docs
-python -m pip install -r requirements-docs.txt
-make clean
-make html
+# cd $TT_METAL_HOME/docs
+# python -m pip install -r requirements-docs.txt
+# make clean
+# make html
 
 # Tests profiler module FW side
 # NOTE: Keep this test last as it requires a fresh ENABLE_PROFILER=1 build
-echo "Run profiler regression"
+#echo "Run profiler regression"
 cd $TT_METAL_HOME
-./tests/scripts/run_profiler_regressions.sh PROFILER
+#./tests/scripts/run_profiler_regressions.sh PROFILER
