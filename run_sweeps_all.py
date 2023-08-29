f132a73a157c6ee44755341e175142044d95d6d2
Nemanja Grujic <ngrujic@tenstorrent.com>
2023-08-29 08:49:05 +0000

    #2173: Edit test sweeps not to get stuck

---
+++ run_sweeps_all.py
@@ -0,0 +1,27 @@
+import os
+from glob import glob
+import subprocess
+
+directory = 'tests/python_api_testing/sweep_tests/test_configs/ci_sweep_tests/'
+result_folder = "/home/ubuntu/tt-metal/ng-test-sweeps/"
+
+if __name__ == "__main__":
+    txt_files = glob(os.path.join(directory, '*.yaml'))
+    txt_files.sort()
+    do_run = False
+
+    for txt_file in txt_files:
+        basename = os.path.splitext(os.path.basename(txt_file))[0]
+        command = f'./tt_metal/tools/profiler/profile_this.py -c "python tests/python_api_testing/sweep_tests/run_pytorch_test.py -i {txt_file} -o {result_folder}{basename}" -o {result_folder}{basename}'
+
+        if basename == "pytorch_eltwise_hypot_test":
+            do_run = True
+
+        if do_run:
+            print(command)
+
+            subprocess.run(["rm -rf tt_metal/tools/profiler/logs/ops_device"], shell=True, check=True)
+            subprocess.run(["rm -rf tt_metal/tools/profiler/logs/ops"], shell=True, check=True)
+            subprocess.run([command], shell=True, check=True)
+
+            break
