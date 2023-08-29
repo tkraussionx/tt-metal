159d906633a708edec565a966301e5303a2d97d3
Nemanja Grujic <ngrujic@tenstorrent.com>
2023-08-29 09:37:17 +0000

    #2173: Sweeps: remove broken tess to broken folder

--- run_sweeps_all.py
+++ run_sweeps_all.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         basename = os.path.splitext(os.path.basename(txt_file))[0]
         command = f'./tt_metal/tools/profiler/profile_this.py -c "python tests/python_api_testing/sweep_tests/run_pytorch_test.py -i {txt_file} -o {result_folder}{basename}" -o {result_folder}{basename}'
 
-        if basename == "pytorch_eltwise_hypot_test":
+        if basename == "pytorch_permute_test":
             do_run = True
 
         if do_run:
@@ -23,5 +23,3 @@ if __name__ == "__main__":
             subprocess.run(["rm -rf tt_metal/tools/profiler/logs/ops_device"], shell=True, check=True)
             subprocess.run(["rm -rf tt_metal/tools/profiler/logs/ops"], shell=True, check=True)
             subprocess.run([command], shell=True, check=True)
-
-            break
