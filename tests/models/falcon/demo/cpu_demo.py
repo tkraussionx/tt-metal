#2549: Added attention mask to mask out paddings, and fix bug in post processor in demo

tests/models/falcon/demo/cpu_demo.py
@@ -27,7 +27,8 @@ def post_process(logits, input_ids, logits_processor):
     dump_tensor("input_ids", "hf", input_ids)
     next_token_logits = logits[:, -1, :]
     next_tokens_scores = logits_processor(input_ids, next_token_logits)
-    topk = torch.topk(next_tokens_scores, 5)[1]
+    topk = torch.topk(next_tokens_scores, 20)[1]
+    print(f"topk {topk}")
     dump_tensor("topk_output", "hf", torch.topk(next_tokens_scores, 5)[1])
     next_tokens = torch.argmax(next_tokens_scores, dim=-1)
     ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)

tests/models/falcon/demo/gs_demo.py
@@ -15,13 +15,14 @@ from tests.models.falcon.model_config import (
 )
 from models.utility_functions import torch2tt_tensor, tt2torch_tensor, dump_tensor
 
-def post_process(logits, input_ids, logits_processor):
+def post_process(logits, input_ids, logits_processor, last_index=-1):
     dump_tensor("logits", "tt", logits)
     dump_tensor("input_ids", "tt", input_ids)
-    next_token_logits = logits[:, -1, :]
+    next_token_logits = logits[:, last_index, :]
     next_tokens_scores = logits_processor(input_ids, next_token_logits)
     next_tokens = torch.argmax(next_tokens_scores, dim=-1)
-    topk = torch.topk(next_tokens_scores, 5)[1]
+    topk = torch.topk(next_tokens_scores, 20)[1]
+    print(f"topk {topk}")
     dump_tensor("topk_output", "tt", torch.topk(next_tokens_scores, 5)[1])
     ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)
     print("OUTPUT IDS", ids)
@@ -36,7 +37,7 @@ def test_gs_demo_kv(device):
     batch_size = 32
     seq_len = 5
     max_seq_len = 32
-    num_layers = 1
+    num_layers = 32
 
     hugging_face_reference_model = FalconForCausalLM.from_pretrained(model_version)
 
@@ -116,7 +117,7 @@ def test_gs_demo_kv(device):
 
     logits = tt2torch_tensor(tt_logits).squeeze(1)
     tt_logits.deallocate()
-    output_ids = post_processor(logits=logits, input_ids=prefill_ids)
+    output_ids = post_processor(logits=logits, input_ids=prefill_ids, last_index=seq_len-1)
     output_ids = output_ids[:, -1:]
 
     generated_ids = torch.concat((prefill_ids[..., :seq_len], output_ids), dim=1)