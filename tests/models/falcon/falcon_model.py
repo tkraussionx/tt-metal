#2549: Added attention mask to mask out paddings, and fix bug in post processor in demo

tests/models/falcon/falcon_model.py
@@ -141,7 +141,7 @@ class TtFalconModelShared(torch.nn.Module):
             attention_mask_bool_padded = torch.cat(
                 (
                     attention_mask_bool,
-                    torch.ones(batch, 1, q_len, kv_len_padded - kv_len, dtype=bool),
+                    torch.ones(batch, 1, q_len, kv_len_padded - kv_len, dtype=bool)*1000,
                 ),
                 dim=-1,
             )

tests/models/falcon/reference/hf_modeling_falcon.py
@@ -870,7 +870,7 @@ class FalconModel(FalconPreTrainedModel):
 
         # Transformer blocks
         self.h = nn.ModuleList(
-            [FalconDecoderLayer(config) for _ in range(1)]
+            [FalconDecoderLayer(config) for _ in range(32)]
         )
 
         # Final Layer Norm