d83caadc696642de56d15c59cc0813f4248cdc9f
Brian Liu <bliu@tenstorrent.com>
2023-09-29 19:02:54 +0000

    #0: Add noc_async_read_barrier to bias reads

--- tt_metal/kernels/dataflow/reader_bmm_single_core_bias.hpp
+++ tt_metal/kernels/dataflow/reader_bmm_single_core_bias.hpp
@@ -21,6 +21,7 @@ FORCE_INLINE void read_bias(uint32_t bias_addr,
         s_bias.noc_async_read_page(bias_tile, bias_l1_addr);
         bias_l1_addr += bias_pagesize;
     }
+    noc_async_read_barrier();
     cb_push_back(bias_cb_id, bias_ntiles);
 } // read_bias()
 
@@ -42,5 +43,6 @@ FORCE_INLINE void read_bias_with_offset(uint32_t bias_addr,
         s_bias.noc_async_read_page(bias_tile, bias_l1_addr);
         bias_l1_addr += bias_pagesize;
     }
+    noc_async_read_barrier();
     cb_push_back(bias_cb_id, bias_ntiles);
 } // read_bias()
