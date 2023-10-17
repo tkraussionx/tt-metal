879c25f40cf1e8f8fa4c1cce7bbdd4983a91df92
Allan Liu <aliu@tenstorrent.com>
2023-10-17 21:32:41 +0000

    #0: adding direct send/receive hexes, tests passing

--- tests/tt_metal/tt_metal/unit_tests/erisc/direct.cpp
+++ tests/tt_metal/tt_metal/unit_tests/erisc/direct.cpp
@@ -20,7 +20,7 @@
 constexpr std::int32_t FIRMWARE_BASE = 0x9040;
 constexpr std::int32_t RUN_APP_FLAG = 0x10E8;
 constexpr std::int32_t DATA_BUFFER_SPACE_BASE = 0x28000;
-constexpr std::int32_t ETH_L1_ARGS_BASE = 0x3E000;
+constexpr std::int32_t ETH_L1_ARGS_BASE = 0x3E420;
 constexpr std::int32_t MAX_NUM_WORDS = 5632;
 constexpr std::int32_t WORD_SIZE = 16; // 16 bytes per eth send packet
 
@@ -39,7 +39,7 @@ bool send_over_eth(
     const CoreCoord& sender_core,
     const CoreCoord& receiver_core,
     const size_t& byte_size) {
-  std::cout << " running test, sender chip " << sender_device->id() << " core " << sender_core.x << " " << sender_core.y << " receiver chip " << receiver_device->id() << " core " << receiver_core.x << " " << receiver_core.y << std::endl;
+  std::cout << " running test, sender chip " << sender_device->id() << " core " << sender_core.x << " " << sender_core.y << " receiver chip " << receiver_device->id() << " core " << receiver_core.x << " " << receiver_core.y << " num_bytes " << byte_size << std::endl;
   std::vector<CoreCoord> eth_cores = {{.x = 9, .y = 0}, {.x = 1, .y = 0}, {.x = 8, .y = 0}, {.x = 2, .y = 0}, {.x = 9, .y = 6}, {.x = 1, .y = 6}, {.x = 8, .y = 6}, {.x = 2, .y = 6}, {.x = 7, .y = 0}, {.x = 3, .y = 0}, {.x = 6, .y = 0}, {.x = 4, .y = 0}, {.x = 7, .y = 6}, {.x = 3, .y = 6}, {.x = 6, .y = 6}, {.x = 4, .y = 6}};
 
   // Disable all eth core runtime app
@@ -60,12 +60,13 @@ bool send_over_eth(
   std::vector<uint32_t> all_zeros(inputs.size(), 0);
   llrt::write_hex_vec_to_core(receiver_device->id(), receiver_core, all_zeros, DATA_BUFFER_SPACE_BASE);
 
-  uint32_t arg_0 = byte_size >> 4; // number of 16 byte sends
+  uint32_t arg_0 = byte_size;
   std::vector<uint32_t> args = {arg_0};
   llrt::write_hex_vec_to_core(sender_device->id(), sender_core, args, ETH_L1_ARGS_BASE);
+  llrt::write_hex_vec_to_core(receiver_device->id(), receiver_core, args, ETH_L1_ARGS_BASE);
 
-  ll_api::memory binary_mem_send = llrt::get_risc_binary("erisc_app_direct_transfer.hex", sender_device->id(), true);
-  ll_api::memory binary_mem_receive = llrt::get_risc_binary("erisc_app_direct_transfer.hex", receiver_device->id(), true);
+  ll_api::memory binary_mem_send = llrt::get_risc_binary("erisc_app_direct_send.hex", sender_device->id(), true);
+  ll_api::memory binary_mem_receive = llrt::get_risc_binary("erisc_app_direct_receive.hex", receiver_device->id(), true);
 
   for (const auto& eth_core: eth_cores) {
     llrt::write_hex_vec_to_core(sender_device->id(), eth_core, binary_mem_send.data(), FIRMWARE_BASE);
@@ -75,6 +76,7 @@ bool send_over_eth(
   // Activate sender core runtime app
     run_test_app_flag = {0x1};
     llrt::write_hex_vec_to_core(sender_device->id(), sender_core, run_test_app_flag, RUN_APP_FLAG);
+    llrt::write_hex_vec_to_core(receiver_device->id(), receiver_core, run_test_app_flag, RUN_APP_FLAG);
 
 
   bool pass = true;
@@ -188,4 +190,28 @@ TEST_F(DeviceFixture, BidirectionalEthCoreDirectSend) {
     unit_tests::erisc::direct::send_over_eth(device_0, device_1, sender_core_1, receiver_core_1, WORD_SIZE * MAX_NUM_WORDS));
   ASSERT_TRUE(
     unit_tests::erisc::direct::send_over_eth(device_1, device_0, receiver_core_1, sender_core_1, WORD_SIZE * MAX_NUM_WORDS));
+}
+
+TEST_F(DeviceFixture, RandomDirectSendTests) {
+  srand(0);
+  ASSERT_TRUE(this->num_devices_ == 2);
+
+  std::map<std::pair<int, CoreCoord>, std::pair<int, CoreCoord>> connectivity = {
+    {{0, {.x=9, .y=6}}, {1, {.x=9, .y=0}}}, {{1, {.x=9, .y=0}}, {0, {.x=9, .y=6}}},
+    {{0, {.x=1, .y=6}}, {1, {.x=1, .y=0}}}, {{1, {.x=1, .y=0}}, {0, {.x=1, .y=6}}}
+  };
+  for (int i=0; i<100; i++) {
+     auto it = connectivity.begin();
+     std::advance(it, rand() % (connectivity.size()) );
+
+     const auto& send_chip = devices_.at(std::get<0>(it->first));
+     CoreCoord sender_core = std::get<1>(it->first);
+     const auto& receiver_chip = devices_.at(std::get<0>(it->second));
+     CoreCoord receiver_core = std::get<1>(it->second);
+     int num_words = rand() % MAX_NUM_WORDS + 1;
+
+     ASSERT_TRUE(
+        unit_tests::erisc::direct::send_over_eth(send_chip, receiver_chip, sender_core, receiver_core, WORD_SIZE * num_words));
+  }
+
 }
