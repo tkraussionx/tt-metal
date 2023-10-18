fa2e48fe21da9bb0e0543e0b40b4550fa344195e
Brian Liu <bliu@tenstorrent.com>
2023-10-18 18:03:33 +0000

    #0: Integrate conv with new reader for 128-128-3-3-1-1 conv

--- tests/models/resnet/metalResnetBlock50.py
+++ tests/models/resnet/metalResnetBlock50.py
@@ -16,7 +16,12 @@ from tt_lib.fused_ops.average_pool import run_avg_pool_on_device_wrapper as TtAv
 from tt_lib.fused_ops.max_pool import run_max_pool_on_device_wrapper as TtMaxPool
 from tt_lib.fused_ops.max_pool import compute_max_pool_shape
 from tt_lib.fused_ops.softmax import softmax as TtSoftmax
-from tt_lib.fused_ops.conv import resnet50_first_conv, resnet50_1x1_conv_as_matmul, resnet50_optimized_conv, resnet50_1x1_conv_s2_as_downsample_and_matmul
+from tt_lib.fused_ops.conv import (
+    resnet50_first_conv,
+    resnet50_1x1_conv_as_matmul,
+    resnet50_optimized_conv,
+    resnet50_1x1_conv_s2_as_downsample_and_matmul,
+)
 from models.utility_functions import _nearest_32, profiler
 from tt_lib.fallback_ops import fallback_ops
 
@@ -549,7 +554,8 @@ class Bottleneck(nn.Module):
         sharded=None,
         out_sharded=False,
         act_block_w_equals_input_channels_x_filter_width=False,
-        use_downsample_op_and_mm_for_conv1x1_s2=False
+        use_downsample_op_and_mm_for_conv1x1_s2=False,
+        conv_halo=False,
     ) -> None:
         super().__init__()
         self.device = device
@@ -560,6 +566,7 @@ class Bottleneck(nn.Module):
         self.norm_layer_after_downsample_conv_on_tt = norm_layer_after_downsample_conv_on_tt
         self.downsample_params = downsample_params
         self.storage_in_dram = storage_in_dram
+        self.conv_halo = conv_halo
         if self.storage_in_dram:
             self.memory_config = tt_lib.tensor.MemoryConfig(
                 tt_lib.tensor.TensorMemoryLayout.INTERLEAVED, tt_lib.tensor.BufferType.DRAM
@@ -689,6 +696,7 @@ class Bottleneck(nn.Module):
             conv2_bias.tolist(),
             True,
             output_mem_config=self.sharded_memory_config,
+            input_tensor_shape=self.conv1_output_shape,
         )
 
         self.conv3_params = [planes * self.expansion, width, 1, 1, 1, 1, 0, 0, dilation, groups]
@@ -719,7 +727,9 @@ class Bottleneck(nn.Module):
         if self.downsample_or_noop is None:
             self.downsample_or_noop = do_nothing_op
         else:
-            if (not use_downsample_op_and_mm_for_conv1x1_s2) and (self.downsample_params[2] != 1 or self.downsample_params[4] != 1 or self.downsample_params[6] != 0):
+            if (not use_downsample_op_and_mm_for_conv1x1_s2) and (
+                self.downsample_params[2] != 1 or self.downsample_params[4] != 1 or self.downsample_params[6] != 0
+            ):
                 # this downsample conv requires row major input
                 def downsample_conv_op_wrapper(op):
                     def downsample_conv_op_with_formatting(x):
@@ -747,13 +757,23 @@ class Bottleneck(nn.Module):
         # Relu after conv1 is fused with the 1x1 conv (matmul)
         # out = self.relu(out, self.memory_config)
         # print("Running untilize op")
-        out = format_tensor(out, tt_lib.tensor.Layout.ROW_MAJOR, self.device, self.memory_config)
-        out = out.reshape(
-            self.conv1_output_shape[0],
-            self.conv1_output_shape[1],
-            self.conv1_output_shape[2],
-            self.conv1_output_shape[3],
-        )
+        if self.conv_halo:
+            out = tt_lib.tensor.untilize_with_halo(
+                out,
+                0x0,
+                self.conv1_output_shape[0],
+                self.conv1_output_shape[1],
+                self.conv1_output_shape[2],
+                self.sharded_memory_config,
+            )
+        else:
+            out = format_tensor(out, tt_lib.tensor.Layout.ROW_MAJOR, self.device, self.memory_config)
+            out = out.reshape(
+                self.conv1_output_shape[0],
+                self.conv1_output_shape[1],
+                self.conv1_output_shape[2],
+                self.conv1_output_shape[3],
+            )
 
         # print("Running conv2")
         out = self.conv2(out)
@@ -926,6 +946,7 @@ class ResNet(nn.Module):
             sharded=tt_lib.tensor.TensorMemoryLayout.HEIGHT_SHARDED if sharded else None,
             out_sharded=False,
             use_downsample_op_and_mm_for_conv1x1_s2=True if sharded else False,
+            conv_halo=True if sharded else False,
         )
         self.layer3, self.layer3_output_shape = self._make_layer(
             block,
@@ -1018,6 +1039,7 @@ class ResNet(nn.Module):
         out_sharded=False,
         act_block_w_equals_input_channels_x_filter_width=False,
         use_downsample_op_and_mm_for_conv1x1_s2=False,
+        conv_halo=False,
     ):
         norm_layer = self._norm_layer
         downsample = None
@@ -1120,15 +1142,15 @@ class ResNet(nn.Module):
                 ]
                 assert stride == 2
                 downsample_op_params = [batch_size, layer_input_shape[1], layer_input_shape[2], stride, stride]
-                #print("Calling ds op and matmul op, input shape - ", layer_input_shape)
+                # print("Calling ds op and matmul op, input shape - ", layer_input_shape)
                 self.downsample_conv_on_tt = resnet50_1x1_conv_s2_as_downsample_and_matmul(
                     downsample_conv_weight.reshape(-1).tolist(),
                     self.downsample_params,
-                    downsample_op_params, # used by downsample op
+                    downsample_op_params,  # used by downsample op
                     self.device,
                     downsample_conv_bias.tolist(),
                     matmul_config,
-                    self.ds_conv_output_memory_config
+                    self.ds_conv_output_memory_config,
                 )
             else:
                 assert (
@@ -1192,11 +1214,12 @@ class ResNet(nn.Module):
                 sharded=sharded,
                 out_sharded=sharded,
                 act_block_w_equals_input_channels_x_filter_width=act_block_w_equals_input_channels_x_filter_width,
-                use_downsample_op_and_mm_for_conv1x1_s2=use_downsample_op_and_mm_for_conv1x1_s2
+                use_downsample_op_and_mm_for_conv1x1_s2=use_downsample_op_and_mm_for_conv1x1_s2,
+                # TODO: Add conv_halo=conv_halo once stride 2 is supported
             )
         )
         self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
+        for block_num in range(1, blocks):
             previous_layer = layers[-1]
             layers.append(
                 block(
@@ -1208,14 +1231,15 @@ class ResNet(nn.Module):
                     norm_layer=norm_layer,
                     device=self.device,
                     state_dict=self.state_dict,
-                    base_address=f"{self.base_address_with_dot}{name}.{_}",
+                    base_address=f"{self.base_address_with_dot}{name}.{block_num}",
                     fold_batchnorm=self.fold_batchnorm,
                     storage_in_dram=self.storage_in_dram,
                     input_shape=previous_layer.conv3_output_shape,
                     batch_size=batch_size,
                     sharded=sharded,
-                    out_sharded=True if _ != blocks - 1 else out_sharded,
+                    out_sharded=True if block_num != blocks - 1 else out_sharded,
                     act_block_w_equals_input_channels_x_filter_width=act_block_w_equals_input_channels_x_filter_width,
+                    conv_halo=conv_halo,
                 )
             )
         last_layer_shape = layers[-1].conv3_output_shape
