17a079ccac311399dcc6cce0e745a484301211f7
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2023-10-31 14:07:35 +0000

    #3003: only allow slicing the tensor if it's not on device

--- tests/ttnn/unit_tests/test_to_and_from.py
+++ tests/ttnn/unit_tests/test_to_and_from.py
@@ -12,7 +12,7 @@ import ttnn
 @pytest.mark.parametrize("h", [32])
 @pytest.mark.parametrize("w", [2 * 32])
 def test_to_and_from(device, h, w):
-    torch_activations = torch.rand((1, 1, h, w), dtype=torch.bfloat16)
-    tt_output = ttnn.from_torch(torch_activations)
+    torch_input = torch.rand((1, 1, h, w), dtype=torch.bfloat16)
+    tt_output = ttnn.from_torch(torch_input)
     torch_output = ttnn.to_torch(tt_output)
-    assert torch.allclose(torch_output, torch_activations, atol=1e-1, rtol=1e-2)
+    assert torch.allclose(torch_output, torch_input)
