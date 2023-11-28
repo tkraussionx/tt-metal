18099dc6b3904186e94530f943cad54fb683cb3f
seunghwan100 <seunghwan100@moreh.io>
2023-11-29 00:47:03 +0900

    #4041: Add moreh_layernorm op

--- tests/ttnn/unit_tests/experimental/test_layer_norm.py
+++ tests/ttnn/unit_tests/experimental/test_layer_norm.py
@@ -92,3 +92,41 @@ def test_layer_norm_with_weight_bias_and_residual_input(device, h, w):
     output_tensor = ttnn.to_torch(output_tensor)
 
     assert_with_pcc(torch_output_tensor, output_tensor, 0.9997)
+
+
+@skip_for_wormhole_b0()
+@pytest.mark.parametrize("h", [32])
+@pytest.mark.parametrize("w", [2 * 32])
+def test_moreh_layer_norm_with_weight_bias_and_residual_input(device, h, w):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.rand((1, 1, h, w), dtype=torch.bfloat16)
+    torch_residual_input_tensor = torch.rand((1, 1, h, w), dtype=torch.bfloat16)
+    torch_weight = torch.rand((w,), dtype=torch.bfloat16)
+    torch_bias = torch.rand((w,), dtype=torch.bfloat16)
+    torch_output_tensor = torch.nn.functional.layer_norm(
+        torch_input_tensor + torch_residual_input_tensor, normalized_shape=[w], weight=torch_weight, bias=torch_bias
+    )
+
+    input_tensor = ttnn.from_torch(torch_input_tensor)
+    residual_input_tensor = ttnn.from_torch(torch_residual_input_tensor)
+    weight = ttnn.from_torch(torch_weight)
+    bias = ttnn.from_torch(torch_bias)
+
+    input_tensor = ttnn.to_device(input_tensor, device)
+    input_tensor = ttnn.to_layout(input_tensor, ttnn.TILE_LAYOUT)
+    residual_input_tensor = ttnn.to_device(residual_input_tensor, device)
+    residual_input_tensor = ttnn.to_layout(residual_input_tensor, ttnn.TILE_LAYOUT)
+    weight = ttnn.to_device(weight, device)
+    weight = ttnn.to_layout(weight, ttnn.TILE_LAYOUT)
+    bias = ttnn.to_device(bias, device)
+    bias = ttnn.to_layout(bias, ttnn.TILE_LAYOUT)
+
+    output_tensor = ttnn.experimental.moreh_layer_norm(
+        input_tensor, residual_input=residual_input_tensor, weight=weight, bias=bias
+    )
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, 0.9997)
--- ttnn/experimental.py
+++ ttnn/experimental.py
@@ -65,3 +65,44 @@ def layer_norm(
     output_tensor = Tensor(output_tensor)
     output_tensor = reshape(output_tensor, original_shape)
     return output_tensor
+
+
+def moreh_layer_norm(
+    input_tensor: Tensor,
+    *,
+    epsilon: float = 1e-12,
+    residual_input: Optional[Tensor] = None,
+    weight: Optional[Tensor] = None,
+    bias: Optional[Tensor] = None,
+    memory_config: Optional[MemoryConfig] = DRAM_MEMORY_CONFIG,
+) -> Tensor:
+    original_shape = tuple(input_tensor.shape)
+    input_tensor = _reshape_to_4D(input_tensor)
+    if residual_input is not None:
+        residual_input = _reshape_to_4D(residual_input)
+    if weight is not None:
+        weight = _reshape_to_4D(weight)
+    if bias is not None:
+        bias = _reshape_to_4D(bias)
+
+    ttl_input_tensor = input_tensor._tensor
+    residual_input = residual_input._tensor if residual_input is not None else None
+    ttl_weight = weight._tensor if weight is not None else None
+    ttl_bias = bias._tensor if bias is not None else None
+
+    # if residual_input is not None:
+    #     output_tensor = ttl.tensor.add_layernorm(
+    #         ttl_input_tensor, residual_input, epsilon, ttl_weight, ttl_bias, output_mem_config=memory_config
+    #     )
+    # else:
+    output_tensor = ttl.tensor.layernorm(
+        ttl_input_tensor, epsilon, ttl_weight, ttl_bias, output_mem_config=memory_config
+    )
+    normalized_dims = 1
+    output_tensor = ttl.operations.primary.moreh_layernorm(
+        ttl_input_tensor, normalized_dims, epsilon, ttl_weight, ttl_bias
+    )  # , mean=npu_mean, rstd=npu_rstd)
+
+    output_tensor = Tensor(output_tensor)
+    output_tensor = reshape(output_tensor, original_shape)
+    return output_tensor
