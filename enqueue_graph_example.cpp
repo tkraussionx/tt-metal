16883ce2c992e0afd878225073b53f56d7f11543
DrJessop <andrewgrebenisan@gmail.com>
2024-01-04 21:14:37 +0000

    #0: Better apis

--- enqueue_graph_example.cpp
+++ enqueue_graph_example.cpp
@@ -6,30 +6,30 @@ int main() {
         that our command queues are in a start state (can potentially implicitly call
         finish and then reset the pointers)
     */
-    BeginTrace(dispatch_queue=cq0, dataloader_queue=cq1);
+    CommandQueueGraph g = BeginTrace(dispatch_queue=cq0, dataloader_queue=cq1);
 
-    CreateInputNode(cq1, input_buffer0); // Notice this API does not take actual data, just the buffer
-    CreateInputNode(cq1, input_buffer1);
+    CreateInputNode(input_buffer0); // Notice this API does not take actual data, just the buffer
+    CreateInputNode(input_buffer1);
 
-    CreateProgramNode(cq0, program0); // Caches the enqueue program command and writes the program to DRAM
-    CreateProgramNode(cq0, program1);
+    CreateProgramNode(program0); // Caches the enqueue program command and writes the program to DRAM
+    CreateProgramNode(program1);
     ...
-    CreateProgramNode(cq0, program100);
+    CreateProgramNode(program100);
 
-    CreateOutput(cq1, output_buffer0);
-
+    CreateOutput(output_buffer0);
     /*
         Implicitly calls finish, resets the dispatch command queue pointers and re-writes the enqueue program
-        commands into the hugepage. Ensures that the inputs are double-buffered.
+        commands into the hugepage. Ensures that the inputs are double-buffered. Has to write the program twice
+        given that the inputs are double-buffered.
     */
-    CommandQueueGraph g = EndTrace(dispatch_queue=cq0, dataloader_queue=cq1);
+    EndTrace(g);
 
     /*
         Events handled behind the scenes when running in graph mode.
     */
     for (int i = 0; i < 100; i++) {
         g.EnqueueWriteBuffer(0, data); // Writes data to buffer0 in a double buffered fashion.
-        g.Enqueue();
+        g.Enqueue(); // Runs the graph
     }
 
     EnqueueWriteBuffer(...); // Will assert, since in graph mode we can only use graph APIs
