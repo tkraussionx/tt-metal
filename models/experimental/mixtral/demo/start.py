63250e551723d26738fe6c75e4bb211f0b21ffbb
farbabi <farbabi@tenstorrent.com>
2024-01-17 15:33:49 +0000

    #0: mixtral demo

--- models/experimental/mixtral/demo/start.py
+++ models/experimental/mixtral/demo/start.py
@@ -1,14 +1,21 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 device = "cpu"  # the device to load the model onto
-
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-8x7B")
+print("model...")
+model = AutoModelForCausalLM.from_pretrained(
+    "mistralai/Mixtral-8x7B-Instruct-v0.1", torch_dtype=torch.bfloat16, low_cpu_mem_usage=True
+)
+print("tokenizer...")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
 
 prompt = "My favourite condiment is"
-
+print("model inputs...")
 model_inputs = tokenizer([prompt], return_tensors="pt").to(device)
+print("model to device...")
 model.to(device)
-
-generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample=True)
-tokenizer.batch_decode(generated_ids)[0]
+print("generate ids...")
+generated_ids = model.generate(**model_inputs, max_new_tokens=80, do_sample=True, pad_token_id=tokenizer.eos_token_id)
+print("tokenizer...")
+out = tokenizer.batch_decode(generated_ids)[0]
+print(out)
