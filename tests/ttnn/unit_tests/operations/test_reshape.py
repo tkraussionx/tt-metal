6323c4d7a57cb121ceb7006d84a2c5a2fda97407
Dalar Vartanians <dvartanians@tenstorrent.com>
2024-02-08 00:30:15 +0000

    #5197: TTNN reshape fails

---
+++ tests/ttnn/unit_tests/operations/test_reshape.py
@@ -0,0 +1,21 @@
+import ttnn
+import torch
+import time
+
+device_id = 0
+device = ttnn.open(device_id)
+
+# Reshape into specified shape
+start = time.time()
+x = torch.rand((3, 224, 224), dtype=torch.bfloat16)
+x = ttnn.from_torch(x)
+x = ttnn.to_device(x, device, memory_config=ttnn.L1_MEMORY_CONFIG)
+x = ttnn.to_layout(x, ttnn.ROW_MAJOR_LAYOUT)
+# output = ttnn.reshape(x,(224, 3, 224))
+output = ttnn.reshape(x, (224, 224, 3))
+# a or b cannot be reshaped because they are in TILE_LAYOUT. Only ROW_MAJOR_LAYOUT can be reshaped
+end = time.time()
+execution_time = end - start
+print("Total time: {}ms".format(execution_time * 1000))
+
+ttnn.close(device)
