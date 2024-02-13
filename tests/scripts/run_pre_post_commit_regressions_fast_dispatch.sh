68989a73b5292f92a4d11b30b3d0923896ed7302
Michael Chiou <mchiou@tenstorrent.com>
2024-02-12 22:17:40 -0800

    #4697: Time only fast dispatch regression tests for CI runs

--- tests/scripts/run_pre_post_commit_regressions_fast_dispatch.sh
+++ tests/scripts/run_pre_post_commit_regressions_fast_dispatch.sh
@@ -20,12 +20,19 @@ fi
 cd $TT_METAL_HOME
 export PYTHONPATH=$TT_METAL_HOME
 
+START="$(date +%s)"
 ./tests/scripts/run_python_api_unit_tests.sh
-
+D1=$[$(date +%s)-${START}]
+echo "4697: run_python_api_unit_tests time: ${D1}"
 env python tests/scripts/run_tt_metal.py --dispatch-mode fast
+D2=$[$(date +%s)-${D1}]
+echo "4697: run_python_api_unit_tests time: ${D2}"
 env python tests/scripts/run_tt_eager.py --dispatch-mode fast
+D3=$[$(date +%s)-${D2}]
+echo "4697: run_python_api_unit_tests time: ${D3}"
 ./build/test/tt_metal/unit_tests_fast_dispatch
-
+D4=$[$(date +%s)-${D3}]
+echo "4697: run_python_api_unit_tests time: ${D4}"
 
 echo "Checking docs build..."
 
