26a710c83e9ffdc56be80ad7021375b8e9e00a7b
mtairum <mtairum@tenstorrent.com>
2024-02-13 16:55:25 +0000

    #5337: Updated mistral model config and MLP and respective test.

--- models/experimental/mistral/tests/test_mistral_mlp.py
+++ models/experimental/mistral/tests/test_mistral_mlp.py
@@ -1,60 +1,62 @@
 # SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
 
 # SPDX-License-Identifier: Apache-2.0
+
 import torch
-import tt_lib
 import pytest
 from loguru import logger
 import json
-from models.experimental.mistral.tt.mistral_configuration import TtModelArgs
-from models.experimental.mistral.tt.mistral_feed_forward import TtFeedForward
+from pathlib import Path
+from models.experimental.mistral.tt.model_config import TtModelArgs, get_model_config
+from models.experimental.mistral.tt.mistral_mlp import TtMistralMLP
 from models.experimental.mistral.reference.model import FeedForward
-from models.utility_functions import torch_to_tt_tensor_rm, tt_to_torch_tensor
+from models.utility_functions import torch2tt_tensor, tt2torch_tensor
 from models.utility_functions import (
     comp_pcc,
     comp_allclose,
 )
+from models.experimental.mistral.tt.model_config import (
+    get_model_config,
+)
 
 
 @pytest.mark.parametrize(
-    "dtype",
-    (tt_lib.tensor.DataType.BFLOAT16, tt_lib.tensor.DataType.BFLOAT8_B),
+    "model_config",
+    ("BFLOAT16-DRAM", "BFLOAT16-L1", "BFLOAT8-DRAM", "BFLOAT8-L1"),
 )
 @pytest.mark.parametrize(
     "pcc",
     ((0.99),),
 )
-def test_mistral_feed_forward_inference(pcc, model_location_generator, get_tt_cache_path, device, dtype, reset_seeds):
-    mistral_path = model_location_generator("mistral-7B-v0.1", model_subdir="Mistral")
+def test_mistral_mlp_inference(pcc, model_config, model_location_generator, get_tt_cache_path, device, reset_seeds):
+    dtype = model_config.split("-")[0]
+    mistral_path = Path(model_location_generator("mistral-7B-v0.1", model_subdir="mistral"))
     state_dict = torch.load(mistral_path / "consolidated.00.pth")
-    base_address = f"layers.0.feed_forward."
     with open(mistral_path / "params.json", "r") as f:
         model_args = TtModelArgs(**json.loads(f.read()))
 
     state_dict = {k[22:]: v for k, v in state_dict.items() if (k.startswith("layers.0.feed_forward"))}
+    base_address = f""
     model_args.max_batch_size = 1
     model_args.WEIGHTS_DTYPE = dtype
     reference_model = FeedForward(args=model_args)
     reference_model.load_state_dict(state_dict)
-    output_mem_config = tt_lib.tensor.MemoryConfig(
-        tt_lib.tensor.TensorMemoryLayout.INTERLEAVED, tt_lib.tensor.BufferType.DRAM
-    )
-    tt_cache_path = str(get_tt_cache_path("mistral-7B-v0.1", model_subdir="mistral")) + "/"
+    # print(state_dict.keys())
 
-    tt_model = TtFeedForward(
-        args=model_args,
+    tt_model = TtMistralMLP(
         device=device,
+        state_dict=state_dict,
         base_address=base_address,
-        tt_cache_path=tt_cache_path,
-        output_mem_config=output_mem_config,
+        model_config=get_model_config(model_config),
     )
-    input = torch.rand(1, 11, 4096)
+    input = torch.rand(1, 32, 4096)
     reference_output = reference_model(input)
 
-    tt_input = torch_to_tt_tensor_rm(input, device, put_on_device=False)
+    tt_input = torch2tt_tensor(input, device)
 
     tt_output = tt_model(tt_input)
-    tt_output_torch = tt_to_torch_tensor(tt_output).squeeze(0)
+    tt_output_torch = tt2torch_tensor(tt_output).squeeze(0)
+    # tt_output_torch = tt_to_torch_tensor(tt_output).squeeze(0)
 
     passing, pcc_message = comp_pcc(reference_output, tt_output_torch, pcc)
 
@@ -66,4 +68,4 @@ def test_mistral_feed_forward_inference(pcc, model_location_generator, get_tt_ca
     else:
         logger.warning("Mistral_feed_Forward Failed!")
 
-    assert passing, f"Mistral_Feed_forward output does not meet PCC requirement {pcc}."
+    assert passing, f"Mistral_Feed_forward output does not meet PCC requirement {pcc}: {pcc_message}."
