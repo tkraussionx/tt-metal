c28612331e49f61fede2d7d26b188692996fec8c
Olof Johansson <olofj@tenstorrent.com>
2024-02-12 22:07:13 -0800

    python_env: set pytorch download URL for dev requirements too
    Signed-off-by: Olof Johansson <olofj@tenstorrent.com>

--- tt_metal/python_env/module.mk
+++ tt_metal/python_env/module.mk
@@ -22,6 +22,7 @@ $(PYTHON_ENV)/%: $(PYTHON_ENV)/.installed
 
 $(PYTHON_ENV)/.installed-dev: $(PYTHON_ENV)/.installed $(TT_LIB_LIB_LOCAL_SO) tt_metal/python_env/requirements-dev.txt
 	echo "Installing dev environment packages..."
+	bash -c "source $(PYTHON_ENV)/bin/activate && python3 -m pip config set global.extra-index-url https://download.pytorch.org/whl/cpu"
 	bash -c "source $(PYTHON_ENV)/bin/activate && python -m pip install -r tt_metal/python_env/requirements-dev.txt"
 	echo "Installing editable dev version of tt_eager packages..."
 	bash -c "source $(PYTHON_ENV)/bin/activate && pip install -e ."
