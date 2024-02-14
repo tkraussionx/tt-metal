13a4aa3c3c22ef86ff525982af821ecfc6e480dd
Stuti Raizada <sraizada@tenstorrent.com>
2024-02-14 15:27:14 -0600

    #5337: WIP add attention from llama2 model + sliding window + rolling cache

---
+++ models/experimental/mistral/tt/mistral_common.py
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from typing import Tuple
+import tt_lib
+from models.utility_functions import tt2torch_tensor, torch2tt_tensor
+
+
+def tt_all_reduce(tensors, output_mem_config=None):
+    """
+    reduction on a list of tensors
+    """
+    if len(tensors) == 1:
+        return tensors[0]
+    base_tensor = tensors[0]
+    for tensor in tensors[1:]:
+        # base_tensor = tt_lib.tensor.add(base_tensor, tensor, output_mem_config=output_mem_config)  Cbinding doesnt support this optional argument passed in as None
+        if output_mem_config is not None:
+            base_tensor = tt_lib.tensor.add(base_tensor, tensor, output_mem_config)
+        else:
+            base_tensor = tt_lib.tensor.add(base_tensor, tensor)
+    dev = base_tensor.device()
+    # Emulate replication on all chips
+    res_pt = tt2torch_tensor(base_tensor)
+    res = [torch2tt_tensor(res_pt.clone(), dev) for _ in range(len(tensors))]
+    return res
+
+
+def precompute_freqs(dim: int, end: int, theta: float = 10000.0):
+    """
+    Precompute the frequency tensor for sine and cosine values with given dimensions.
+
+    Args:
+        dim (int): Dimension of the frequency tensor.
+        end (int): End index for precomputing frequencies.
+        theta (float, optional): Scaling factor for frequency computation. Defaults to 10000.0.
+
+    Returns:
+        Tuple[torch.Tensor, torch.Tensor]: Tensors containing cosine and sine values.
+    """
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end)
+    freqs = torch.outer(t, freqs).float()
+    return torch.cos(freqs), torch.sin(freqs)
+
+
+def freqs_to_rotation_matrix(cos_freqs, sin_freqs):
+    """
+    Transform cos/sin frequencies to a rotation matrix.
+    """
+    emb_size, emb_dim = cos_freqs.shape
+    dhead = emb_dim * 2
+    rot_emb_matrix = torch.zeros(emb_size, dhead, dhead)
+    rot_emb_matrix[..., torch.arange(0, dhead, 2), torch.arange(0, dhead, 2)] = cos_freqs.clone()
+    rot_emb_matrix[..., torch.arange(1, dhead, 2), torch.arange(1, dhead, 2)] = cos_freqs.clone()
+    rot_emb_matrix[..., torch.arange(0, dhead, 2), torch.arange(1, dhead, 2)] = -sin_freqs.clone()
+    rot_emb_matrix[..., torch.arange(1, dhead, 2), torch.arange(0, dhead, 2)] = sin_freqs.clone()
+
+    rot_emb_matrix = rot_emb_matrix.transpose(-1, -2)  # Necessary for correct rotation when applied as (x @ R)
+    return rot_emb_matrix
+
+
+def gather_rotary_emb(rot_emb_matrix, position_ids):
+    """
+    Gather the rotary embeddings for a given position_ids
+    """
+    batch_size, seqlen = position_ids.shape
+    emb_size, _, dhead = rot_emb_matrix.shape
+    position_ids = position_ids.view(-1).unsqueeze(-1).unsqueeze(-1).expand(-1, dhead, dhead)
+    rot_emb = rot_emb_matrix.gather(0, position_ids).view(batch_size, seqlen, dhead, dhead)
+    return rot_emb
