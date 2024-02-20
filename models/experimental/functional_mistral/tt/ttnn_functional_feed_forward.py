a86602cdb77fa1b651a6cfb1c7a359134f15f099
Mark O'Connor <moconnor@tenstorrent.com>
2024-02-20 15:59:44 +0000

    #5337: Both work with sharding, 1ms on device bfp8

--- models/experimental/functional_mistral/tt/ttnn_functional_feed_forward.py
+++ models/experimental/functional_mistral/tt/ttnn_functional_feed_forward.py
@@ -3,65 +3,45 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import ttnn
-import tt_lib
-from loguru import logger
 
 
 class MistralMLP:
     def __init__(self, input_shape, parameters, grid=ttnn.CoreGrid(8, 8)):
-        """Set up the configs ahead of time as this takes 30-50us"""
+        """Set up the configs ahead of time as this adds around 1ms of overhead per call."""
         self.w1 = parameters.w1.weight
         self.w2 = parameters.w2.weight
         self.w3 = parameters.w3.weight
         self.grid = grid
 
-        rows = input_shape.with_tile_padding()[-2]
-        shard_shape = ttnn.ShardShape(rows, self.w1.shape.with_tile_padding()[-1] // grid.num_cores)
-        self.shard = ttnn.create_sharded_memory_config(grid, shard_shape, ttnn.ShardStrategy.WIDTH)
-        self.interleave = ttnn.ttl.tensor.MemoryConfig(
-            ttnn.ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttnn.ttl.tensor.BufferType.L1
+        shard_shape = ttnn.ShardShape(
+            input_shape.with_tile_padding()[-2], self.w1.shape.with_tile_padding()[-1] // grid.num_cores
         )
+        self.shard = ttnn.create_sharded_memory_config(grid, shard_shape, ttnn.ShardStrategy.WIDTH)
 
     def __call__(self, x):
-        w1_out = ttnn.matmul(
+        w1_out = ttnn.linear(
             x, self.w1, activation="silu", core_grid=self.grid, use_1d_systolic_array=True, memory_config=self.shard
         )
-        w3_out = ttnn.matmul(x, self.w3, core_grid=self.grid, use_1d_systolic_array=True, memory_config=self.shard)
+        w3_out = ttnn.linear(x, self.w3, core_grid=self.grid, use_1d_systolic_array=True, memory_config=self.shard)
         w2_in = ttnn.mul(w1_out, w3_out, memory_config=self.shard)
-        w2_out = ttnn.matmul(w2_in, self.w2, core_grid=self.grid, use_1d_systolic_array=True, memory_config=self.shard)
+        w2_out = ttnn.linear(
+            w2_in, self.w2, core_grid=self.grid, use_1d_systolic_array=True, memory_config=ttnn.L1_MEMORY_CONFIG
+        )
         return w2_out
 
 
-def feed_forward(config, x, parameters):
+def feed_forward(config, x, parameters, grid=ttnn.CoreGrid(8, 8)):
     """Lower performance but maches functional paradigm"""
-    mlp = MistralMLP(input_shape=x.shape, parameters=parameters)
-    return mlp(x)
+    w1 = parameters.w1.weight
+    w2 = parameters.w2.weight
+    w3 = parameters.w3.weight
 
+    shard_shape = ttnn.ShardShape(x.shape.with_tile_padding()[-2], w1.shape.with_tile_padding()[-1] // grid.num_cores)
+    shard = ttnn.create_sharded_memory_config(grid, shard_shape, ttnn.ShardStrategy.WIDTH)
 
-def matmul_1d_config(m, k, n, grid=ttnn.CoreGrid(8, 8), act=None):
-    tile_width = 32
-    tile_height = 32
+    w1_out = ttnn.linear(x, w1, activation="silu", core_grid=grid, use_1d_systolic_array=True, memory_config=shard)
+    w3_out = ttnn.linear(x, w3, core_grid=grid, use_1d_systolic_array=True, memory_config=shard)
+    w2_in = ttnn.mul(w1_out, w3_out, memory_config=shard)
+    w2_out = ttnn.linear(w2_in, w2, core_grid=grid, use_1d_systolic_array=True, memory_config=ttnn.L1_MEMORY_CONFIG)
 
-    per_core_m = m // tile_height
-    per_core_k = k // tile_width // grid.num_cores
-    per_core_n = n // tile_width // grid.num_cores
-
-    # find the largest value between 1 and 8 that is a factor of per_core_n
-    # e.g. if per_core_n is 14, then out_subblock_w = 7
-    out_subblock_w = max([i for i in range(1, 9) if per_core_n % i == 0])
-
-    # find the largest value that is a factor of per_core_m such that
-    # out_subblock_w * out_subblock_h <= 8
-    out_subblock_h = max([i for i in range(1, 9) if per_core_m % i == 0 and i * out_subblock_w <= 8])
-
-    return ttnn.ttl.operations.primary.MatmulMultiCoreReuseMultiCast1DProgramConfig(
-        compute_with_storage_grid_size=tt_lib.tensor.CoreCoord(grid.x, grid.y),
-        in0_block_w=per_core_k,
-        out_subblock_h=out_subblock_h,
-        out_subblock_w=out_subblock_w,
-        per_core_M=per_core_m,
-        per_core_N=per_core_n,
-        fuse_batch=True,
-        fused_activation=act,
-        mcast_in0=True,
-    )
+    return w2_out
