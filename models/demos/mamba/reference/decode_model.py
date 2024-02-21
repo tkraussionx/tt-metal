e401a742ad9b616af58473f5005b1f9fae3ce877
Evan Smal <esmal@tenstorrent.com>
2024-02-21 12:27:46 +0000

    Fix some typing issues

--- models/demos/mamba/reference/decode_model.py
+++ models/demos/mamba/reference/decode_model.py
@@ -28,29 +28,7 @@ import torch.nn.functional as F
 from dataclasses import dataclass
 from einops import rearrange, repeat, einsum
 
-
-@dataclass
-class ModelArgs:
-    d_model: int
-    n_layer: int
-    vocab_size: int
-    d_state: int = 16
-    expand: int = 2
-    dt_rank: Union[int, str] = "auto"
-    d_conv: int = 4
-    pad_vocab_size_multiple: int = 8
-    conv_bias: bool = True
-    bias: bool = False
-    batch_size: int = 1
-
-    def __post_init__(self):
-        self.d_inner = int(self.expand * self.d_model)
-
-        if self.dt_rank == "auto":
-            self.dt_rank = math.ceil(self.d_model / 16)
-
-        if self.vocab_size % self.pad_vocab_size_multiple != 0:
-            self.vocab_size += self.pad_vocab_size_multiple - self.vocab_size % self.pad_vocab_size_multiple
+from .args import ModelArgs
 
 
 class MambaDecode(nn.Module):
