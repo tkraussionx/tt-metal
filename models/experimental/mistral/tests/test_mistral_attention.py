#5337: Moved prepare_inputs to mistral_common. Also include small fix to cos/sin calculation

models/experimental/mistral/tests/test_mistral_attention.py
@@ -8,7 +8,7 @@ from loguru import logger
 import json
 
 from models.experimental.mistral.tt.mistral_attention import TtMistralAttention
-from models.experimental.mistral.tt.mistral_common import precompute_freqs, generate_cos_sin_cache
+from models.experimental.mistral.tt.mistral_common import precompute_freqs, generate_cos_sin_cache, prepare_inputs
 from models.experimental.mistral.tt.model_config import TtModelArgs, get_model_config
 from models.experimental.mistral.reference.model import Attention
 from models.utility_functions import torch_to_tt_tensor_rm, tt2torch_tensor
@@ -48,7 +48,7 @@ from models.utility_functions import (
 )
 @pytest.mark.parametrize(
     "iterations",
-    ((1),),
+    ((3),),
 )
 @pytest.mark.parametrize(
     "pcc",
@@ -87,7 +87,7 @@ def test_mistral_attention_inference(
         devices,
         state_dict,
         base_url=base_address,
-        layer_num=0,
+        layer_num=None,
         model_config=get_model_config(model_config),
         configuration=model_args,
     )
@@ -99,7 +99,7 @@ def test_mistral_attention_inference(
     freqs_cis = torch.complex(cos, sin)
 
     tt_model.tt_cos_cached, tt_model.tt_sin_cached = generate_cos_sin_cache(
-        devices, model_args.head_dim, "", model_args.max_seq_len * 2, 1000, tt_model.model_config
+        devices, model_args.head_dim, "", model_args.max_seq_len * 2, 10000, tt_model.model_config
     )
 
     # TODO Update start_pos (check llama test for reference)
@@ -107,14 +107,20 @@ def test_mistral_attention_inference(
         pt_attention_input = (torch.rand(batch, seq_len, model_args.dim) * 2) - 1
         tt_attention_input = pt_attention_input.clone()
         start_pos = generation_start_pos + i
-        attention_input, start_pos, attn_mask = tt_model.prepare_inputs(
+        attention_input, start_pos, attn_mask, current_pos = prepare_inputs(
             tt_attention_input,
             start_pos,
+            tt_model.hidden_size,
+            tt_model.n_local_heads,
+            tt_model.sliding_window,
+            tt_model.devices,
+            tt_model.num_devices,
         )
 
         tt_out = tt_model(
             attention_input,
             start_pos,
+            current_pos,
             attn_mask,
         )
         assert isinstance(tt_out, list)  # tt_out should be replicated on N devices

models/experimental/mistral/tests/test_mistral_decoder.py
@@ -7,7 +7,7 @@ import pytest
 from loguru import logger
 import json
 from pathlib import Path
-from models.experimental.mistral.tt.mistral_common import precompute_freqs, generate_cos_sin_cache
+from models.experimental.mistral.tt.mistral_common import precompute_freqs, generate_cos_sin_cache, prepare_inputs
 from models.experimental.mistral.tt.mistral_decoder import TtTransformerBlock
 from models.experimental.mistral.tt.model_config import TtModelArgs, get_model_config
 from models.experimental.mistral.reference.model import TransformerBlock
@@ -88,8 +88,9 @@ def test_mistral_decoder_inference(pcc, model_config, model_location_generator, 
     cos, sin = precompute_freqs(model_args.head_dim, model_args.max_seq_len * 2)
     freqs_cis = torch.complex(cos, sin)
 
-    tt_model.tt_cos_cached, tt_model.tt_sin_cached = generate_cos_sin_cache(
-        devices, model_args.head_dim, "", model_args.max_seq_len * 2, 1000, tt_model.model_config
+    # Generate attn cos/sin cache
+    tt_model.attention.tt_cos_cached, tt_model.attention.tt_sin_cached = generate_cos_sin_cache(
+        devices, model_args.head_dim, "", model_args.max_seq_len * 2, 10000, tt_model.model_config
     )
 
     # TODO Update start_pos (check llama test for reference)
@@ -101,9 +102,17 @@ def test_mistral_decoder_inference(pcc, model_config, model_location_generator, 
         tt_decode_input = pt_decode_input.clone()
         start_pos = generation_start_pos + i
 
-        decode_input, start_pos, attn_mask = tt_model.prepare_inputs(tt_decode_input, start_pos)
+        decode_input, start_pos, attn_mask, current_pos = prepare_inputs(
+            tt_decode_input,
+            start_pos,
+            tt_model.hidden_size,
+            tt_model.n_local_heads,
+            tt_model.sliding_window,
+            tt_model.devices,
+            tt_model.num_devices,
+        )
         # Run TT model
-        tt_out = tt_model(decode_input, start_pos, attn_mask)
+        tt_out = tt_model(decode_input, start_pos, current_pos, attn_mask)
         # tt_output = tt_model(tt_input, bcast_freq_xq, bcast_freq_xk, tt_position, mask, seqlen)
 
         tt_output_torch = tt2torch_tensor(tt_out).permute(2, 1, 0, 3).squeeze(1)  # [seq, batch, hidden_dim]

models/experimental/mistral/tt/mistral_attention.py
@@ -37,7 +37,7 @@ class TtMistralAttention(nn.Module):
 
         self.model_config = model_config
 
-        self.current = 0
+        # self.current = 0
 
         if layer_num:
             layer_name = f"{base_url}.{layer_num}.attention."
@@ -120,55 +120,11 @@ class TtMistralAttention(nn.Module):
         self.tt_sin_cached = None
         self.tt_cos_cached = None
 
-    def prepare_inputs(self, x, start_pos):
-        """
-        Prepare inputs for decode mode. Assume that current token is at
-        start_pos, and KV cache has valid data up to start_pos.
-        x: (batch, seq, hidden_dim)
-        start_pos: int
-        """
-        assert x.size(2) == self.hidden_size
-        assert len(x.size()) == 3
-
-        batch = x.size(0)
-        seq_len = x.size(1)
-        assert seq_len == 1, "Only supporting decode mode"
-        x = x.transpose(0, 1).unsqueeze(1)  # [seq_len, 1, batch, hidden_dim]
-
-        padded_layer_past_len = min(nearest_32(start_pos + 1), self.sliding_window)
-        self.current = start_pos % self.sliding_window
-        attn_mask = torch.zeros(seq_len, 1, batch, padded_layer_past_len)
-
-        if start_pos < self.sliding_window:
-            attn_mask[:, :, :, self.current + 1 :] = torch.finfo(attn_mask.dtype).min
-        else:
-            attn_mask[:, :, :, : self.current] = torch.finfo(attn_mask.dtype).min
-            attn_mask[:, :, :, self.sliding_window - self.current :] = torch.finfo(attn_mask.dtype).min
-        attn_mask = attn_mask.expand(-1, self.n_local_heads, -1, -1)
-
-        # expected shapes:
-        # x: (seq_len, 1, batch, hidden_dim)
-        # start_pos: int
-        # attn_mask: [seq_len, n_heads, batch, padded_layer_past_len]
-        assert x.size() == (seq_len, 1, batch, self.hidden_size)
-        assert attn_mask.size() == (seq_len, self.n_local_heads, batch, padded_layer_past_len)
-
-        xs, attn_masks = [], []
-        for i in range(self.num_devices):
-            device = self.devices[i]
-            xs.append(torch2tt_tensor(x.clone(), device))
-            attn_masks.append(torch2tt_tensor(attn_mask.clone(), device))
-
-        return (
-            xs,
-            start_pos,
-            attn_masks,
-        )
-
     def forward(
         self,
         xs: tt_lib.tensor.Tensor,
         start_pos: int,
+        current_pos: int,
         attn_masks: tt_lib.tensor.Tensor,
     ) -> tt_lib.tensor.Tensor:
         """
@@ -233,8 +189,8 @@ class TtMistralAttention(nn.Module):
             # v_heads [seqlen, n_kv_heads, bsz, head_dim]
             # keys, [max_batch_size, n_kv_heads // self.num_devices, sliding_window, head_dim]
 
-            tt_lib.tensor.update_cache(keys, k_heads, self.current)
-            tt_lib.tensor.update_cache(values, v_heads, self.current)
+            tt_lib.tensor.update_cache(keys, k_heads, current_pos)  # self.current)
+            tt_lib.tensor.update_cache(values, v_heads, current_pos)  # self.current)
 
             k_heads.deallocate()
             v_heads.deallocate()

models/experimental/mistral/tt/mistral_common.py
@@ -5,7 +5,11 @@
 import torch
 from typing import Tuple
 import tt_lib
-from models.utility_functions import tt2torch_tensor, torch2tt_tensor
+from models.utility_functions import (
+    tt2torch_tensor,
+    torch2tt_tensor,
+    nearest_32,
+)
 
 
 def tt_all_reduce(tensors, output_mem_config=None):
@@ -103,12 +107,48 @@ def freqs_to_rotation_matrix(cos_freqs, sin_freqs):
     return rot_emb_matrix
 
 
-def gather_rotary_emb(rot_emb_matrix, position_ids):
+def prepare_inputs(x, start_pos, hidden_size, n_local_heads, sliding_window, devices, num_devices):
     """
-    Gather the rotary embeddings for a given position_ids
+    Prepare inputs for decode mode. Assume that current token is at
+    start_pos, and KV cache has valid data up to start_pos.
+    x: (batch, seq, hidden_dim)
+    start_pos: int
     """
-    batch_size, seqlen = position_ids.shape
-    emb_size, _, dhead = rot_emb_matrix.shape
-    position_ids = position_ids.view(-1).unsqueeze(-1).unsqueeze(-1).expand(-1, dhead, dhead)
-    rot_emb = rot_emb_matrix.gather(0, position_ids).view(seqlen, batch_size, dhead, dhead)
-    return rot_emb
+    assert x.size(2) == hidden_size
+    assert len(x.size()) == 3
+
+    batch = x.size(0)
+    seq_len = x.size(1)
+    assert seq_len == 1, "Only supporting decode mode"
+    x = x.transpose(0, 1).unsqueeze(1)  # [seq_len, 1, batch, hidden_dim]
+
+    padded_layer_past_len = min(nearest_32(start_pos + 1), sliding_window)
+    current = start_pos % sliding_window
+    attn_mask = torch.zeros(seq_len, 1, batch, padded_layer_past_len)
+
+    if start_pos < sliding_window:
+        attn_mask[:, :, :, current + 1 :] = torch.finfo(attn_mask.dtype).min
+    else:
+        attn_mask[:, :, :, :current] = torch.finfo(attn_mask.dtype).min
+        attn_mask[:, :, :, sliding_window - current :] = torch.finfo(attn_mask.dtype).min
+    attn_mask = attn_mask.expand(-1, n_local_heads, -1, -1)
+
+    # expected shapes:
+    # x: (seq_len, 1, batch, hidden_dim)
+    # start_pos: int
+    # attn_mask: [seq_len, n_heads, batch, padded_layer_past_len]
+    assert x.size() == (seq_len, 1, batch, hidden_size)
+    assert attn_mask.size() == (seq_len, n_local_heads, batch, padded_layer_past_len)
+
+    xs, attn_masks = [], []
+    for i in range(num_devices):
+        device = devices[i]
+        xs.append(torch2tt_tensor(x.clone(), device))
+        attn_masks.append(torch2tt_tensor(attn_mask.clone(), device))
+
+    return (
+        xs,
+        start_pos,
+        attn_masks,
+        current,
+    )

models/experimental/mistral/tt/mistral_decoder.py
@@ -16,9 +16,9 @@ from models.utility_functions import (
 
 from models.experimental.mistral.tt.mistral_common import (
     precompute_freqs as tt_precompute_freqs,
-    freqs_to_rotation_matrix,
-    gather_rotary_emb as tt_gather_rotary_emb,
-    tt_all_reduce,
+    # freqs_to_rotation_matrix,
+    # gather_rotary_emb as tt_gather_rotary_emb,
+    # tt_all_reduce,
 )
 
 
@@ -84,73 +84,12 @@ class TtTransformerBlock(nn.Module):
             model_config=model_config,
         )
 
-    # TODO function taken from mistral_attention.py. Move to mistral_common.py
-    def get_rotation_mat(self, cos, sin, start_pos, seqlen, batch):
-        # cos, sin = tt_precompute_freqs(dhead, end)
-        rot_mat = freqs_to_rotation_matrix(cos, sin)
-        position_ids = torch.ones(batch, seqlen, dtype=torch.long) * start_pos
-        rot_emb = tt_gather_rotary_emb(rot_mat, position_ids)
-        return rot_emb
-
-    # TODO function taken from mistral_attention.py. Move to mistral_common.py
-    def prepare_inputs(self, x, start_pos, cos, sin):
-        """
-        Prepare inputs for decode mode. Assume that current token is at
-        start_pos, and KV cache has valid data up to start_pos.
-        x: (batch, seq, hidden_dim)
-        start_pos: int
-        """
-        assert x.size(2) == self.hidden_size
-        assert len(x.size()) == 3
-
-        batch = x.size(0)
-        seq_len = x.size(1)
-        assert seq_len == 1, "Only supporting decode mode"
-        x = x.transpose(0, 1).unsqueeze(1)  # [seq_len, 1, batch, hidden_dim]
-        rot_mat = self.get_rotation_mat(cos, sin, start_pos=start_pos, seqlen=seq_len, batch=batch)
-
-        padded_layer_past_len = min(nearest_32(start_pos + 1), self.sliding_window)
-        self.current = self.current % self.sliding_window
-        attn_mask = torch.zeros(seq_len, 1, batch, padded_layer_past_len)
-
-        if start_pos < self.sliding_window:
-            attn_mask[:, :, :, self.current + 1 :] = torch.finfo(attn_mask.dtype).min
-        else:
-            attn_mask[:, :, :, : self.current] = torch.finfo(attn_mask.dtype).min
-            attn_mask[:, :, :, self.sliding_window - self.current :] = torch.finfo(attn_mask.dtype).min
-        attn_mask = attn_mask.expand(-1, self.n_local_heads, -1, -1)
-
-        # TODO: mask out >sliding_window prev tokens
-
-        # expected shapes:
-        # x: (seq_len, 1, batch, hidden_dim)
-        # start_pos: int
-        # rot_mat: [1, bsz, head_dim, head_dim]
-        # attn_mask: [seq_len, n_heads, batch, padded_layer_past_len]
-        assert x.size() == (seq_len, 1, batch, self.hidden_size)
-        assert rot_mat.size() == (1, batch, self.head_dim, self.head_dim)
-        assert attn_mask.size() == (seq_len, self.n_local_heads, batch, padded_layer_past_len)
-
-        xs, rot_mats, attn_masks = [], [], []
-        for i in range(self.num_devices):
-            device = self.devices[i]
-            xs.append(torch2tt_tensor(x.clone(), device))
-            rot_mats.append(torch2tt_tensor(rot_mat.clone(), device))
-            attn_masks.append(torch2tt_tensor(attn_mask.clone(), device))
-
-        return (
-            xs,
-            start_pos,
-            rot_mats,
-            attn_masks,
-        )
-
     def forward(
         self,
         # x: tt_lib.tensor.Tensor,
         xs: tt_lib.tensor.Tensor,
-        rot_mats: tt_lib.tensor.Tensor,
         start_pos: int,
+        current_pos: int,
         attn_masks: Optional[tt_lib.tensor.Tensor],
         # bcast_freq_xq: tt_lib.tensor.complex_tensor,
         # bcast_freq_xk: tt_lib.tensor.complex_tensor,
@@ -162,8 +101,8 @@ class TtTransformerBlock(nn.Module):
         attn_norm = [self.attention_norm(xs[0])]
         r = self.attention.forward(
             attn_norm,
-            rot_mats,
             start_pos,
+            current_pos,
             attn_masks,
         )
         # Attn takes a list of inputs (assuming multiple devices) and returns multiple outputs