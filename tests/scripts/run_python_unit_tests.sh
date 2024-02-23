7fa449585a63a605795ed3662fe81e63dfdcfffa
Michael Chiou <mchiou@tenstorrent.com>
2024-02-23 07:15:37 +0000

    #4697: Fix issue with export PYTHONPATH in workflows

--- tests/scripts/run_python_unit_tests.sh
+++ tests/scripts/run_python_unit_tests.sh
@@ -3,8 +3,8 @@
 set -eo pipefail
 
 if [[ -z "$TT_METAL_HOME" ]]; then
-  echo "Must provide TT_METAL_HOME in environment" 1>&2
-  exit 1
+    echo "Must provide TT_METAL_HOME in environment" 1>&2
+    exit 1
 fi
 
 if [[ ! -z "$TT_METAL_SLOW_DISPATCH_MODE" ]]; then
