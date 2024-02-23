f192a272f6c3b0da16ed66e1dc2794d17c97de14
Michael Chiou <mchiou@tenstorrent.com>
2024-02-23 01:07:40 +0000

    #4697: Add tests and check of env variables for test scripts

--- tests/scripts/run_python_model_tests.sh
+++ tests/scripts/run_python_model_tests.sh
@@ -7,6 +7,19 @@ if [[ -z "$TT_METAL_HOME" ]]; then
   exit 1
 fi
 
+if [[ -z "$ARCH_NAME" ]]; then
+  echo "Must provide ARCH_NAME in environment" 1>&2
+  exit 1
+fi
+
+if [[ ! -z "$TT_METAL_SLOW_DISPATCH_MODE" ]]; then
+  echo "Only Fast Dispatch mode allowed - Must have TT_METAL_SLOW_DISPATCH_MODE unset" 1>&2
+  exit 1
+fi
+
+cd $TT_METAL_HOME
+export PYTHONPATH=$TT_METAL_HOME
+
 
 # This must run in slow dispatch mode
 # pytest -svv $TT_METAL_HOME/tests/python_api_testing/sweep_tests/pytests/test_sweep_conv_with_address_map.py
--- tests/scripts/run_python_unit_tests.sh
+++ tests/scripts/run_python_unit_tests.sh
@@ -7,6 +7,18 @@ if [[ -z "$TT_METAL_HOME" ]]; then
   exit 1
 fi
 
+if [[ -z "$ARCH_NAME" ]]; then
+  echo "Must provide ARCH_NAME in environment" 1>&2
+  exit 1
+fi
+
+if [[ ! -z "$TT_METAL_SLOW_DISPATCH_MODE" ]]; then
+  echo "Only Fast Dispatch mode allowed - Must have TT_METAL_SLOW_DISPATCH_MODE unset" 1>&2
+  exit 1
+fi
+
+cd $TT_METAL_HOME
+export PYTHONPATH=$TT_METAL_HOME
 
 if [[ ! -z "$TT_METAL_SLOW_DISPATCH_MODE" ]]; then
     D0="$(date +%s)"
