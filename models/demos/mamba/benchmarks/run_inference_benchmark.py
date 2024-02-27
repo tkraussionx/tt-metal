a3f11626c3ce58da27aede2cab8c19530fd285fa
Evan Smal <esmal@tenstorrent.com>
2024-02-27 09:53:32 -0500

    Refactor inference benchmark

--- models/demos/mamba/benchmarks/run_inference_benchmark.py
+++ models/demos/mamba/benchmarks/run_inference_benchmark.py
@@ -6,6 +6,8 @@ import torch
 
 from transformers import AutoTokenizer
 
+MODEL_VERSION = "state-spaces/mamba-370m"
+
 
 @dataclass
 class InferenceBenchmarkResult:
@@ -14,68 +16,77 @@ class InferenceBenchmarkResult:
     sequence: str
 
 
-MODEL_VERSION = "state-spaces/mamba-370m"
+class MambaDecodeWrapper(torch.nn.Module):
+
+    @dataclass
+    class Result:
+        logits: torch.Tensor
+
+    def __init__(self, model_version):
+        super().__init__()
+
+        from models.demos.mamba.reference.decode_model import MambaDecode
+
+        self.decode = MambaDecode.from_pretrained(model_version)
+
+    def forward(self, inputs):
+        x = inputs.squeeze(0)
+        return MambaDecodeWrapper.Result(self.decode(x).unsqueeze(1))
 
 
 def create_model(model_type: str):
     if model_type == "cpu":
-
-        from models.demos.mamba.reference.decode_model import MambaDecode
-
-        model = MambaDecode.from_pretrained(MODEL_VERSION)
-        model.eval()
-
-        def generate_cpu(inputs):
-            with torch.no_grad():
-                next_token_logits = model(inputs)
-                return next_token_logits
-
-        return generate_cpu
-
+        model = MambaDecodeWrapper(MODEL_VERSION)
+        device = "cpu"
     elif model_type == "gpu":
-
         from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
 
         model = MambaLMHeadModel.from_pretrained(MODEL_VERSION, device="cuda")
-        model.eval()
-
-        def generate_gpu(inputs):
-            with torch.no_grad():
-                inputs = inputs.unsqueeze(1).to(device="cuda")
-                next_token_logits = model(inputs).logits
-                return next_token_logits.squeeze(1)
-
-        return generate_gpu
-
+        device = "cuda"
     else:
         raise RuntimeError(f"Invalid model type: {model_type}")
+
+    return model, device
 
 
 def run_inference_benchmark(model_type: str, prompt: str = "Mamba is the", sequence_length: int = 64):
 
-    torch.manual_seed(0)
+    torch.random.manual_seed(0)
 
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
-    model = create_model(model_type)
+    model, device = create_model(model_type)
 
-    sequence = tokenizer(prompt, return_tensors="pt").input_ids
-    tokens_in_prompt = len(sequence[0])
+    sequence = tokenizer(prompt, return_tensors="pt").input_ids.to(device=device).split(1, dim=1)
+    tokens_in_prompt = len(sequence)
 
-    for idx in range(tokens_in_prompt - 1):
-        model(sequence[:, idx])
+    @torch.inference_mode()
+    def decode(prompt: list[torch.Tensor]) -> list[torch.Tensor]:
+        for idx in range(len(prompt) - 1):
+            model(prompt[idx])
 
-    start = time.time()
-    for idx in range(tokens_in_prompt - 1, tokens_in_prompt + sequence_length):
-        logits = model(sequence[:, idx])
-        probs = torch.nn.functional.softmax(logits, dim=-1)
-        next_token = torch.argmax(probs, dim=-1).unsqueeze(-1)
-        sequence = torch.cat([sequence, next_token.to(device="cpu")], dim=1)
-    end = time.time()
+        sequence = [*prompt]
+        for idx in range(sequence_length):
+            logits = model(sequence[-1]).logits
+            probs = torch.nn.functional.softmax(logits, dim=-1)
+            next_token = torch.argmax(probs, dim=-1)
+            sequence.append(next_token)
+        return sequence
+
+    print("Warming up...")
+    decode(sequence)
+    print("Done warming up")
+
+    start = time.process_time()
+    result = decode(sequence)
+    end = time.process_time()
+
+    result = torch.cat(result, dim=1)
+    assert result.shape[1] == tokens_in_prompt + sequence_length
 
     return InferenceBenchmarkResult(
-        total_time=end - start,
-        tokens_per_s=float(sequence_length) / (end - start),
-        sequence=[tokenizer.decode(output.tolist()) for output in sequence][0],
+        total_time=1000.0 * (end - start),
+        tokens_per_s=float(tokens_in_prompt + sequence_length) / (end - start),
+        sequence=tokenizer.batch_decode(result),
     )
 
 
