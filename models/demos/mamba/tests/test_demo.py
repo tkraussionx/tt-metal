068bd0786970543cf0f8d83a60acb3f9b6efc566
Evan Smal <esmal@tenstorrent.com>
2024-02-28 15:00:46 +0000

    Add tests for demo code

---
+++ models/demos/mamba/tests/test_demo.py
@@ -0,0 +1,25 @@
+import torch
+import pytest
+from loguru import logger
+
+from transformers import AutoTokenizer
+
+from models.demos.mamba.demo.demo import run_demo
+from models.demos.mamba.reference.decode_model import MambaPretrainedModelName
+
+
+@pytest.mark.parametrize(
+    "model_version, genlen",
+    (("state-spaces/mamba-130m", 1),),
+)
+def test_cpu_reference_model_decode_vs_selective_scan(
+    model_version: MambaPretrainedModelName,
+    genlen: int,
+):
+    prompt = ["Hello"]
+
+    res1 = run_demo(prompt, "cpu", generated_sequence_length=genlen, model_version=model_version, display=False)
+    assert len(res1) == genlen + 1
+
+    # res2 = run_demo(prompt, "wh", generated_sequence_length=genlen, model_version=model_version, display=False)
+    # assert res1 == res2, "Model outputs should match"
