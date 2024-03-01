1d1a3f7060d4819f4827904949192dfe5dd62986
Nenad Petrovic <npetrovic@tenstorrent.com>
2024-03-01 13:20:33 +0000

    #5589: Add tests for exp2 and efinv

--- tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_exp2.py
+++ tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_exp2.py
@@ -7,14 +7,13 @@ import random
 import pytest
 import torch
 import ttnn
-
+import traceback
 
 from tests.ttnn.utils_for_testing import assert_with_pcc
-
 from tests.ttnn.python_api_testing.sweep_tests import ttnn_ops
 
 
-def run_exp2_tests(
+def run_eltwise_exp2_tests(
     input_shape,
     dtype,
     dlayout,
@@ -24,13 +23,13 @@ def run_exp2_tests(
     device,
 ):
     torch.manual_seed(data_seed)
-
-    x = torch.Tensor(size=input_shape).uniform_(-100, 100).to(torch.bfloat16)
+    x = torch.Tensor(size=input_shape[0]).uniform_(-10, 10).to(torch.bfloat16)
 
     try:
         # get ref result
         ref_value = torch.exp2(x)
-        x = ttnn_ops.torch_to_ttnn(x, device, dlayout[0], in_mem_config, dtype[0])
+
+        x = ttnn_ops.setup_ttnn_tensor(x, device, dlayout[0], in_mem_config[0], dtype[0])
 
         tt_result = ttnn.exp2(x)
         tt_result = ttnn_ops.ttnn_tensor_to_torch(tt_result, output_mem_config)
@@ -47,28 +46,20 @@ def run_exp2_tests(
 
 test_sweep_args = [
     (
-        (3, 12, 192, 224),
-        [ttnn.bfloat16],
+        [(3, 2, 192, 32)],
+        [ttnn.bfloat8_b],
         [ttnn.TILE_LAYOUT],
-        (ttnn.DRAM_MEMORY_CONFIG),
-        (ttnn.DRAM_MEMORY_CONFIG),
-        15785057,
+        [ttnn.DRAM_MEMORY_CONFIG],
+        ttnn.DRAM_MEMORY_CONFIG,
+        11079580,
     ),
     (
-        (192, 64),
-        [ttnn.bfloat16],
+        [(12, 224, 224)],
+        [ttnn.bfloat8_b],
         [ttnn.TILE_LAYOUT],
-        (ttnn.DRAM_MEMORY_CONFIG),
-        (ttnn.DRAM_MEMORY_CONFIG),
-        4157231,
-    ),
-    (
-        (6, 224, 64),
-        [ttnn.bfloat16],
-        [ttnn.TILE_LAYOUT],
-        (ttnn.DRAM_MEMORY_CONFIG),
-        (ttnn.DRAM_MEMORY_CONFIG),
-        11158061,
+        [ttnn.DRAM_MEMORY_CONFIG],
+        ttnn.L1_MEMORY_CONFIG,
+        6411147,
     ),
 ]
 
@@ -77,5 +68,5 @@ test_sweep_args = [
     "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed",
     (test_sweep_args),
 )
-def test_exp2(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
-    run_exp2_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
+def test_eltwise_exp2(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    run_eltwise_exp2_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
