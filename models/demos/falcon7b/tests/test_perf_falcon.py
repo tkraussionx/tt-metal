829568a860195bf61908eddc79ba3c73be68b416
Salar <skhorasgani@tenstorrent.com>
2024-03-05 17:56:06 +0000

    #0: [Falcon7b experimental] Add alpacaeval dataloading to test gelu pcc
    Signed-off-by: Salar <skhorasgani@tenstorrent.com>

--- models/demos/falcon7b/tests/test_perf_falcon.py
+++ models/demos/falcon7b/tests/test_perf_falcon.py
@@ -40,6 +40,363 @@ from models.utility_functions import (
 )
 from models.perf.perf_utils import prep_perf_report
 
+from transformers import AutoTokenizer
+import transformers
+from datasets import load_dataset, load_from_disk
+from langdetect import detect, LangDetectException
+import os
+import urllib.request
+from typing import Dict, Sequence
+import copy
+from dataclasses import dataclass
+from tqdm import tqdm
+
+
+def set_random_seed(seed):
+    torch.use_deterministic_algorithms(True)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    if torch.cuda.is_available():
+        torch.backends.cudnn.deterministic = True
+        torch.cuda.manual_seed(seed)
+        os.environ[
+            "CUBLAS_WORKSPACE_CONFIG"
+        ] = ":4096:8"  # CuBLAS and CUDA >= 10.2. require this to use deterministic behaviour, see https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+
+
+def get_tokenizer(model_name, explicit_pad_token=False, hf_cache=None):
+    # Get tokenizer from HF
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=hf_cache)
+    if explicit_pad_token:
+        num_new_tokens = tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+        print(f"Num new tokens added : {num_new_tokens}")
+    else:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    tokenizer.bos_token = tokenizer.eos_token
+    return tokenizer
+
+
+def extract_alpaca_dataset(example):
+    ALPACA_PROMPT_DICT = {
+        "prompt_input": (
+            "Below is an instruction that describes a task, paired with an input that provides further context. "
+            "Write a response that appropriately completes the request.\n\n"
+            "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response: "
+        ),
+        "prompt_no_input": (
+            "Below is an instruction that describes a task. "
+            "Write a response that appropriately completes the request.\n\n"
+            "### Instruction:\n{instruction}\n\n### Response: "
+        ),
+    }
+    if example.get("input", "") != "":
+        prompt_format = ALPACA_PROMPT_DICT["prompt_input"]
+    else:
+        prompt_format = ALPACA_PROMPT_DICT["prompt_no_input"]
+    return {"input": prompt_format.format(**example)}
+
+
+def get_dataset(dataset_name, cached_dataset_dir=None, hf_cache=None):
+    if dataset_name == "alpaca":
+        dataset = load_dataset("tatsu-lab/alpaca", cache_dir=hf_cache)
+    elif dataset_name == "alpaca_eval":
+        dataset = load_dataset("tatsu-lab/alpaca_eval", download_mode="force_redownload", cache_dir=hf_cache)
+    elif dataset_name == "guanaco":
+        dataset = load_dataset("timdettmers/openassistant-guanaco", cache_dir=hf_cache)
+    elif dataset_name == "guanaco_en_sp_fr":
+        ds_dir = os.path.join(cached_dataset_dir, "openassistant_guanaco_en_sp_fr")
+        if os.path.exists(ds_dir):
+            print(f"Loading prefiltered dataset from {ds_dir}")
+            dataset = load_from_disk(ds_dir)
+            print(f'Train: {len(dataset["train"])} samples')
+            print(f'Test : {len(dataset["test"])} samples')
+        else:
+            print(f"Filtered guanaco_en_sp_fr dataset doesn't exist")
+            dataset = load_dataset("timdettmers/openassistant-guanaco", cache_dir=hf_cache)
+            languages = ["en", "es", "fr"]
+
+            def check_lang(example):
+                try:
+                    return detect(example["text"]) in languages
+                except LangDetectException:
+                    return False
+
+            orig_len_train = len(dataset["train"])
+            orig_len_test = len(dataset["test"])
+            dataset = dataset.filter(check_lang)
+            print(f'Train: filtered {orig_len_train} -> {len(dataset["train"])} samples for languages: {languages}')
+            print(f'Test : filtered {orig_len_test} -> {len(dataset["test"])} samples for languages: {languages}')
+            dataset.save_to_disk(ds_dir)
+            print(f"Saved prefiltered dataset to: {ds_dir}")
+
+    elif dataset_name == "mmlu_eval":
+        # Create the directory if it doesn't exist
+        os.makedirs("mmlu", exist_ok=True)
+        data_files = {"eval": "mmlu/five_shot_mmlu_val.json", "test": "mmlu/five_shot_mmlu_test.json"}
+        url_map = {
+            "eval": "https://raw.githubusercontent.com/artidoro/qlora/main/data/mmlu/five_shot_mmlu_val.json",
+            "test": "https://raw.githubusercontent.com/artidoro/qlora/main/data/mmlu/five_shot_mmlu_test.json",
+        }
+
+        # Check and download JSON files if they don't exist
+        for split, file_name in data_files.items():
+            if not os.path.exists(file_name):
+                print(f"'{file_name}' doesn't exist. Downloading...")
+                try:
+                    urllib.request.urlretrieve(url_map[split], file_name)
+                    print(f"'{file_name}' downloaded successfully.")
+                except Exception as e:
+                    print(f"Error downloading '{file_name}': {e}")
+        dataset = load_dataset("json", data_files=data_files, cache_dir=hf_cache)
+    else:
+        raise NotImplementedError(f"Dataset {dataset_name} not implemented yet.")
+
+    return dataset
+
+
+def format_alpaca_eval_dataset(example):
+    prompt_format = "A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.\n### Human: {instruction}\n### Assistant:"
+    return {"input": prompt_format.format(**example)}
+
+
+def format_dataset(dataset, dataset_name):
+    if dataset_name == "alpaca":
+        dataset = dataset.map(extract_alpaca_dataset, remove_columns=["instruction"])
+        dataset = dataset.remove_columns(
+            [col for col in dataset.column_names["train"] if col not in ["input", "output"]]
+        )
+
+    elif dataset_name == "alpaca_eval":
+        dataset = dataset.map(format_alpaca_eval_dataset)
+
+    elif dataset_name == "guanaco" or dataset_name == "guanaco_en_sp_fr":
+        dataset = dataset.map(
+            lambda x: {
+                "input": "",
+                "output": x["text"],
+            }
+        )
+        dataset = dataset.remove_columns(
+            [col for col in dataset.column_names["train"] if col not in ["input", "output"]]
+        )
+
+    elif dataset_name == "mmlu_eval":
+        # leave as is
+        dataset = dataset.remove_columns(
+            [col for col in dataset.column_names["eval"] if col not in ["input", "output"]]
+        )
+    # Remove unused columns.
+
+    return dataset
+
+
+def preprocess_dataset(
+    dataset,
+    dataset_name,
+    tokenizer,
+    filter_longer_sequences=True,
+    filter_longer_inputs=False,
+    max_input_len=256,
+    max_seq_len=512,
+):
+    def append_eos(text, dataset_name):
+        if dataset_name == "guanaco" or dataset_name == "guanaco_en_sp_fr":
+            """
+            Add the eos_token after each response, so during inference the model can stop generating tokens after it completes its response. For example
+            ### HUMAN:
+            Hello
+            ### Assistant:
+            Hi, how are you?<eos_token>
+            ### HUMAN:
+            I'm fine.
+            ### Assistant:
+            How can I help you?<eos_token>
+            """
+
+            splitted_text = text.split("### Human:")
+            for i in range(1, len(splitted_text)):
+                splitted_text[i] = splitted_text[i] + tokenizer.eos_token
+            processed_text = "### Human:".join(splitted_text)
+
+        else:
+            processed_text = text + tokenizer.eos_token
+
+        return processed_text
+
+    dataset = dataset.map(
+        lambda x: {
+            "output": append_eos(x["output"], dataset_name),
+        }
+    )
+
+    def filter_long_sequences(sample):
+        complete_sequence = sample["input"] + sample["output"]
+        tokenized_complete_sequence = tokenizer.tokenize(complete_sequence)
+        return len(tokenized_complete_sequence) <= max_seq_len
+
+    def filter_long_inputs(sample, max_input_len=max_input_len):
+        tokenized_input = tokenizer.tokenize(sample["input"])
+        return len(tokenized_input) <= max_input_len
+
+    if "eval" in dataset:
+        orig_len_eval = len(dataset["eval"])
+    if "train" in dataset:
+        orig_len_train = len(dataset["train"])
+    if filter_longer_sequences:
+        dataset = dataset.filter(filter_long_sequences)
+    if filter_longer_inputs:
+        dataset = dataset.filter(filter_long_inputs)
+    if "train" in dataset:
+        print(f'Train: filtered {orig_len_train} -> {len(dataset["train"])} samples for max_seq_len: {max_seq_len}')
+    if "eval" in dataset:
+        print(f'Eval: filtered {orig_len_eval} -> {len(dataset["eval"])} samples for max_seq_len: {max_seq_len}')
+    return dataset
+
+
+@dataclass
+class DataCollatorForCausalLM(object):
+    """Collate examples for supervised fine-tuning."""
+
+    tokenizer: transformers.PreTrainedTokenizer
+
+    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        input_ids, labels, attention_mask = tuple(
+            [instance[key] for instance in instances] for key in ("input_ids", "labels", "attention_mask")
+        )
+
+        return dict(
+            input_ids=torch.tensor(input_ids, dtype=torch.long),
+            labels=torch.tensor(labels, dtype=torch.long),
+            attention_mask=torch.tensor(attention_mask, dtype=torch.long),
+            hidden_states=None,
+        )
+
+
+def make_data_module(
+    tokenizer: transformers.PreTrainedTokenizer,
+    dataset_name,
+    train=True,
+    eval=True,
+    max_train_samples=None,
+    max_eval_samples=None,
+    max_seq_len=512,
+    data_seed=42,
+    cached_dataset_dir=None,
+    hf_cache=None,
+    filter_longer_sequences=True,
+    filter_longer_inputs=False,
+    max_input_len=256,
+    train_on_source=False,
+) -> Dict:
+    """
+    Make dataset and collator for supervised fine-tuning.
+    Datasets are expected to have the following columns: { `input`, `output` }
+
+    Available datasets to be selected with `dataset` argument:
+        - alpaca, 52002 examples
+        - oasst1 (OpenAssistant) primary message tree only, 9,846 examples
+
+    """
+    raw_dataset = get_dataset(dataset_name, cached_dataset_dir, hf_cache)
+    formatted_dataset = format_dataset(raw_dataset, dataset_name)
+    dataset = preprocess_dataset(
+        formatted_dataset,
+        dataset_name,
+        tokenizer,
+        filter_longer_sequences,
+        filter_longer_inputs,
+        max_input_len,
+        max_seq_len,
+    )
+
+    def tokenize(dataset):
+        """Preprocess the data by tokenizing."""
+        sources = dataset["input"]
+        targets = dataset["output"]
+        examples = [s + t for s, t in zip(sources, targets)]
+        examples_tokenized = tokenizer(examples, truncation=True, padding="max_length", max_length=max_seq_len)
+        sources_tokenized = tokenizer(sources, truncation=True, padding=False, max_length=max_seq_len)
+        input_ids = examples_tokenized["input_ids"]
+        labels = copy.deepcopy(input_ids)
+        # Don't train on source
+        for label, source in zip(labels, sources_tokenized["input_ids"]):
+            label[: len(source)] = [tokenizer.pad_token_id for _ in range(len(source))]
+        return dict(input_ids=input_ids, labels=labels, attention_mask=examples_tokenized["attention_mask"])
+
+    # Split train/eval, reduce size
+    if eval:
+        if "eval" in dataset:
+            eval_dataset = dataset["eval"]
+        elif "test" in dataset:
+            eval_dataset = dataset["test"]
+        else:
+            print("Splitting train dataset in train and validation according to `90-10`")
+            dataset = dataset["train"].train_test_split(test_size=0.10, shuffle=True, seed=data_seed)
+            eval_dataset = dataset["test"]
+
+        if max_eval_samples is not None and len(eval_dataset) > max_eval_samples:
+            eval_dataset = eval_dataset.select(range(max_eval_samples))
+        # At the moment do not remove the columns
+        # eval_dataset = eval_dataset.map(tokenize, batched=True, remove_columns=eval_dataset.column_names)
+        eval_dataset = eval_dataset.map(tokenize, batched=True)
+
+        print(f"Eval Dataset Length : {len(eval_dataset)}")
+
+    if train:
+        train_dataset = dataset["train"]
+        if max_train_samples is not None and len(train_dataset) > max_train_samples:
+            train_dataset = train_dataset.select(range(max_train_samples))
+
+        if data_seed is not None:
+            set_random_seed(data_seed)
+            train_dataset = train_dataset.shuffle(seed=data_seed)
+        train_dataset = train_dataset.map(tokenize, batched=True, remove_columns=train_dataset.column_names)
+
+        print(f"Train Dataset Length : {len(train_dataset)}")
+
+    data_collator = DataCollatorForCausalLM(tokenizer=tokenizer)
+
+    return dict(
+        train_dataset=train_dataset if train else None,
+        eval_dataset=eval_dataset if eval else None,
+        data_collator=data_collator,
+    )
+
+
+def dataloader_for_eval_metrics(
+    tokenizer,
+    dataset_name,
+    dataset_split,
+    batch_size,
+    num_sequences=128,
+    sequence_length=128,
+    seed=42,
+    prefiltered_dataset_dir=None,
+):
+    data_module = make_data_module(
+        tokenizer,
+        dataset_name,
+        train=dataset_split == "train",
+        eval=dataset_split == "eval",
+        max_seq_len=sequence_length,
+        data_seed=seed,
+        cached_dataset_dir=prefiltered_dataset_dir,
+        filter_longer_sequences=False,
+        filter_longer_inputs=False,
+    )
+
+    dataset = data_module["train_dataset"] if dataset_split == "train" else data_module["eval_dataset"]
+    dataset = dataset.filter(lambda x: tokenizer.eos_token_id not in x["input_ids"])
+    assert num_sequences <= len(
+        dataset
+    ), f"num_sequences ({num_sequences}) must be less than or equal to the number of sequences in the dataset after filtering for sequences at least as long as {sequence_length} ({len(dataset)})"
+    dataset = dataset.select(range(num_sequences))
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, collate_fn=data_module["data_collator"], shuffle=False, drop_last=True
+    )
+    return dataloader
+
 
 # TODO: Replace this with actual Falcon application-level tests
 def run_test_FalconCausalLM_end_to_end(
@@ -81,6 +438,15 @@ def run_test_FalconCausalLM_end_to_end(
     else:
         # batch identical sequences for debugging
         model_input = torch.stack([torch.arange(seq_len)] * batch).reshape(batch, seq_len)
+
+    tokenizer = get_tokenizer("tiiuae/falcon-7b", explicit_pad_token=False)
+    dataloader = dataloader_for_eval_metrics(
+        tokenizer, "alpaca_eval", "eval", 1, batch, seq_len, 0, "/proj_sw/user_dev/falcon-ft/datasets"
+    )
+    inputs = []
+    for sample in dataloader:
+        inputs.append(sample["input_ids"])
+    model_input = torch.cat(inputs)
 
     # Generate dummy kv_cache --------------------------------------------------------------
     if llm_mode == "prefill":
@@ -175,7 +541,7 @@ def run_test_FalconCausalLM_end_to_end(
                 for m_i in model_inputs
             ]
         )
-        for user_id in range(batch):
+        for user_id in tqdm(range(batch)):
             tt_out, tt_layer_present = tt_FalconCausalLM(
                 input_embeddings=tt_embeddings[user_id],
                 llm_mode=llm_mode,
@@ -250,7 +616,7 @@ def run_test_FalconCausalLM_end_to_end(
                 for m_i in model_inputs
             ]
         )
-        for user_id in range(batch):
+        for user_id in tqdm(range(batch)):
             tt_out, tt_layer_present = tt_FalconCausalLM(
                 input_embeddings=tt_embeddings[user_id],
                 llm_mode=llm_mode,
@@ -284,13 +650,14 @@ def run_test_FalconCausalLM_end_to_end(
     does_pass, output_pcc = comp_pcc(pytorch_out, tt_out, pcc)
     logger.info(f"Output: {output_pcc}")
 
-    reference_logits = pytorch_out.view(batch, -1).float().detach().numpy()
-    eval_logits = tt_out.view(batch, -1).float().detach().numpy()
+    reference_logits = pytorch_out.view(batch * seq_len, -1).float().detach().numpy()
+    eval_logits = tt_out.view(batch * seq_len, -1).float().detach().numpy()
     reference_top1 = np.argmax(reference_logits, axis=-1)
     top1_acc = top_k_accuracy_score(reference_top1, eval_logits, k=1, labels=np.arange(eval_logits.shape[-1]))
     top5_acc = top_k_accuracy_score(reference_top1, eval_logits, k=5, labels=np.arange(eval_logits.shape[-1]))
     logger.info(f"Top-1 Accuracy: {top1_acc}")
     logger.info(f"Top-5 Accuracy: {top5_acc}")
+    breakpoint()
 
     for i in range(num_layers):
         tt_layer_pres = (
@@ -359,6 +726,8 @@ def run_test_FalconCausalLM_end_to_end(
     (
         ("prefill", 1, 128, 0, 0.30),
         ("prefill", 1, 256, 0, 0.44),
+        ("prefill", 32, 256, 0, 0.44),
+        ("prefill", 64, 256, 0, 0.44),
         ("decode", 32, 1, 128, 0.27),
         ("decode", 32, 1, 1024, 0.35),
         ("decode", 32, 1, 2047, 0.48),
@@ -366,6 +735,8 @@ def run_test_FalconCausalLM_end_to_end(
     ids=[
         "prefill_seq128",
         "prefill_seq256",
+        "prefill_seq256_batch32",
+        "prefill_seq256_batch64",
         "decode_batch32",
         "decode_batch32_1024",
         "decode_batch32_2047",
