29bb5fec1c1a84dc9c4782b974d28488ff9f2db4
Apala Guha <apala.guha@gmail.com>
2024-03-05 20:22:55 +0000

    abar mul h with randoms

--- models/demos/mamba/tt/mamba_shard_explore.py
+++ models/demos/mamba/tt/mamba_shard_explore.py
@@ -1,13 +1,36 @@
 import torch
 import ttnn
+import sys
+
+def AbarMulH(num_users, hidden_dim):
+
+    abar = torch.rand((1,1,num_users,hidden_dim), dtype=torch.bfloat16)
+    h = torch.rand((1,1,num_users,hidden_dim), dtype=torch.bfloat16)
+
+    cfg = ttnn.create_sharded_memory_config(shape=(1,1,num_users,hidden_dim), core_grid=ttnn.CoreGrid(y=num_users//32, x=8), strategy=ttnn.ShardStrategy.BLOCK, orientation=ttnn.ShardOrientation.ROW_MAJOR, use_height_and_width_as_shard_shape=False)
+
+    abar = ttnn.from_torch(abar, layout=ttnn.TILE_LAYOUT, device=device, memory_config=cfg)
+    h = ttnn.from_torch(h, layout=ttnn.TILE_LAYOUT, device=device, memory_config=cfg)
+
+    output_tensor = ttnn.add(abar, h, memory_config=cfg)
+
+def mamba_ssm_block(num_users, hidden_dim):
+    # call elementwise mul of Abar and h
+    AbarMulH(num_users, hidden_dim)
 
 device_id = 0
 device = ttnn.open_device(device_id=device_id)
 
 torch.manual_seed(0)
+ttnn.enable_program_cache()
 
-torch_input_tensor_a = torch.rand((32, 32), dtype=torch.bfloat16)
-torch_input_tensor_b = torch.rand((32, 32), dtype=torch.bfloat16)
+# number of users is arg 1
+num_users = int(sys.argv[1])
 
-input_tensor_a = ttnn.from_torch(torch_input_tensor_a, layout=ttnn.TILE_LAYOUT, device=device)
-input_tensor_b = ttnn.from_torch(torch_input_tensor_b, layout=ttnn.TILE_LAYOUT, device=device)
+# hidden dimension is arg 2
+hidden_dim = int(sys.argv[2])
+
+mamba_ssm_block(num_users, hidden_dim)
+
+
+ttnn.close_device(device)
