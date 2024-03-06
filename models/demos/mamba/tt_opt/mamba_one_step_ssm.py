34abc19ce60e32510fb8284ff0c49b776669d292
Apala Guha <apala.guha@gmail.com>
2024-03-06 21:46:33 +0000

    ah plus bx in bfp16

--- models/demos/mamba/tt_opt/mamba_one_step_ssm.py
+++ models/demos/mamba/tt_opt/mamba_one_step_ssm.py
@@ -41,13 +41,28 @@ class TtMambaSSM(torch.nn.Module):
         self.tt_hidden_state = ttnn.zeros((1,1,self.num_users,self.hidden_size), layout=ttnn.TILE_LAYOUT, device=self.device, memory_config=ttnn.DRAM_MEMORY_CONFIG)
         
     def forward(self, x):
-
+        # allocate abar
         abar = torch.rand((1,1,self.num_users,self.hidden_size), dtype=torch.bfloat16)
-        
-
         abar = ttnn.from_torch(abar, layout=ttnn.TILE_LAYOUT, device=self.device, memory_config=self.configs['sharded'])
+        
+        # multiply abar and hidden_state
         hidden_state = ttnn.to_memory_config(self.tt_hidden_state, self.configs['sharded'])
-        self.tt_hidden_state = ttnn.mul(abar, hidden_state, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+        amulh = ttnn.mul(abar, hidden_state, memory_config=self.configs['sharded'])
+        
+        # deallocate abar and hidden_state
+        ttnn.deallocate(abar)
+        ttnn.deallocate(hidden_state)
+        
+        # allocate bbar
+        bbar = torch.rand((1,1,self.num_users,self.hidden_size), dtype=torch.bfloat16)
+        bbar = ttnn.from_torch(bbar, layout=ttnn.TILE_LAYOUT, device=self.device, memory_config=self.configs['sharded'])
+        
+        # multiply bbar and x
+        bmulx = ttnn.mul(bbar, x, memory_config=self.configs['sharded'])
+        
+        # deallocate bbar and x
+        ttnn.deallocate(bbar)
+        ttnn.deallocate(x)
 
-        self.output = self.tt_hidden_state
-        return self.output
+        self.tt_hidden_state = ttnn.add(amulh, bmulx, memory_config=self.configs['sharded'])
+        return self.tt_hidden_state
