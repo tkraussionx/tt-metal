26aa43f84066249c3c10b4506bdc7b3d92c48ef4
Mark O'Connor <moconnor@tenstorrent.com>
2024-03-08 10:22:47 +0000

    #5337: Add --layers option

--- models/perf/perf_report.py
+++ models/perf/perf_report.py
@@ -2,7 +2,7 @@ import pandas as pd
 import argparse
 
 
-def analyze_perf_csv(csv_file, show_all):
+def analyze_perf_csv(csv_file, layers, show_all):
     df = pd.read_csv(csv_file)
     df = df[df["DEVICE FW DURATION [ns]"] != "-"]
     df["DEVICE FW DURATION [ns]"] = df["DEVICE FW DURATION [ns]"].astype(int)
@@ -43,8 +43,6 @@ def analyze_perf_csv(csv_file, show_all):
     )
     sorted_df = df.sort_values(by="DEVICE FW DURATION [ns]", ascending=False)
     sum_duration = df["DEVICE FW DURATION [ns]"].sum()
-    tokens_per_sec_user = 1000000000 / sum_duration / 32
-    tokens_per_sec = 32 * tokens_per_sec_user
 
     matmul_rows = sorted_df[sorted_df["OP CODE"].str.contains("tt::operations::primary::Matmul")]
     matmul_rows.loc[:, "bytes"] = (
@@ -59,13 +57,17 @@ def analyze_perf_csv(csv_file, show_all):
     matmul_rows["% TIME SUM"] = matmul_rows["% TIME"].cumsum()
     matmul_sum_duration = matmul_rows["DEVICE FW DURATION [ns]"].sum()
 
+    # shorten some column names
+    matmul_rows.rename(columns={"DEVICE FW DURATION [ns]": "DURATION [ns]"}, inplace=True)
+    sorted_df.rename(columns={"DEVICE FW DURATION [ns]": "DURATION [ns]"}, inplace=True)
+
     selected_columns = [
         "OP CODE",
         "% TIME",
         "% TIME SUM",
         "% DRAM (240)",
         "% FPU (82)",
-        "DEVICE FW DURATION [ns]",
+        "DURATION [ns]",
         "GB/s",
         "TFLOP/s",
         "CORE COUNT",
@@ -83,7 +85,7 @@ def analyze_perf_csv(csv_file, show_all):
             "OP CODE",
             "% TIME",
             "% TIME SUM",
-            "DEVICE FW DURATION [ns]",
+            "DURATION [ns]",
             "CORE COUNT",
             "INPUT_0_Y",
             "INPUT_0_X",
@@ -92,13 +94,17 @@ def analyze_perf_csv(csv_file, show_all):
             "OUTPUT_0_Y",
             "OUTPUT_0_X",
         ]
-        sorted_df["% TIME"] = 100 * sorted_df["DEVICE FW DURATION [ns]"] / sum_duration
+        sorted_df["% TIME"] = 100 * sorted_df["DURATION [ns]"] / sum_duration
         sorted_df["% TIME SUM"] = sorted_df["% TIME"].cumsum()
+        print()
         print(sorted_df[selected_columns])
 
-    print(f"Layer ms: {sum_duration / 1000000:.1f} ({matmul_sum_duration / sum_duration:.1%} matmul)")
-    print(f"Tokens/sec/user: {tokens_per_sec_user:.1f}")
-    print(f"Tokens/sec: {tokens_per_sec:.1f}")
+    if layers:
+        tokens_per_sec_user = 1000000000 / sum_duration / layers
+        tokens_per_sec = 32 * tokens_per_sec_user
+        print(f"Layer ms: {sum_duration / 1000000:.1f} ({matmul_sum_duration / sum_duration:.1%} matmul)")
+        print(f"Tokens/sec/user: {tokens_per_sec_user:.1f}")
+        print(f"Tokens/sec: {tokens_per_sec:.1f}")
 
 
 def main():
@@ -106,12 +112,13 @@ def main():
     parser.add_argument(
         "-a", "--all", action="store_true", help="List ops in the CSV file - by default only matmul ops are shown."
     )
+    parser.add_argument("-l", "--layers", type=int, help="Number of layers to extrapolate perf results up to.")
     parser.add_argument(
         "csv_file", type=str, help="Path to the perf CSV file from tt-metal for a single decoder layer."
     )
     args = parser.parse_args()
 
-    analyze_perf_csv(args.csv_file, show_all=args.all)
+    analyze_perf_csv(args.csv_file, layers=args.layers, show_all=args.all)
 
 
 if __name__ == "__main__":
