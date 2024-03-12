3184e73ed8139d31c3b3f0a95b4b19fd86ef83e8
Apala Guha <apala.guha@gmail.com>
2024-03-12 20:54:21 +0000

    conv2d

--- models/experimental/mamba/tt_opt/mamba_block.py
+++ models/experimental/mamba/tt_opt/mamba_block.py
@@ -30,16 +30,45 @@ class TtMambaBlock(torch.nn.Module):
         self.hidden_size = hidden_size
         self.configs = configs
         
-        # mlp wt
-        self.mlp_proj = ttnn.from_torch(torch.rand(1,1,self.hidden_size,2*self.hidden_size), layout=ttnn.TILE_LAYOUT, device=self.device, 
+        # ssm wt
+        self.ssm_proj = ttnn.from_torch(torch.rand(1,1,self.hidden_size,2*self.hidden_size), layout=ttnn.TILE_LAYOUT, device=self.device, 
                                  memory_config=ttnn.DRAM_MEMORY_CONFIG, dtype=ttnn.bfloat16)
+        
+        # conv states
+        self.conv_states = []
+        for i in range(4):
+            self.conv_states.append(ttnn.zeros((1,1,self.num_users,self.hidden_size*2), layout=ttnn.TILE_LAYOUT, device=self.device, 
+                                 memory_config=ttnn.DRAM_MEMORY_CONFIG, dtype=ttnn.bfloat16))
+        self.conv_wts = [] 
+        for i in range(4):
+            self.conv_wts.append(ttnn.from_torch(torch.rand(1,1,1,self.hidden_size*2,), layout=ttnn.TILE_LAYOUT, device=self.device, 
+                                 memory_config=ttnn.DRAM_MEMORY_CONFIG, dtype=ttnn.bfloat16))
+        self.conv_bias = ttnn.from_torch(torch.rand(1,1,1,self.hidden_size*2), layout=ttnn.TILE_LAYOUT, device=self.device, memory_config=ttnn.DRAM_MEMORY_CONFIG, dtype=ttnn.bfloat16)
 
 
         self.tt_ssm = TtMambaSSM(self.args,self.device,self.state_dict, num_users, hidden_size, configs)
 
     def forward(self, x):
-
-        x0 = ttnn.linear(x, self.mlp_proj, memory_config=ttnn.L1_MEMORY_CONFIG)
-        ttnn.deallocate(x)
-        x = self.tt_ssm(x0)
+        
+        x = ttnn.linear(x, self.ssm_proj, memory_config=ttnn.L1_MEMORY_CONFIG)
+     
+        # left shift conv states
+        ttnn.deallocate(self.conv_states[0])
+        for i in range(3):
+            self.conv_states[i] = self.conv_states[i + 1]
+        self.conv_states[3] = x
+        
+        # do the convolution
+        conv_wts = ttnn.repeat_interleave(self.conv_wts[0], self.num_users, dim=2)
+        x = ttnn.mul(conv_wts, self.conv_states[0])        
+        for i in range(1,4):
+            print('**********', self.conv_wts[i].shape, self.conv_states[i].shape)
+            conv_wts = ttnn.repeat_interleave(self.conv_wts[i], self.num_users, dim=2)
+            prod = ttnn.mul(conv_wts, self.conv_states[i])
+            x = ttnn.add(x, prod)
+        conv_bias = ttnn.repeat_interleave(self.conv_bias, self.num_users, dim=2)
+        x = ttnn.add(x, conv_bias)
+        
+        print('**********', x.shape)
+        x = self.tt_ssm(x)
         return x
