375b7f22b80e229f5e25aae0ec7c813073b4306f
Dalar Vartanians <dvartanians@tenstorrent.com>
2024-03-13 00:33:43 +0000

    #5792: add unit test for log sigmoid of Brain UNet

--- tests/ttnn/unit_tests/operations/test_logsigmoid.py
+++ tests/ttnn/unit_tests/operations/test_logsigmoid.py
@@ -19,10 +19,8 @@ from models.utility_functions import torch_random
 # @pytest.mark.parametrize("dim", [-1, -2, -3])
 def test_logsigmoid(device, batch_size, h, w):
     torch.manual_seed(0)
-    torch_input_tensor = ttnn.from_torch(torch.tensor((batch_size, h, w), dtype=torch.bfloat16), device=device)
     torch_input_tensor = torch_random((batch_size, h, w), -1, 1, dtype=torch.bfloat16)
-    torch_output_tensor = F.softmax(torch_input_tensor, dim=dim, dtype=torch.bfloat16)
-
+    torch_output_tensor = F.sigmoid(torch_input_tensor)
     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
 
     input_tensor = ttnn.to_device(input_tensor, device)
@@ -31,4 +29,4 @@ def test_logsigmoid(device, batch_size, h, w):
     output_tensor = ttnn.from_device(output_tensor)
     output_tensor = ttnn.to_torch(output_tensor)
 
-    assert_with_pcc(torch_output_tensor, output_tensor, 0.997)
+    assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
