456d35ebd4b9e41337be84d9fdcbf89879ba3512
Johanna Rock <jrock@tenstorrent.com>
2024-03-14 09:45:06 +0000

    #6241: Remove skipping bfloat16 prefill from tests

--- models/demos/falcon40b/tests/test_falcon_attention.py
+++ models/demos/falcon40b/tests/test_falcon_attention.py
@@ -365,9 +365,6 @@ def test_FalconAttention_inference(
     all_devices,
     # use_program_cache, # TODO: enable program cache as soon as multi chip correctness is verified
 ):
-    if llm_mode == "prefill" and model_config_str == "BFLOAT16-SHARDED":
-        pytest.skip("Prefill is only tested for BFLOAT8_B!")
-
     input_shape = [batch, seq_len]
     model_config = get_model_config(model_config_str, llm_mode, input_shape, num_devices)
     devices = get_devices_for_t3000(all_devices, num_devices)
