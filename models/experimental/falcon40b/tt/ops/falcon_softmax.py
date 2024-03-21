a84f915509e8cc3b492ce74323dede0438e43ac5
Johanna Rock <jrock@tenstorrent.com>
2024-03-21 13:40:07 +0000

    #6241: Fix falcon softmax op test

--- models/experimental/falcon40b/tt/ops/falcon_softmax.py
+++ models/experimental/falcon40b/tt/ops/falcon_softmax.py
@@ -12,15 +12,10 @@ from models.utility_functions import torch2tt_tensor
 
 
 class TtFalconSoftmax:
-    def __init__(
-        self,
-        device,
-        model_config,
-        head_dim: int = 64,
-        seqlen: int = 32,
-    ):
+    def __init__(self, device, model_config, head_dim: int = 64, seqlen: int = 32, is_sharded=False):
         super().__init__()
 
+        self.is_sharded = is_sharded
         self.model_config = model_config
         self.seqlen = seqlen
         self.scalar = 1 / math.sqrt(head_dim)
@@ -31,15 +26,27 @@ class TtFalconSoftmax:
             self.seqlen // 32
         )  # TODO: We can directly set this for prefill model config, but it might be confusing...
 
-        # Subtract max value from activation before softmax
-        out = ttl.operations.primary.transformers.scale_mask_softmax_in_place(
-            x,
-            self.scalar,
-            attention_mask,
-            # program_config=softmax_progcfg,
-            # output_mem_config=self.model_config["DEFAULT_MEMCFG"],
-            program_config=ttl.operations.primary.transformers.SoftmaxDefaultProgramConfig(),
-            is_causal_mask=True,
-        )
+        if self.is_sharded:
+            # Subtract max value from activation before softmax
+            out = ttl.operations.primary.transformers.scale_mask_softmax_in_place(
+                x,
+                self.scalar,
+                attention_mask,
+                program_config=softmax_progcfg,
+                # output_mem_config=self.model_config["DEFAULT_MEMCFG"],
+                # program_config=ttl.operations.primary.transformers.SoftmaxDefaultProgramConfig(),
+                is_causal_mask=True,
+            )
+        else:
+            # Subtract max value from activation before softmax
+            out = ttl.operations.primary.transformers.scale_mask_softmax_in_place(
+                x,
+                self.scalar,
+                attention_mask,
+                # program_config=softmax_progcfg,
+                # output_mem_config=self.model_config["DEFAULT_MEMCFG"],
+                program_config=ttl.operations.primary.transformers.SoftmaxDefaultProgramConfig(),
+                is_causal_mask=True,
+            )
 
         return out
