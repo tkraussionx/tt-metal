18e8c4a9aa4ed21e070aea566cc54359ccda8e2e
Tapasvi Patel <tpatel@tenstorrent.com>
2024-03-22 05:13:37 +0000

    #0: DO NOT MERGE - Update grid size for n150 for upsample

--- ttnn/ttnn/operations/others.py
+++ ttnn/ttnn/operations/others.py
@@ -256,7 +256,7 @@ def mean(input_tensor: ttnn.Tensor, dim: Union[int, Tuple[int]], keepdim: bool =
 
 ## helper function for upsample. currently only supports HEIGHT sharding
 def _get_upsample_shard_grid_from_num_shards(ncores: int, device):
-    max_grid_size = (8, 8) if device.arch() == ttl.device.Arch.WORMHOLE_B0 else (9, 12)  ## (y, x)
+    max_grid_size = (9, 8) if device.arch() == ttl.device.Arch.WORMHOLE_B0 else (9, 12)  ## (y, x)
     if ncores % max_grid_size[1] == 0:
         core_grid = ttnn.CoreGrid(y=ncores // max_grid_size[1], x=max_grid_size[1])
         grid_coord = ttnn.experimental.tensor.CoreCoord(core_grid.x - 1, core_grid.y - 1)
@@ -290,7 +290,7 @@ def _get_upsample_num_shards(
     batch_size: int, height: int, num_channels: int, shard_strategy: ttnn.ShardStrategy, device
 ):
     ## calculate ncores, corresponding grid_size and in_shard_shape based on the input_shape
-    max_grid_size = (8, 8) if device.arch() == ttl.device.Arch.WORMHOLE_B0 else (9, 12)  ## (y, x)
+    max_grid_size = (9, 8) if device.arch() == ttl.device.Arch.WORMHOLE_B0 else (9, 12)  ## (y, x)
     if shard_strategy == ttnn.ShardStrategy.HEIGHT:
         ## nsticks per shard should be divisible by in_w
         max_nshards = min(batch_size * height, max_grid_size[0] * max_grid_size[1])
