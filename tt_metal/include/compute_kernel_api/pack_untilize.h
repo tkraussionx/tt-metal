551571fd945da5e16946e3090a91833ae936d244
Milos <mtrajkovic@tenstorrent.com>
2024-03-25 22:20:33 +0000

    #4624: Add support for UINT32 format throughout the stack.

--- tt_metal/include/compute_kernel_api/pack_untilize.h
+++ tt_metal/include/compute_kernel_api/pack_untilize.h
@@ -21,17 +21,17 @@ namespace ckernel {
 template <uint32_t block_ct_dim = 8>
 ALWI void pack_untilize_init(uint32_t icb, uint32_t ocb)
 {
-    MATH(( llk_math_eltwise_unary_datacopy_init<A2D, BroadcastType::NONE>(false /*transpose of faces*/, false /*transpose within 16x16 face*/, icb) ));
-    MATH(( llk_math_pack_sync_init<SyncHalf>() ));
+    MATH(( llk_math_eltwise_unary_datacopy_init<A2D, BroadcastType::NONE, DST_ACCUM_MODE>(false /*transpose of faces*/, false /*transpose within 16x16 face*/, icb) ));
+    MATH(( llk_math_pack_sync_init<SyncHalf, DST_ACCUM_MODE>() ));
 
-    PACK(( llk_pack_hw_configure_disaggregated<false>(ocb) ));
+    PACK(( llk_pack_hw_configure_disaggregated<false, DST_ACCUM_MODE>(ocb) ));
     PACK(( llk_pack_untilize_init<block_ct_dim>(ocb) ));
     PACK(( llk_setup_outputs() ));
-    PACK(( llk_pack_dest_init<SyncHalf, true>() ));
+    PACK(( llk_pack_dest_init<SyncHalf, true, DST_ACCUM_MODE>() ));
 
     UNPACK(( llk_setup_operands() ));
-    UNPACK(( llk_unpack_A_hw_configure_disaggregated(icb) ));
-    UNPACK(( llk_unpack_A_init(false, false, icb) )); // init must be after configure
+    UNPACK(( llk_unpack_A_hw_configure_disaggregated<DST_ACCUM_MODE>(icb) ));
+    UNPACK(( llk_unpack_A_init<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(false, false, icb) )); // init must be after configure
 }
 
 /**
@@ -42,14 +42,14 @@ ALWI void pack_untilize_block(uint32_t icb, uint32_t block_rt_dim, uint32_t ocb)
     for (uint32_t r = 0; r < block_rt_dim; ++r) {
         MATH(( llk_math_wait_for_dest_available<SyncHalf>() ));
         for (uint32_t c = 0; c < block_ct_dim; ++c) {
-            UNPACK(( llk_unpack_A(icb, c) ));
-            MATH(( llk_math_eltwise_unary_datacopy<A2D, BroadcastType::NONE, SyncHalf>(c) ));
+            UNPACK(( llk_unpack_A<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(icb, c) ));
+            MATH(( llk_math_eltwise_unary_datacopy<A2D, BroadcastType::NONE, SyncHalf, DST_ACCUM_MODE, UnpackToDestEn>(c) ));
         }
-        MATH(( llk_math_dest_section_done<SyncHalf>() ));
+        MATH(( llk_math_dest_section_done<SyncHalf, DST_ACCUM_MODE>() ));
 
         PACK(( llk_packer_wait_for_math_done() ));
         PACK(( llk_pack_untilize<block_ct_dim>(1 /*num_blocks*/, ocb) ));
-        PACK(( llk_pack_dest_section_done<SyncHalf>() ));
+        PACK(( llk_pack_dest_section_done<SyncHalf, DST_ACCUM_MODE>() ));
     }
 }
 
@@ -76,8 +76,8 @@ ALWI void pack_untilize_dst(uint32_t ocb, uint32_t block_rt_dim = 1, uint32_t bl
 template <uint32_t block_ct_dim = 8>
 ALWI void pack_untilize_init_short(uint32_t icb, uint32_t ocb)
 {
-    UNPACK(( llk_unpack_A_init(false, false, icb) )); // init must be after configure
-    MATH(( llk_math_eltwise_unary_datacopy_init<A2D, BroadcastType::NONE>(false /*transpose of faces*/, false /*transpose within 16x16 face*/, icb) ));
+    UNPACK(( llk_unpack_A_init<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(false, false, icb) )); // init must be after configure
+    MATH(( llk_math_eltwise_unary_datacopy_init<A2D, BroadcastType::NONE, DST_ACCUM_MODE>(false /*transpose of faces*/, false /*transpose within 16x16 face*/, icb) ));
     pack_untilize_dst_init_short<block_ct_dim>(ocb);
 }
 
