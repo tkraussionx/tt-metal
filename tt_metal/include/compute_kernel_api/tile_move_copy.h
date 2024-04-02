428162b03a22d458041eb8d3d8a6ddc5016cd04c
Reem Tawfik <rtawfik@tenstorrent.com>
2024-04-02 18:54:34 +0000

    #0: perf debug

--- tt_metal/include/compute_kernel_api/tile_move_copy.h
+++ tt_metal/include/compute_kernel_api/tile_move_copy.h
@@ -26,7 +26,7 @@ namespace ckernel {
  */
 ALWI void copy_tile_to_dst_init_short(uint32_t cbid = 0, uint32_t transpose = 0)
 {
-    UNPACK(( llk_unpack_A_init<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(transpose)  ));
+    UNPACK(( llk_unpack_A_init<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE>(transpose)  ));
     MATH(( llk_math_eltwise_unary_datacopy_init<A2D, BroadcastType::NONE, DST_ACCUM_MODE>(false /*transpose of faces*/, false /*transpose within 16x16 face*/, cbid)  ));
 }
 /**
@@ -75,14 +75,14 @@ ALWI void copy_tile_to_dst_init_short_with_dt(uint32_t old_cbid, uint32_t new_cb
  * */
 ALWI void copy_tile(uint32_t in_cb_id, uint32_t in_tile_index, uint32_t dst_tile_index)
 {
-    UNPACK(( llk_unpack_A<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(in_cb_id, in_tile_index)  ));
-    MATH(( llk_math_eltwise_unary_datacopy<A2D, BroadcastType::NONE, DST_ACCUM_MODE, UnpackToDestEn>(dst_tile_index)  ));
+    UNPACK(( llk_unpack_A<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE>(in_cb_id, in_tile_index)  ));
+    MATH(( llk_math_eltwise_unary_datacopy<A2D, BroadcastType::NONE, DST_ACCUM_MODE>(dst_tile_index)  ));
 }
 
 ALWI void copy_block_matmul_partials(uint32_t in_cb_id, uint32_t start_in_tile_index, uint32_t start_dst_tile_index, uint32_t ntiles)
 {
-    UNPACK(( llk_unpack_A_block<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(in_cb_id, start_in_tile_index, ntiles, false)  ));
-    MATH(( llk_math_eltwise_unary_datacopy_block<A2D, BroadcastType::NONE, DST_ACCUM_MODE, UnpackToDestEn>(start_dst_tile_index, ntiles, in_cb_id)  ));
+    UNPACK(( llk_unpack_A_block<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE>(in_cb_id, start_in_tile_index, ntiles, false)  ));
+    MATH(( llk_math_eltwise_unary_datacopy_block<A2D, BroadcastType::NONE, DST_ACCUM_MODE>(start_dst_tile_index, ntiles, in_cb_id)  ));
 }
 
 }
