481bc2512898526907336372ee41cece0e9a5941
Colman Glagovich <cglagovich@gmail.com>
2024-04-03 12:51:37 +0000

    #7003: Optimized Llama prefill config for sequence length 2k. WIP to add generality to seqlen 128.

--- models/demos/llama2_70b/tt/llama_decoder_optimized.py
+++ models/demos/llama2_70b/tt/llama_decoder_optimized.py
@@ -447,7 +447,7 @@ class TtLlamaDecoder_optimized:
         # Do sharded RMS by partial sequence length of 128
         # Input xs[0] is [1, 1, seq_len, 8192]
         seq_len = xs[0].shape[2]
-        slice_size = 128
+        slice_size = 512
         num_slices = seq_len // slice_size  # we do 128 per iteration (slice), then we concat the result.
 
         xs_output_cat = []  # this is the output we write to. Initiate as empty tensors
