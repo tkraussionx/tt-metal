8520208e3745695d83a19df7e368e359e1e7a7ef
Stuti Raizada <sraizada@tenstorrent.com>
2024-04-05 09:24:48 +0000

    #5337: minor import fix

--- models/demos/mixtral8x7b/tests/test_mixtral_perf.py
+++ models/demos/mixtral8x7b/tests/test_mixtral_perf.py
@@ -5,12 +5,13 @@ import torch
 import pytest
 from loguru import logger
 import ttnn
-from models.demos.mixtral8x7b.tt.mixtral_common_ttnn import (
+
+from models.demos.mixtral8x7b.tt.mixtral_common import (
     prepare_inputs_ttnn,
     sample,
 )
-from models.demos.mixtral8x7b.tt.mixtral_model_ttnn import TtTransformer
-from models.demos.mixtral8x7b.tt.model_config_ttnn import TtModelArgs
+from models.demos.mixtral8x7b.tt.mixtral_model import TtTransformer
+from models.demos.mixtral8x7b.tt.model_config import TtModelArgs
 from models.demos.mixtral8x7b.reference.model import Transformer
 from models.demos.mixtral8x7b.reference.tokenizer import Tokenizer
 from models.utility_functions import comp_pcc, comp_allclose, get_devices_for_t3000
