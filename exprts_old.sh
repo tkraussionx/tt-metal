a8264eef02bf741544a5d9a9c2702175b15f65fb
Dalar Vartanians <dvartanians@tenstorrent.com>
2024-04-11 01:20:21 +0000

    #0: debug the profiler

---
+++ exprts_old.sh
@@ -0,0 +1,11 @@
+export TT_METAL_HOME=$(pwd)
+export ARCH_NAME=grayskull
+export PYTHONPATH=$(pwd)
+export TT_METAL_ENV=dev #production
+
+#export TTNN_CONFIG_OVERRIDES='{
+#    "enable_logging": false,
+#    "enable_graph_report": false,
+#    "enable_detailed_buffer_report": false,
+#    "enable_comparison_mode": false
+#}'
