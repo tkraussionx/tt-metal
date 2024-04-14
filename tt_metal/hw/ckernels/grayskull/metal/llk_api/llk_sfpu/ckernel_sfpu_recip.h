b4da200124e8956b772612b33e1ff6ffbb4d0e59
Aswinmcw <azayasankaran@tenstorrent.com>
2024-04-14 04:41:53 +0000

    #0: floor test

--- tt_metal/hw/ckernels/grayskull/metal/llk_api/llk_sfpu/ckernel_sfpu_recip.h
+++ tt_metal/hw/ckernels/grayskull/metal/llk_api/llk_sfpu/ckernel_sfpu_recip.h
@@ -72,11 +72,12 @@ inline void calculate_reciprocal()
     for (int d = 0; d < ITERATIONS; d++)
     {
         vFloat val = dst_reg[0];
+        vFloat end = 60.0f;
         int i=0;
         v_if (dst_reg[0]>0.0f){
             while (i<30){
                 val = val - 1.0f;
-                v_if(val==60.0f){
+                v_if(val-end==0){
                     i=100;
                 }
                 v_endif;
