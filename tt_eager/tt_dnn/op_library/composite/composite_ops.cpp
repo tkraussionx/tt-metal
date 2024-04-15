d51660635fe0c3f13240a5133b6ee2900aede973
Aswinmcw <azayasankaran@tenstorrent.com>
2024-04-15 05:55:38 +0000

    #0: floor test

--- tt_eager/tt_dnn/op_library/composite/composite_ops.cpp
+++ tt_eager/tt_dnn/op_library/composite/composite_ops.cpp
@@ -128,7 +128,7 @@ Tensor _tanhshrink(const Tensor& x, const MemoryConfig& output_mem_config) {
     value = where(logical_and(gte(value, zeros_like(value)), lt(value, ones_like(value))), zeros_like(value), value);
     value = where(logical_and(gte(value, neg(ones_like(value))), lt(value, zeros_like(value))), neg(ones_like(value)), value);
     for (int i=0;i<100;i++){
-        value = where(gte(abs(value), ones_like(value)), sub_unary(abs(value), 1), value);
+        value = where(gt(abs(value), ones_like(value)), sub_unary(abs(value), 1), value);
     }
     value = sub(abs(orig), value);
     value = where(ltz(orig), sub_unary(neg(value), 1), value);
