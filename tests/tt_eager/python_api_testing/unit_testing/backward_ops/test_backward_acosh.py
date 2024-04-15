7ba1b150dc7d31762e4859a6bb02a1a37b2e952e
Aswinmcw <azayasankaran@tenstorrent.com>
2024-04-15 10:51:39 +0000

    #0: floor test

--- tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_acosh.py
+++ tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_acosh.py
@@ -26,7 +26,7 @@ from tests.tt_eager.python_api_testing.sweep_tests import (
 )
 def test_bw_remainder(input_shapes, device):
     in_data, input_tensor = data_gen_with_range(input_shapes, 50, 100, device, True)
-    grad_data, grad_tensor = data_gen_with_val(input_shapes, device, val=5)
+    grad_data, grad_tensor = data_gen_with_range(input_shapes, 10, 30, device)
     print(input_tensor)
 
     golden_tensor = torch.remainder(in_data, grad_data)
