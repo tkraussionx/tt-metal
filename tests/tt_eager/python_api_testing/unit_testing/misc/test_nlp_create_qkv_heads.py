3b82a36b7d4aeb257f3143673ac0a11959078d45
Mariko Tatsumi <mtatsumi@tenstorrent.com>
2024-04-16 21:35:05 +0000

    #0: add unit test for the q-kv separate call

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_nlp_create_qkv_heads.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_nlp_create_qkv_heads.py
@@ -481,3 +481,186 @@ def test_sharded_nlp_create_qkv_heads_with_program_cache(device, use_program_cac
         tt_dummy_tensor = ttl.tensor.Tensor(py_dummy_tensor, dtype).to(ttl.tensor.Layout.TILE).to(device, mem_config)
 
     assert device.num_program_cache_entries() == 2
+
+
+"""
+Generic shape + separte generation support
+"""
+
+
+def run_nlp_create_qkv_heads_separet_test(
+    batch,
+    seq_len,
+    head_dim,
+    num_q_heads,
+    num_kv_heads,
+    transpose_k_heads,
+    read_from_input_tensor_kv,
+    dtype,
+    in_mem_config,
+    out_mem_config,
+    device,
+):
+    torch.manual_seed(1234)
+
+    if read_from_input_tensor_kv:
+        in0_shape = [batch, 1, seq_len, num_q_heads * head_dim]
+        in1_shape = [batch, 1, seq_len, 2 * num_kv_heads * head_dim]
+        A = torch.randn(in0_shape)
+        B = torch.randn(in1_shape)
+        in0_t = ttl.tensor.Tensor(A, dtype).to(ttl.tensor.Layout.TILE).to(device, in_mem_config)
+        in1_t = ttl.tensor.Tensor(B, dtype).to(ttl.tensor.Layout.TILE).to(device, in_mem_config)
+    else:
+        in0_shape = [batch, 1, seq_len, (num_q_heads + 2 * num_kv_heads) * head_dim]
+        A = torch.randn(in0_shape)
+        in0_t = ttl.tensor.Tensor(A, dtype).to(ttl.tensor.Layout.TILE).to(device, in_mem_config)
+
+    # q, k, v = ttl.tensor.nlp_create_qkv_heads(
+    #    in0_t,
+    #    in1_t if read_from_input_tensor_kv else None,
+    #    num_heads=num_q_heads,
+    #    num_kv_heads=num_kv_heads,
+    #    transpose_k_heads=transpose_k_heads,
+    #    output_mem_config=out_mem_config,
+    # )
+
+    q = ttl.tensor.nlp_create_q_heads(
+        in0_t,
+        num_heads=num_q_heads,
+        num_kv_heads=num_kv_heads,
+        output_mem_config=out_mem_config,
+    )
+
+    k, v = ttl.tensor.nlp_create_kv_heads(
+        in0_t,
+        num_heads=num_q_heads,
+        num_kv_heads=num_kv_heads,
+        transpose_k_heads=transpose_k_heads,
+        output_mem_config=out_mem_config,
+    )
+
+    # Check memory of inputs and outputs
+    assert in0_t.memory_config().buffer_type == in_mem_config.buffer_type
+    assert q.memory_config().buffer_type == out_mem_config.buffer_type
+    assert k.memory_config().buffer_type == out_mem_config.buffer_type
+    assert v.memory_config().buffer_type == out_mem_config.buffer_type
+    logger.debug(f"in0: {in0_t.memory_config().buffer_type} and {in0_t.get_dtype()}")
+    logger.debug(f"q: {q.memory_config().buffer_type} and {q.get_dtype()}")
+    logger.debug(f"k: {k.memory_config().buffer_type} and {k.get_dtype()}")
+    logger.debug(f"v: {v.memory_config().buffer_type} and {v.get_dtype()}")
+
+    assert list(q.get_legacy_shape()) == [batch, num_q_heads, seq_len, head_dim]
+    if transpose_k_heads:
+        assert list(k.get_legacy_shape()) == [batch, num_kv_heads, head_dim, seq_len]
+    else:
+        assert list(k.get_legacy_shape()) == [batch, num_kv_heads, seq_len, head_dim]
+    assert list(v.get_legacy_shape()) == [batch, num_kv_heads, seq_len, head_dim]
+
+    pyt_got_back_rm_q = tt2torch_tensor(q)
+    pyt_got_back_rm_k = tt2torch_tensor(k)
+    pyt_got_back_rm_v = tt2torch_tensor(v)
+
+    if read_from_input_tensor_kv:
+        ref_q = A
+        (ref_k, ref_v) = torch.split(B, [num_kv_heads * head_dim, num_kv_heads * head_dim], dim=-1)
+    else:
+        (ref_q, ref_k, ref_v) = torch.split(
+            A, [num_q_heads * head_dim, num_kv_heads * head_dim, num_kv_heads * head_dim], dim=-1
+        )
+
+    # Additional shuffling for Q, K, V heads
+    ref_q = torch.reshape(ref_q, [batch, seq_len, num_q_heads, head_dim]).transpose(-3, -2)
+    ref_k = torch.reshape(ref_k, [batch, seq_len, num_kv_heads, head_dim]).transpose(-3, -2)
+    ref_v = torch.reshape(ref_v, [batch, seq_len, num_kv_heads, head_dim]).transpose(-3, -2)
+    if transpose_k_heads:
+        ref_k = ref_k.transpose(-2, -1)
+
+    if dtype == ttl.tensor.DataType.BFLOAT8_B:
+        pcc = 0.99
+    elif dtype == ttl.tensor.DataType.FLOAT32:  # conversion from fp32 to tf32 will decrease pcc
+        pcc = 0.9999999
+    else:
+        pcc = 1.0
+
+    passing_pcc_q, output_pcc_q = comp_pcc(pyt_got_back_rm_q, ref_q, pcc)
+    logger.debug(f"Q passing={passing_pcc_q}")
+    logger.debug(f"Q output pcc={output_pcc_q}")
+
+    passing_pcc_k, output_pcc_k = comp_pcc(pyt_got_back_rm_k, ref_k, pcc)
+    logger.debug(f"K passing={passing_pcc_k}")
+    logger.debug(f"K output pcc={output_pcc_k}")
+
+    passing_pcc_v, output_pcc_v = comp_pcc(pyt_got_back_rm_v, ref_v, pcc)
+    logger.debug(f"V passing={passing_pcc_v}")
+    logger.debug(f"V output pcc={output_pcc_v}")
+    assert passing_pcc_q
+    assert passing_pcc_k
+    assert passing_pcc_v
+
+
+@pytest.mark.parametrize(
+    "out_mem_config",
+    (
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+    ),
+    ids=["out_DRAM", "out_L1"],
+)
+@pytest.mark.parametrize(
+    "in_mem_config",
+    (
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+    ),
+    ids=["in_DRAM", "in_L1"],
+)
+@pytest.mark.parametrize(
+    "dtype",
+    (ttl.tensor.DataType.BFLOAT8_B, ttl.tensor.DataType.BFLOAT16, ttl.tensor.DataType.FLOAT32),
+    ids=["BFLOAT8_B", "BFLOAT16", "FLOAT32"],
+)
+@pytest.mark.parametrize(
+    "batch, seq_len, head_dim, num_q_heads, num_kv_heads, transpose_k_heads, read_from_input_tensor_kv",
+    (
+        (1, 128, 64, 71, 1, False, False),
+        (111, 64, 96, 5, 3, True, False),
+        (5, 1024, 64, 8, 8, True, True),
+    ),
+)
+def test_nlp_create_qkv_heads_separate_test(
+    batch,
+    seq_len,
+    head_dim,
+    num_q_heads,
+    num_kv_heads,
+    transpose_k_heads,
+    read_from_input_tensor_kv,
+    dtype,
+    in_mem_config,
+    out_mem_config,
+    request,
+    device,
+):
+    if is_grayskull() and dtype == ttl.tensor.DataType.FLOAT32:
+        pytest.skip("Skipping float32 tests on Grayskull")
+    if (
+        dtype == ttl.tensor.DataType.FLOAT32
+        and (batch == 111 or batch == 5)
+        and in_mem_config
+        == ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1)
+    ):
+        logger.warning("fp32 tensor too large to fit L1")
+    else:
+        run_nlp_create_qkv_heads_test(
+            batch,
+            seq_len,
+            head_dim,
+            num_q_heads,
+            num_kv_heads,
+            transpose_k_heads,
+            read_from_input_tensor_kv,
+            dtype,
+            in_mem_config,
+            out_mem_config,
+            device,
+        )
