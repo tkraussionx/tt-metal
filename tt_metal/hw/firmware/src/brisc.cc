15dd8c1e4c39baf4d2c2aa580ee548dc99d079bd
Mo Memarian <mmemarian@tenstorrent.com>
2024-04-16 01:36:32 +0000

    #753: Working 8-chip sync

--- tt_metal/hw/firmware/src/brisc.cc
+++ tt_metal/hw/firmware/src/brisc.cc
@@ -67,6 +67,8 @@ namespace kernel_profiler {
     uint32_t stackSize __attribute__((used));
     uint32_t sums[SUM_COUNT] __attribute__((used));
     uint32_t sumIDs[SUM_COUNT] __attribute__((used));
+    bool resultsPushed __attribute__((used));
+    uint16_t core_flat_id __attribute__((used));
 }
 
 void enable_power_management() {
--- tt_metal/llrt/rtoptions.hpp
+++ tt_metal/llrt/rtoptions.hpp
@@ -44,6 +44,7 @@ class RunTimeOptions {
     bool test_mode_enabled = false;
 
     bool profiler_enabled = false;
+    bool profiler_sync_enabled = false;
 
     bool null_kernels = false;
 
@@ -135,6 +136,7 @@ class RunTimeOptions {
     inline void set_test_mode_enabled(bool enable) { test_mode_enabled = enable; }
 
     inline bool get_profiler_enabled() { return profiler_enabled; }
+    inline bool get_profiler_sync_enabled() { return profiler_sync_enabled; }
 
     inline void set_kernels_nullified(bool v) { null_kernels = v; }
     inline bool get_kernels_nullified() { return null_kernels; }
