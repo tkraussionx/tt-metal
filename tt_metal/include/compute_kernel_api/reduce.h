b2dec04fdc210f119007478945725160e40e444e
Colman Glagovich <cglagovich@gmail.com>
2024-04-17 20:33:30 +0000

    #7461: SDPA templatized reduce.h. reduce_max still works in kernel

--- tt_metal/include/compute_kernel_api/reduce.h
+++ tt_metal/include/compute_kernel_api/reduce.h
@@ -18,59 +18,63 @@
 
 namespace ckernel {
 
-#if (defined(REDUCE_OP) and defined(REDUCE_DIM)) or defined(__DOXYGEN__)
+// #if (defined(REDUCE_OP) and defined(REDUCE_DIM)) or defined(__DOXYGEN__)
 
-template<bool at_start>
-ALWI void reduce_init(PoolType reduce_op, ReduceDim dim, uint32_t icb, uint32_t icb_scaler, uint32_t ocb = 16)
+template<bool at_start, PoolType reduce_op, ReduceDim reduce_dim>
+ALWI void reduce_init(uint32_t icb, uint32_t icb_scaler, uint32_t ocb = 16)
 {
     UNPACK(( llk_setup_operands() ));
     UNPACK(( llk_unpack_AB_hw_configure_disaggregated<DST_ACCUM_MODE>(icb, icb_scaler) ));
-    UNPACK(( llk_unpack_AB_reduce_init<REDUCE_DIM>(icb, icb_scaler) ));
+    UNPACK(( llk_unpack_AB_reduce_init<reduce_dim>(icb, icb_scaler) ));
 
-    MATH(( llk_math_reduce_init<REDUCE_OP, REDUCE_DIM, MATH_FIDELITY>() ));
+    MATH(( llk_math_reduce_init<reduce_op, reduce_dim, MATH_FIDELITY>() ));
     MATH(( llk_math_pack_sync_init<DST_ACCUM_MODE>() ));
 
     PACK(( llk_pack_init() ));
-    PACK(( llk_pack_reduce_config_v2<REDUCE_DIM, at_start, false, DST_ACCUM_MODE>(ocb) ));
+    PACK(( llk_pack_reduce_config_v2<reduce_dim, at_start, false, DST_ACCUM_MODE>(ocb) ));
     PACK(( llk_setup_outputs() ));
     PACK(( llk_pack_dest_init<false, DST_ACCUM_MODE>() ));
 }
 
-ALWI void reduce_init_short(PoolType reduce_op, ReduceDim dim, uint32_t icb, uint32_t icb_scaler, uint32_t ocb = 16) {
+template<PoolType reduce_op, ReduceDim reduce_dim>
+ALWI void reduce_init_short(uint32_t icb, uint32_t icb_scaler, uint32_t ocb = 16) {
 
-    UNPACK(( llk_unpack_AB_reduce_init<REDUCE_DIM>(icb, icb_scaler) ));
-    MATH(( llk_math_reduce_init<REDUCE_OP, REDUCE_DIM, MATH_FIDELITY>() ));
-    PACK(( llk_pack_reduce_config_v2<REDUCE_DIM, false, false, DST_ACCUM_MODE>(ocb) ));
+    UNPACK(( llk_unpack_AB_reduce_init<reduce_dim>(icb, icb_scaler) ));
+    MATH(( llk_math_reduce_init<reduce_op, reduce_dim, MATH_FIDELITY>() ));
+    PACK(( llk_pack_reduce_config_v2<reduce_dim, false, false, DST_ACCUM_MODE>(ocb) ));
 }
 
 // Delta from binary_op_init_common
-template<bool at_start>
-ALWI void reduce_init_delta(PoolType reduce_op, ReduceDim dim, uint32_t ocb = 16, uint32_t icb0 = 0, uint32_t icb1 = 1)
+template<bool at_start, PoolType reduce_op, ReduceDim reduce_dim>
+ALWI void reduce_init_delta(uint32_t ocb = 16, uint32_t icb0 = 0, uint32_t icb1 = 1)
 {
     // FIXME: API Update needed in compute kernel?
-    UNPACK(( llk_unpack_AB_reduce_init<REDUCE_DIM>(icb0, icb1) ));
+    UNPACK(( llk_unpack_AB_reduce_init<reduce_dim>(icb0, icb1) ));
 
-    MATH(( llk_math_reduce_init<REDUCE_OP, REDUCE_DIM, MATH_FIDELITY>() ));
+    MATH(( llk_math_reduce_init<reduce_op, reduce_dim, MATH_FIDELITY>() ));
 
-    PACK(( llk_pack_reduce_config_v2<REDUCE_DIM, at_start, false, DST_ACCUM_MODE>(ocb) ));
+    PACK(( llk_pack_reduce_config_v2<reduce_dim, at_start, false, DST_ACCUM_MODE>(ocb) ));
 }
 
+template<PoolType reduce_op, ReduceDim reduce_dim>
 ALWI void reduce_init_delta_no_pack(uint32_t icb0 = 0, uint32_t icb1 = 1)
 {
     // FIXME: API Update needed in compute kernel?
     UNPACK(( llk_unpack_AB_init<>(icb0, icb1) ));
 
-    MATH(( llk_math_reduce_init<REDUCE_OP, REDUCE_DIM, MATH_FIDELITY>() ));
+    MATH(( llk_math_reduce_init<reduce_op, reduce_dim, MATH_FIDELITY>() ));
 }
 
+template<PoolType reduce_op, ReduceDim reduce_dim>
 ALWI void reduce_init_delta_math()
 {
-    MATH(( llk_math_reduce_init<REDUCE_OP, REDUCE_DIM, MATH_FIDELITY>() ));
+    MATH(( llk_math_reduce_init<reduce_op, reduce_dim, MATH_FIDELITY>() ));
 }
 
+template<ReduceDim reduce_dim>
 ALWI void reduce_revert_delta(uint32_t ocb = 16)
 {
-    PACK(( llk_pack_reduce_config_v2<REDUCE_DIM, false, true, DST_ACCUM_MODE>(ocb) ));
+    PACK(( llk_pack_reduce_config_v2<reduce_dim, false, true, DST_ACCUM_MODE>(ocb) ));
 }
 
 /**
@@ -95,17 +99,19 @@ ALWI void reduce_revert_delta(uint32_t ocb = 16)
  * | itile1         | The index of the tile within the scaling factor CB              | uint32_t | Must be less than the size of the CB           | True     |
  * | idst           | The index of the tile in DST REG for the result                 | uint32_t | Must be less than the acquired size of DST REG | True     |
  */
-ALWI void reduce_tile(PoolType reduce_op, ReduceDim dim, uint32_t icb0, uint32_t icb1, uint32_t itile0, uint32_t itile1, uint32_t idst)
+template<PoolType reduce_op, ReduceDim reduce_dim>
+ALWI void reduce_tile(uint32_t icb0, uint32_t icb1, uint32_t itile0, uint32_t itile1, uint32_t idst)
 {
-    MATH(( llk_math_reduce<REDUCE_OP, REDUCE_DIM, MATH_FIDELITY, DST_ACCUM_MODE>(idst) ));
+    MATH(( llk_math_reduce<reduce_op, reduce_dim, MATH_FIDELITY, DST_ACCUM_MODE>(idst) ));
     UNPACK(( llk_unpack_AB(icb0, icb1, itile0, itile1) ));
 }
 
+template<PoolType reduce_op, ReduceDim reduce_dim>
 ALWI void reduce_tile_math(uint32_t idst, uint32_t num_faces = 4)
 {
-    MATH(( llk_math_reduce<REDUCE_OP, REDUCE_DIM, MATH_FIDELITY, DST_ACCUM_MODE>(idst, num_faces) ));
+    MATH(( llk_math_reduce<reduce_op, reduce_dim, MATH_FIDELITY, DST_ACCUM_MODE>(idst, num_faces) ));
 }
-#endif
+// #endif
 
 
 
