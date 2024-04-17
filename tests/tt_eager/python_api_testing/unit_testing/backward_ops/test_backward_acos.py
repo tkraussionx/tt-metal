32d98fd5c64b547b397f0b2eb284cf5218778fb3
Aswinmcw <azayasankaran@tenstorrent.com>
2024-04-17 08:20:45 +0000

    #0: floor test

--- tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_acos.py
+++ tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_acos.py
@@ -26,7 +26,7 @@ from tests.tt_eager.python_api_testing.sweep_tests import (
 )
 def test_bw_remainder(input_shapes, device):
     in_data, input_tensor = data_gen_with_range(input_shapes, 50, 100, device, True)
-    grad_data, grad_tensor = data_gen_with_range(input_shapes, 10, 30, device)
+    grad_data, grad_tensor = data_gen_with_range(input_shapes, 10, 50, device)
     print(in_data)
     print(grad_data)
 
