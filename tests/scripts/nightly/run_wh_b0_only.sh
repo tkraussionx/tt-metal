7803f8b28d33b2ebae1d98f008519666997df0a0
Mariko Tatsumi <mtatsumi@tenstorrent.com>
2024-04-19 14:46:45 +0000

    #0: move SD tests to the middle

--- tests/scripts/nightly/run_wh_b0_only.sh
+++ tests/scripts/nightly/run_wh_b0_only.sh
@@ -1,7 +1,5 @@
 echo "Running nightly tests for WH B0 only"
 
-env TT_METAL_WATCHER=1 WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest tests/ttnn/integration_tests/stable_diffusion
-
 env pytest tests/ttnn/integration_tests/unet                # -> failing: issue #7556
 # env pytest tests/ttnn/integration_tests/stable_diffusion    # -> failing/hanging: issue #7560
 
@@ -9,6 +7,8 @@ env pytest models/experimental/mamba/tests/test_full_model.py       # -> failing
 env pytest models/experimental/mamba/tests_opt/test_full_model.py
 env pytest models/experimental/mamba/tests/test_benchmarks.py
 env pytest models/experimental/mamba/tests/test_demo.py             # -> failing with autoformat error: issue #7551
+
+env TT_METAL_WATCHER=1 WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest tests/ttnn/integration_tests/stable_diffusion
 
 env pytest models/demos/mistral7b/tests/test_mistral_embedding.py
 env pytest models/demos/mistral7b/tests/test_mistral_rms_norm.py
