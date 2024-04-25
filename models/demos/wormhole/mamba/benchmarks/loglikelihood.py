fced2b5ab0855911033b932d7ba8b8fc856cc68b
kpaigwar <kpaigwar@tenstorrent.com>
2024-04-25 15:19:36 -0400

    #7843: add wh model to lm harness

--- models/demos/wormhole/mamba/benchmarks/loglikelihood.py
+++ models/demos/wormhole/mamba/benchmarks/loglikelihood.py
@@ -13,7 +13,7 @@ def compute_loglikelihood(logits, labels) -> float:
     assert len(labels.shape) == 3, "Expected rank 3"
     assert logits.shape[-1] > 1, "Logits should be >1 in final dim"
     assert labels.shape[1] == logits.shape[1], "Length dimension should match"
-    logits = torch.nn.functional.log_softmax(logits, dim=-1)  # (B x L x VOCAB)
+    # logits = torch.nn.functional.log_softmax(logits, dim=-1)  # (B x L x VOCAB)
     return torch.gather(logits, -1, labels).sum().detach().cpu().item()
 
 
@@ -29,8 +29,50 @@ def compute_loglikelihood_given_prompt_and_target(context_ids, target_ids, model
 
     logits = []
     is_greedy = True
+    print("Sequence Length : ", input_ids.shape[-1])
     for idx in range((input_ids.shape[-1])):
-        out = model(input_ids[:, idx].unsqueeze(1))  # (B x 1) => (B x 1 x VOCAB)
+        with torch.no_grad():
+            out = model(input_ids[:, idx].unsqueeze(1))  # (B x 1) => (B x 1 x VOCAB)
+        probs = torch.nn.functional.log_softmax(out, dim=-1)
+        logits.append(probs)
+
+        last_token = torch.argmax(out, dim=-1)
+        target_token = input_ids[:, idx].unsqueeze(0)
+        assert (
+            last_token.shape == target_token.shape
+        ), f"Expected actual and target token to be same shape ({last_token.shape} vs. {target_token.shape})"
+
+        if last_token.item() != target_token.item():
+            is_greedy = False
+
+    # Compute loglikelihood using the recorded logits
+    logits = torch.cat(logits, dim=1)[:, -num_target_tokens:, :vocab_size]  # (B x L x VOCAB )
+    labels = target_ids.unsqueeze(-1)  # (B x L x 1)
+    loglikelihood = compute_loglikelihood(logits, labels)
+
+    return loglikelihood, is_greedy
+
+
+def compute_loglikelihood_given_prompt_and_target_for_wh(context_ids, target_ids, model: MambaDecode, vocab_size: int):
+    # Reset the model hidden/conv states before decoding
+    model.reset_states()
+
+    # We want logits for each target token so slice the last one off
+    input_ids = torch.cat([context_ids, target_ids], dim=-1)[:, :-1]  # B x L
+
+    num_target_tokens = target_ids.shape[1]
+    last_token = context_ids[:, -1].unsqueeze(1)  # Model expects (Bx1)
+
+    logits = []
+    is_greedy = True
+    print("Sequence Length : ", input_ids.shape[-1])
+    for idx in range((input_ids.shape[-1])):
+        # Replicate inputs to match the model's expected input shape
+        tt_model_inputs = input_ids[:, idx].unsqueeze(1).repeat(32, 1)
+        with torch.no_grad():
+            out = model(tt_model_inputs)  # (B x 1) => (B x 1 x VOCAB)
+            # select the first output from the batch
+            out = out[0].unsqueeze(0)
         probs = torch.nn.functional.log_softmax(out, dim=-1)
         logits.append(probs)
 
