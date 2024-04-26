6344fd752fe32ec6394dce8fee1492b5c9892b5c
Colman Glagovich <cglagovich@tenstorrent.com>
2024-04-26 14:09:36 +0000

    #7461: SDPA: Add license to test file

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_scaled_dot_product_attention.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_scaled_dot_product_attention.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
 import torch
 from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import (
     comp_allclose,
