828a98c31e28fb8ab8003ba1d94c0ad31e887848
Michael Chiou <mchiou@tenstorrent.com>
2024-04-29 14:31:42 -0700

    #7944: Add dockerfile and scripts to bringup docker instance of tt-metal

---
+++ scripts/docker/install_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Setup sources for docker
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+# Install docker and plugins
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+
+# Set up docker for non-root user
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Test docker installation
+docker run hello-world
