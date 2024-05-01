92efa44d111851e17c573cbe4b890d8f248e38dc
Brian Liu <bliu@tenstorrent.com>
2024-05-01 20:23:43 +0000

    #0: Cherry pick test + add unit test

--- tests/ttnn/integration_tests/stable_diffusion/test_sharded_attention.py
+++ tests/ttnn/integration_tests/stable_diffusion/test_sharded_attention.py
@@ -241,9 +241,9 @@ def test_time_sharded_attnention_hwb(
 
 
 # Test matmul attention sequence with InterleavedToShardedPartialOp
-@skip_for_grayskull()
+# @skip_for_grayskull()
 @pytest.mark.parametrize("seq_len", [4096, 1024])
-@pytest.mark.parametrize("num_slices", [16])
+@pytest.mark.parametrize("num_slices", [8])
 @pytest.mark.parametrize("num_cores", [64])
 @pytest.mark.parametrize("num_heads", [16])
 @pytest.mark.parametrize("data_format", [ttl.tensor.DataType.BFLOAT8_B])
@@ -256,7 +256,9 @@ def test_time_sharded_attnention(
     data_format,
     function_level_defaults,
 ):
-    pytest.skip()  # ND hang on CI
+    num_slices = 8 if seq_len == 4096 else 2
+    # num_slices = 16
+    # pytest.skip()  # ND hang on CI
     compute_grid_size = device.compute_with_storage_grid_size()
     if num_cores > (compute_grid_size.x * compute_grid_size.y):
         pytest.skip(f"Need {num_cores} cores to run this test but core grid is {compute_grid_size}")
@@ -305,12 +307,13 @@ def test_time_sharded_attnention(
         tt_dtype=data_format,
     )
 
-    compute_kernel_config = ttl.tensor.WormholeComputeKernelConfig(
-        math_fidelity=ttl.tensor.MathFidelity.LoFi,
-        math_approx_mode=True,
-        fp32_dest_acc_en=False,
-        packer_l1_acc=True,
-    )
+    compute_kernel_config = None
+    # compute_kernel_config = ttl.tensor.WormholeComputeKernelConfig(
+    #     math_fidelity=ttl.tensor.MathFidelity.LoFi,
+    #     math_approx_mode=True,
+    #     fp32_dest_acc_en=False,
+    #     packer_l1_acc=True,
+    # )
 
     passing = True
     output = None
@@ -354,6 +357,7 @@ def test_time_sharded_attnention(
             (0, (i * heads_per_slice) + (heads_per_slice - 1), 63, seq_len - 1),
             output_mem_config=l1_interleaved_memory_config,
         )
+        slice = ttnn.reshape(slice, (1, heads_per_slice, seq_len, 64))
         mm_slice = ttl.operations.primary.matmul(
             slice,
             k_slice,
@@ -411,21 +415,167 @@ def test_time_sharded_attnention(
 
         mm_slice.deallocate()
 
-        return
-
     mm_out_torch = tt2torch_tensor(mm_out)
 
-    attn_weights = ttl.tensor.bmm(
-        reference_query_layer, reference_key_layer_transposed, output_mem_config=dram_interleaved_memory_config
-    )
-    attn_weights = ttl.operations.primary.softmax_in_place(attn_weights)
-    attn_weights = ttl.tensor.bmm(attn_weights, reference_value_layer, output_mem_config=dram_interleaved_memory_config)
+    attn_weights_torch = torch_query_layer @ torch_key_layer_transposed
+    attn_weights_torch = torch.nn.functional.softmax(attn_weights_torch, dim=-1)
+    attn_weights_torch = attn_weights_torch @ torch_value_layer
 
-    attn_weights_torch = tt2torch_tensor(attn_weights)
     passing, output = comp_pcc(mm_out_torch, attn_weights_torch)
 
     print(output)
     assert passing
+
+
+# Test matmul attention sequence with InterleavedToShardedPartialOp
+# @skip_for_grayskull()
+@pytest.mark.parametrize("seq_len", [4096, 1024])
+@pytest.mark.parametrize("num_slices", [8])
+@pytest.mark.parametrize("num_cores", [64])
+@pytest.mark.parametrize("num_heads", [16])
+@pytest.mark.parametrize("data_format", [ttl.tensor.DataType.BFLOAT8_B])
+def test_time_sharded_attnention_v2(
+    device,
+    seq_len,
+    num_slices,
+    num_cores,
+    num_heads,
+    data_format,
+    function_level_defaults,
+):
+    num_slices = 8 if seq_len == 4096 else 2
+    # num_slices = 16
+    # pytest.skip()  # ND hang on CI
+    compute_grid_size = device.compute_with_storage_grid_size()
+    if num_cores > (compute_grid_size.x * compute_grid_size.y):
+        pytest.skip(f"Need {num_cores} cores to run this test but core grid is {compute_grid_size}")
+    grid_size = (8, 8)
+
+    query_layer_shape = [1, num_heads, seq_len, 64]
+    key_layer_transposed_shape = [1, num_heads, 64, seq_len]
+    value_layer_shape = [1, num_heads, seq_len, 64]
+    output_shape = [1, num_heads, seq_len, 64]
+
+    torch_query_layer = torch.randn(query_layer_shape).bfloat16().float()
+    torch_key_layer_transposed = torch.randn(key_layer_transposed_shape).bfloat16().float()
+    torch_value_layer = torch.randn(value_layer_shape).bfloat16().float()
+    torch_output = torch.randn(output_shape).bfloat16().float()
+
+    dram_interleaved_memory_config = ttl.tensor.MemoryConfig(
+        memory_layout=ttl.tensor.TensorMemoryLayout.INTERLEAVED,
+        buffer_type=ttl.tensor.BufferType.DRAM,
+    )
+    l1_interleaved_memory_config = ttl.tensor.MemoryConfig(
+        memory_layout=ttl.tensor.TensorMemoryLayout.INTERLEAVED,
+        buffer_type=ttl.tensor.BufferType.L1,
+    )
+
+    height_sharded_memory_config = ttl.tensor.MemoryConfig(
+        memory_layout=ttl.tensor.TensorMemoryLayout.HEIGHT_SHARDED, buffer_type=ttl.tensor.BufferType.L1
+    )
+
+    # compare output to regular case
+    reference_query_layer = torch2tt_tensor(
+        torch_query_layer,
+        device,
+        tt_memory_config=dram_interleaved_memory_config,
+        tt_dtype=data_format,
+    )
+    reference_key_layer_transposed = torch2tt_tensor(
+        torch_key_layer_transposed,
+        device,
+        tt_memory_config=dram_interleaved_memory_config,
+        tt_dtype=data_format,
+    )
+    reference_value_layer = torch2tt_tensor(
+        torch_value_layer,
+        device,
+        tt_memory_config=dram_interleaved_memory_config,
+        tt_dtype=data_format,
+    )
+
+    compute_kernel_config = None
+    # compute_kernel_config = ttl.tensor.WormholeComputeKernelConfig(
+    #     math_fidelity=ttl.tensor.MathFidelity.LoFi,
+    #     math_approx_mode=True,
+    #     fp32_dest_acc_en=False,
+    #     packer_l1_acc=True,
+    # )
+
+    passing = True
+    output = None
+
+    mm_out = torch2tt_tensor(
+        torch_output,
+        device,
+        tt_memory_config=dram_interleaved_memory_config,
+        tt_dtype=data_format,
+    )
+    tiles_per_shard = math.ceil((((num_heads * seq_len) / num_cores) / num_slices) / 32)
+    mm_activations_height_shard_spec = [tiles_per_shard * 32, 2 * 32]
+    mm_output_height_shard_spec = [tiles_per_shard * 32, seq_len]
+
+    heads_per_slice = num_heads // num_slices
+    i = 0
+    slice = ttl.tensor.interleaved_to_sharded_partial(
+        reference_query_layer,
+        grid_size,
+        mm_activations_height_shard_spec,
+        num_slices,
+        i,
+        ttl.tensor.TensorMemoryLayout.HEIGHT_SHARDED,
+        ttl.tensor.ShardOrientation.ROW_MAJOR,
+    )
+    program_config = ttl.operations.primary.MatmulMultiCoreReuseProgramConfig(
+        compute_with_storage_grid_size=grid_size,
+        in0_block_w=2,
+        per_core_M=tiles_per_shard,
+        per_core_N=seq_len // 32,
+        out_subblock_h=1,
+        out_subblock_w=1,
+    )
+    # program_config = ttl.operations.primary.MatmulMultiCoreReuseMultiCast1DProgramConfig(
+    #     compute_with_storage_grid_size=grid_size,
+    #     in0_block_w=2,
+    #     per_core_M=tiles_per_shard,
+    #     per_core_N=seq_len // 32,
+    #     out_subblock_h=1,
+    #     out_subblock_w=1,
+    #     fuse_batch=True,
+    #     fused_activation=None,
+    #     mcast_in0=False,
+    # )
+
+    k_slice = ttl.tensor.unpad(
+        reference_key_layer_transposed,
+        (0, (i * heads_per_slice), 0, 0),
+        (0, (i * heads_per_slice) + (heads_per_slice - 1), 63, seq_len - 1),
+        output_mem_config=dram_interleaved_memory_config,
+    )
+    slice = ttnn.reshape(slice, (1, heads_per_slice, seq_len, 64))
+    mm_slice = ttl.operations.primary.matmul(
+        slice,
+        k_slice,
+        program_config=program_config,
+        output_mem_config=height_sharded_memory_config,
+        output_dtype=data_format,
+        compute_kernel_config=compute_kernel_config,
+    )
+
+    mm_out_torch = tt2torch_tensor(mm_slice)
+
+    # attn_weights_torch = torch_query_layer @ torch_key_layer_transposed
+    # attn_weights_torch = torch.nn.functional.softmax(attn_weights_torch, dim=-1)
+    # attn_weights_torch = attn_weights_torch @ torch_value_layer
+    attn_weights_torch = (
+        torch_query_layer[:, i * heads_per_slice : (i + 1) * heads_per_slice, ...]
+        @ torch_key_layer_transposed[:, i * heads_per_slice : (i + 1) * heads_per_slice, ...]
+    )
+
+    passing, output = comp_pcc(mm_out_torch, attn_weights_torch)
+
+    print(output)
+    # assert passing
 
 
 # Test matmul attention sequence with InterleavedToShardedPartialOp
