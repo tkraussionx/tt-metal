fd720e1f6e5125f08d6ebb0843f1953b6dca38d5
Nicholas Smith <nsmith@tenstorrent.com>
2024-05-08 20:09:59 -0500

    #0: wip

--- tests/ttnn/unit_tests/operations/test_conv2d.py
+++ tests/ttnn/unit_tests/operations/test_conv2d.py
@@ -1634,7 +1634,8 @@ def test_yolov4_conv_groups_1_low_resolution(
     )
 
 
-@skip_for_wormhole_b0()
+# @skip_for_wormhole_b0()
+@pytest.mark.parametrize("device_l1_small_size", [32768], indirect=True)
 @pytest.mark.parametrize(
     "batch_size, output_channels, input_channels, input_height, input_width, filter_height, filter_width, stride_h, stride_w, pad_h, pad_w, use_1d_systolic_array, config_override, use_shallow_conv_variant",
     (
