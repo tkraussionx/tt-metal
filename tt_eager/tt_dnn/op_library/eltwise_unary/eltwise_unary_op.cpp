8e2aeadb0e852521dd51ebd8b63896cb973fc0d4
Austin Ho <aho@tenstorrent.com>
2024-05-09 15:33:05 +0000

    #0: Unify create_device_tensor and create_sharded_device_tensor

--- tt_eager/tt_dnn/op_library/eltwise_unary/eltwise_unary_op.cpp
+++ tt_eager/tt_dnn/op_library/eltwise_unary/eltwise_unary_op.cpp
@@ -282,7 +282,7 @@ std::vector<Tensor> EltwiseUnary::create_output_tensors(const std::vector<Tensor
     const auto& input_tensor = input_tensors.at(0);
     if(this->output_mem_config.is_sharded()){
             Shape output_shape = compute_output_shapes(input_tensors).at(0);
-            return {create_sharded_device_tensor(output_shape, input_tensor.get_dtype(), input_tensor.get_layout(), input_tensor.device(), this->output_mem_config)};
+            return {create_device_tensor(output_shape, input_tensor.get_dtype(), input_tensor.get_layout(), input_tensor.device(), this->output_mem_config)};
     }
     return operation::generic_create_output_tensors(*this, input_tensors, input_tensor.get_dtype(), Layout::TILE, this->output_mem_config);
     }
