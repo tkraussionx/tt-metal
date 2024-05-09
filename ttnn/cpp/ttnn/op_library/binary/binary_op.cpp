a8d96e7b9a2c08b9dbb485edc81c729589bce2fa
Austin Ho <aho@tenstorrent.com>
2024-05-09 15:33:05 +0000

    #0: Unify create_device_tensor and create_sharded_device_tensor

--- ttnn/cpp/ttnn/op_library/binary/binary_op.cpp
+++ ttnn/cpp/ttnn/op_library/binary/binary_op.cpp
@@ -222,7 +222,7 @@ std::vector<Tensor> Binary<binary_op_type, in_place>::create_output_tensors(
                 }
                 auto memory_config = this->program_config.memory_config;
                 memory_config.shard_spec = shard_spec;
-                return {create_sharded_device_tensor(
+                return {create_device_tensor(
                     this->compute_output_shapes(input_tensors).at(0),
                     this->program_config.dtype,
                     Layout::TILE,
@@ -238,7 +238,7 @@ std::vector<Tensor> Binary<binary_op_type, in_place>::create_output_tensors(
                 }
                 auto memory_config = this->program_config.memory_config;
                 memory_config.shard_spec = shard_spec;
-                return {create_sharded_device_tensor(
+                return {create_device_tensor(
                     this->compute_output_shapes(input_tensors).at(0),
                     this->program_config.dtype,
                     Layout::TILE,
