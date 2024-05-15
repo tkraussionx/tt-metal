53a9a028291197301cb9353cc59080fb7740e120
Colman Glagovich <cglagovich@tenstorrent.com>
2024-05-15 13:54:29 +0000

    #8189: Remove ring_buffer from lock_free_queue. Remove thread affinity from command_queue

--- tt_metal/impl/dispatch/command_queue.cpp
+++ tt_metal/impl/dispatch/command_queue.cpp
@@ -1205,7 +1205,7 @@ HWCommandQueue::HWCommandQueue(Device* device, uint32_t id) :
     std::thread completion_queue_thread = std::thread(&HWCommandQueue::read_completion_queue, this);
     this->completion_queue_thread = std::move(completion_queue_thread);
     // Set the affinity of the completion queue reader.
-    set_device_thread_affinity(this->completion_queue_thread, device->id());
+    // set_device_thread_affinity(this->completion_queue_thread, device->id());
     this->expected_num_workers_completed = 0;
 }
 
