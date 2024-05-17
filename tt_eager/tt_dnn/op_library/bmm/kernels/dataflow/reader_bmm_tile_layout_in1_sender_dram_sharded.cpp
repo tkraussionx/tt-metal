b068e1e2688e8378b94067e7aa7208aaa2f273d7
yugaoT <yugao@tenstorrent.com>
2024-05-17 14:14:37 +0000

    #0: add set state noc api for mm

--- tt_eager/tt_dnn/op_library/bmm/kernels/dataflow/reader_bmm_tile_layout_in1_sender_dram_sharded.cpp
+++ tt_eager/tt_dnn/op_library/bmm/kernels/dataflow/reader_bmm_tile_layout_in1_sender_dram_sharded.cpp
@@ -11,6 +11,47 @@
 
 template <uint32_t page_size, bool use_vc>
 FORCE_INLINE
+uint32_t noc_async_read_tile_dram_sharded_set_state(uint32_t bank_base_address, uint32_t bank_id = 0, const uint32_t vc = 0) {
+    uint32_t src_addr_;
+    uint32_t src_noc_xy;
+
+    src_addr_ = bank_base_address + bank_to_dram_offset[bank_id];
+    src_noc_xy = dram_bank_to_noc_xy[noc_index][bank_id];
+
+    DEBUG_STATUS("NRTW");
+    DEBUG_SANITIZE_NOC_READ_TRANSACTION(get_noc_addr_helper(src_noc_xy, src_addr_), dest_addr, page_size);
+    while (!noc_cmd_buf_ready(noc_index, NCRISC_RD_CMD_BUF));
+    DEBUG_STATUS("NRTD");
+
+    if constexpr(use_vc) {
+        uint32_t noc_rd_cmd_field = NOC_CMD_CPY | NOC_CMD_RD | NOC_CMD_RESP_MARKED | NOC_CMD_VC_STATIC | NOC_CMD_STATIC_VC(vc);
+        NOC_CMD_BUF_WRITE_REG(noc_index, NCRISC_RD_CMD_BUF, NOC_CTRL, noc_rd_cmd_field);
+    }
+
+    NOC_CMD_BUF_WRITE_REG(noc_index, NCRISC_RD_CMD_BUF, NOC_TARG_ADDR_MID, src_noc_xy);   // src_addr >> 32
+    NOC_CMD_BUF_WRITE_REG(noc_index, NCRISC_RD_CMD_BUF, NOC_AT_LEN_BE, page_size);  // len_bytes
+
+    return src_addr_;
+}
+
+FORCE_INLINE
+void noc_async_read_tile_dram_sharded_with_state(uint32_t src_base_addr, uint32_t src_addr, uint32_t dest_addr) {
+    uint32_t src_addr_;
+
+    src_addr_ = src_base_addr + src_addr;
+
+    DEBUG_STATUS("NRTW");
+    while (!noc_cmd_buf_ready(noc_index, NCRISC_RD_CMD_BUF));
+    DEBUG_STATUS("NRTD");
+
+    NOC_CMD_BUF_WRITE_REG(noc_index, NCRISC_RD_CMD_BUF, NOC_RET_ADDR_LO, dest_addr);
+    NOC_CMD_BUF_WRITE_REG(noc_index, NCRISC_RD_CMD_BUF, NOC_TARG_ADDR_LO, src_addr_);      // (uint32_t)src_addr
+    NOC_CMD_BUF_WRITE_REG(noc_index, NCRISC_RD_CMD_BUF, NOC_CMD_CTRL, NOC_CTRL_SEND_REQ);
+    noc_reads_num_issued[noc_index] += 1;
+}
+
+template <uint32_t page_size, bool use_vc>
+FORCE_INLINE
 void noc_async_read_tile_dram_sharded(uint32_t bank_base_address, uint32_t src_addr, uint32_t dest_addr, uint32_t bank_id = 0, const uint32_t vc = 0) {
     uint32_t src_addr_;
     uint32_t src_noc_xy;
@@ -92,6 +133,8 @@ void kernel_main() {
     // DPRINT << in1_page_size << ENDL();
     // DPRINT << vc << ENDL();
 
+    uint32_t in1_base_addr = noc_async_read_tile_dram_sharded_set_state<in1_page_size, true>(in1_tensor_addr, dram_bank_id, vc);
+
     uint32_t l1_read_addr_in1 = 0;
     for (uint32_t block = 0; block < num_blocks; ++block) {
         // Operand 1
@@ -102,7 +145,8 @@ void kernel_main() {
 
         // Copy in1 block into CB, as the default kernel
         for(uint32_t h = 0; h < in1_num_pages; ++h) {
-            noc_async_read_tile_dram_sharded<in1_page_size, true>(in1_tensor_addr, l1_read_addr_in1, l1_write_addr_in1, dram_bank_id, vc);
+            // noc_async_read_tile_dram_sharded<in1_page_size, true>(in1_tensor_addr, l1_read_addr_in1, l1_write_addr_in1, dram_bank_id, vc);
+            noc_async_read_tile_dram_sharded_with_state(in1_base_addr, l1_read_addr_in1, l1_write_addr_in1);
             l1_read_addr_in1 += in1_page_size;
             l1_write_addr_in1 += in1_page_size;
         }
@@ -117,8 +161,11 @@ void kernel_main() {
         uint32_t l1_write_addr_in3 = get_write_ptr(cb_id_in3);
         uint32_t l1_read_addr_in3 = 0;
 
+        uint32_t in3_base_addr = noc_async_read_tile_dram_sharded_set_state<in3_page_size, true>(in3_tensor_addr, dram_bank_id, vc);
+
         for(uint32_t h = 0; h < in3_num_pages; ++h) {
-            noc_async_read_tile_dram_sharded<in3_page_size, true>(in3_tensor_addr, l1_read_addr_in3, l1_write_addr_in3, dram_bank_id, vc);
+            // noc_async_read_tile_dram_sharded<in3_page_size, true>(in3_tensor_addr, l1_read_addr_in3, l1_write_addr_in3, dram_bank_id, vc);
+            noc_async_read_tile_dram_sharded_with_state(in3_base_addr, l1_read_addr_in3, l1_write_addr_in3);
             l1_read_addr_in3 += in3_page_size;
             l1_write_addr_in3 += in3_page_size;
         }
@@ -129,7 +176,7 @@ void kernel_main() {
     #endif
 
 
-
+    // WRITER
     cb_wait_front(cb_id_out, out_block_num_tiles);
     uint32_t index_offset = 0;
     uint32_t l1_read_addr_out_offset = 0;
