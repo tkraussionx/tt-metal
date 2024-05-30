83a14957b8c6afc4007eda267e3e511d77a52361
Stuti Raizada <sraizada@tenstorrent.com>
2024-05-30 11:55:57 +0000

    #5337: moe prefill

--- models/demos/t3000/mixtral8x7b/tt/mixtral_moe.py
+++ models/demos/t3000/mixtral8x7b/tt/mixtral_moe.py
@@ -37,17 +37,6 @@ class TtMoeLayer(torch.nn.Module):
         self.num_devices = 8
         self.compute_kernel = args.get_compute_kernel_attn_config()
 
-        reduce_mask_torch = torch.zeros(1, 1, self.args.max_batch_size, self.args.max_batch_size * 8)
-        for i in range(self.args.max_batch_size):
-            reduce_mask_torch[:, :, i, range(i, self.args.max_batch_size * 8, self.args.max_batch_size)] = 1
-        self.reduce_mask = ttnn.from_torch(
-            reduce_mask_torch,
-            dtype=ttnn.bfloat8_b,
-            layout=ttnn.TILE_LAYOUT,
-            device=self.device_mesh,
-            mesh_mapper=ReplicateTensorToMesh(device_mesh),
-        )
-        self.reduce_mask = ttnn.to_device(self.reduce_mask, device_mesh)
         self.expert_mask_11BB = ttnn.from_torch(
             torch.cat([torch.full((1, 1, 32, 32), fill_value=i + 1) for i in range(8)], dim=3),
             dtype=ttnn.uint16,
@@ -55,7 +44,7 @@ class TtMoeLayer(torch.nn.Module):
             device=device_mesh,
             mesh_mapper=ShardTensorToMesh(device_mesh, dim=3),
         )
-        top8_mask = torch.full((1, 1, 32, 64), fill_value=torch.finfo(torch.float).min)
+        top8_mask = torch.full((1, 1, 1, 64), fill_value=torch.finfo(torch.float).min)
         top8_mask[:, :, :, 1:9] = 0.0
         self.top8_mask_11B_64 = ttnn.from_torch(
             top8_mask,
@@ -86,6 +75,7 @@ class TtMoeLayer(torch.nn.Module):
         """
         input_i_1SBH = inputs
         expert_i_HH = self.experts
+        batch = input_i_1SBH.shape[2]
         # get logits for the experts
         gate_logits_1SB8 = ttnn.linear(
             input_i_1SBH,
@@ -99,6 +89,7 @@ class TtMoeLayer(torch.nn.Module):
         # get weights for top-2 experts
         gate_logits_1SB8 = ttnn.add(gate_logits_1SB8, self.top8_mask_11B_64)
         ttl_topk_values, ttl_topk_indices = ttnn.experimental.operations.primary.topk(gate_logits_1SB8, 32)
+        self.top2_mask_11BB = ttnn.repeat(self.top2_mask_11BB, [1, 1, batch / 32, 1])
         ttl_topk_values = ttnn.add(ttl_topk_values, self.top2_mask_11BB)
         mask_B2 = ttnn.eq(self.expert_mask_11BB, ttl_topk_indices)
         weights_1SB1 = ttnn.sum(ttnn.softmax(ttl_topk_values, dim=-1) * mask_B2, dim=3)
@@ -108,7 +99,7 @@ class TtMoeLayer(torch.nn.Module):
         results_11BH = ttnn.mul(weights, weights_1SB1)
 
         # all gather
-        output_11BH_gathered = ttnn.all_gather(results_11BH, dim=2, num_links=1)
+        output_11BH_gathered = ttnn.all_gather(results_11BH, dim=1, num_links=1)
         # sum on each device
-        output_11BH_gathered = ttnn.experimental.operations.primary.matmul(self.reduce_mask, output_11BH_gathered)
+        output_11BH_gathered = ttnn.sum(output_11BH_gathered, dim=1)
         return output_11BH_gathered
