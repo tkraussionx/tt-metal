4422f864725d936051c8e11493440c6bff2507ef
mcw-anasuya <mcw-anair@ext.tenstorrent.com>
2024-06-17 16:54:37 +0000

    #8683: Add bitwise_not op

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_bitwise_not.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_bitwise_not.h
@@ -6,8 +6,8 @@
 
 #include "ckernel.h"
 #include "ckernel_defs.h"
+#include "sfpi.h"
 #include "noc_nonblocking_api.h"
-#include "limits.h"
 
 using namespace sfpi;
 
@@ -15,21 +15,28 @@ namespace ckernel {
 namespace sfpu {
 
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
-inline void calculate_bitwise_xor(const uint value) {
+inline void calculate_bitwise_not(const uint value) {
 #pragma GCC unroll 0
     for (int d = 0; d < ITERATIONS; d++) {
         vInt input = dst_reg[0];
-        vInt v = value;
-        vInt res = input ^ v;
-        v_if(res > INT_MIN && res < 0)
-        {
-            res = 0 - res;
-            res = setsgn(res, v);
+        vInt res;
+
+        v_if(input < 0){
+            vFloat fval = int32_to_float(input);
+            fval = fval + 1;
+            fval = setsgn(fval, 2);
+            res = float_to_int16(fval);
         }
-        v_endif
+        v_else {
+            res = setsgn(input, -1);
+            res = res + 1;
+        }
+        v_endif;
+
         dst_reg[0] = res;
         dst_reg++;
     }
 }
+
 }  // namespace sfpu
 }  // namespace ckernel
