c8181ab1931dbcd0f131f650c037a98661aed905
Sankar Manoj <smanoj@tenstorrent.com>
2024-06-26 16:51:43 +0000

    #9685: Updated Test case

--- tests/ttnn/integration_tests/resnet/test_pad_fold.py
+++ tests/ttnn/integration_tests/resnet/test_pad_fold.py
@@ -7,15 +7,18 @@ from models.utility_functions import pad_and_fold_conv_activation_for_unity_stri
 
 
 def test_pad_fold():
-    torch_input_tensor = torch.randint(-10, 10, (8, 3, 224, 224), dtype=torch.float32)
-
+    torch_input_tensor = torch.rand((8, 3, 224, 224), dtype=torch.float32)
+    # torch_input_tensor = torch.randint(-10,10,(8, 3, 224, 224))
+    print("Input tensor shape is ", torch_input_tensor.shape)
     start_time = time.time()
-    input_tensor = pad_and_fold_conv_activation_for_unity_stride(torch_input_tensor, 3, 3, 2, 2)
-    input_tensor = torch.permute(input_tensor, (0, 2, 3, 1))
-    print("Old time taken is ", time.time() - start_time, "s ")
+    for x in range(10):
+        input_tensor = pad_and_fold_conv_activation_for_unity_stride(torch_input_tensor, 3, 3, 2, 2)
+        input_tensor = torch.permute(input_tensor, (0, 2, 3, 1))
+    print("Old time taken is ", (time.time() - start_time) / 10, "s ")
     start_time = time.time()
-    input_tensor2 = pad_and_fold_act_2(torch_input_tensor, 3, 3, 2, 2)
-    print("New time taken is ", time.time() - start_time, "s ")
+    for x in range(100):
+        input_tensor2 = pad_and_fold_act_2(torch_input_tensor, 3, 3, 2, 2)
+    print("New time taken is ", (time.time() - start_time) * 10, "ms ")
     print(input_tensor.shape)
     print(input_tensor2.shape)
 
