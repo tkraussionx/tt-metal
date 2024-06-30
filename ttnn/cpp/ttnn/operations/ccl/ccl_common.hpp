3eb2e858a37ec79f773c6e578f648fea1e0170e6
--global <--global>
2024-06-30 21:51:13 +0000

    #0: Dispatch EDM kernels as a single program

--- ttnn/cpp/ttnn/operations/ccl/ccl_common.hpp
+++ ttnn/cpp/ttnn/operations/ccl/ccl_common.hpp
@@ -453,7 +453,7 @@ KernelHandle generate_edm_kernel(
    tt::tt_metal::Program& program,
     Device const* device,
     ccl::EriscDatamoverBuilder const& edm_builder,
-    CoreCoord const& eth_core,
+    CoreRangeSet const& eth_cores,
     NOC noc_id);
 
 void generate_edm_kernels_for_ring_or_linear_topology(
@@ -464,6 +464,13 @@ void generate_edm_kernels_for_ring_or_linear_topology(
     std::vector<ccl::EriscDatamoverBuilder> const& counter_clockwise_edm_builders,
     std::optional<uint32_t> receiver_device_id,
     std::optional<uint32_t> sender_device_id);
+
+void set_edm_runtime_args(
+    tt_metal::Program& program,
+    KernelHandle edm_kernel_handle,
+    ccl::EriscDatamoverBuilder const& edm_builder,
+    CoreCoord const& eth_core
+);
 
 ccl::EriscDatamoverBuilder create_erisc_datamover_builder(
     std::size_t num_channels,
