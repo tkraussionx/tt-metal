e18e56cc63e9c2b7b1b2cbd03b1faf6be009527c
jayasuryamaganuru <maganuru.jayasurya@multicoreware.com>
2024-07-18 12:44:10 +0000

    #0: Failed ttnn conv unit tests

--- models/experimental/functional_vgg/tests/test_conv_vgg16.py
+++ models/experimental/functional_vgg/tests/test_conv_vgg16.py
@@ -9,13 +9,12 @@ import ttnn
 from torchvision import models
 from loguru import logger
 from models.utility_functions import comp_allclose, comp_pcc
-from tests.ttnn.utils_for_testing import assert_with_pcc
+from tests.ttnn.utils_for_testing import assert_with_pcc, check_with_pcc_without_tensor_printout
 
 
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 24576}], indirect=True)
 @pytest.mark.parametrize(
-    "batch_size, act_dtype, weight_dtype, math_fidelity",
-    ((1, ttnn.bfloat8_b, ttnn.bfloat8_b, ttnn.MathFidelity.LoFi),),
+    "batch_size, act_dtype, weight_dtype, math_fidelity", ((1, ttnn.bfloat16, ttnn.bfloat16, ttnn.MathFidelity.LoFi),)
 )
 @pytest.mark.parametrize(
     "use_pretrained_weight",
@@ -25,20 +24,12 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
     ],
 )
 @pytest.mark.parametrize(
-    "output_channels, input_channels, input_height, input_width",
+    "input_channels, output_channels, input_height, input_width",
     (
-        (3, 64, 224, 224),
-        (64, 64, 224, 224),
-        (64, 128, 112, 112),
-        (128, 128, 112, 112),
         (128, 256, 56, 56),
-        (256, 256, 56, 56),
         (256, 256, 56, 56),
         (256, 512, 28, 28),
         (512, 512, 28, 28),
-        (512, 512, 28, 28),
-        (512, 512, 14, 14),
-        (512, 512, 14, 14),
         (512, 512, 14, 14),
     ),
 )
@@ -51,8 +42,8 @@ def test_vgg_conv(
     weight_dtype,
     math_fidelity,
     use_pretrained_weight,
-    output_channels,
     input_channels,
+    output_channels,
     input_height,
     input_width,
 ):
@@ -64,27 +55,38 @@ def test_vgg_conv(
         "ACTIVATIONS_DTYPE": act_dtype,
     }
 
-    conv1_config = ttnn.Conv2dConfig(
-        dtype=model_config["ACTIVATIONS_DTYPE"],
-        weights_dtype=model_config["WEIGHTS_DTYPE"],
-        math_fidelity=model_config["MATH_FIDELITY"],
+    shard_grid = ttnn.experimental.tensor.CoreRangeSet(
+        {
+            ttnn.experimental.tensor.CoreRange(
+                ttnn.experimental.tensor.CoreCoord(0, 0),
+                ttnn.experimental.tensor.CoreCoord(7, 0),
+            ),
+        }
+    )
+    conv_config = ttnn.Conv2dConfig(
+        # core_grid=shard_grid,
+        dtype=ttnn.bfloat16,
+        weights_dtype=ttnn.bfloat16,
+        math_fidelity=ttnn.MathFidelity.LoFi,
+        math_approx_mode_enabled=True,
+        fp32_dest_acc_enabled=True,
         activation="relu",
-        deallocate_activation=True,
-        input_channels_alignment=16,
+        deallocate_activation=False,
+        input_channels_alignment=32,
+        reallocate_halo_output=False,
         act_block_h_override=0,
         transpose_shards=True,
+        height_sharding=True,
+        # reshard_if_not_optimal = True,
+        # override_sharding_config=True,
     )
-
-    # use_pretrained_weight=True,
-    # dealloc_input=True,
-    # final_output_mem_config=ttnn.DRAM_MEMORY_CONFIG,
 
     torch_model.to(torch.bfloat16)
     torch_input_tensor = imagenet_sample_input.to(torch.bfloat16)
 
-    conv_p = [output_channels, input_channels, input_height, input_width]
+    conv_p = [input_channels, output_channels, input_height, input_width]
     has_bias = True
-    conv_input_shape = [batch_size, conv_p[0], conv_p[2], conv_p[2]]
+    conv_input_shape = [batch_size, conv_p[0], conv_p[2], conv_p[3]]  # 1, 3, 224, 224
     conv_weight_shape = [conv_p[1], conv_p[0], 3, 3]
     conv_bias_shape = [1, 1, 1, conv_p[1]]
     torch_input_tensor_nchw = torch.randn(conv_input_shape, dtype=torch.bfloat16).float()
@@ -99,17 +101,17 @@ def test_vgg_conv(
         padding=(1, 1),
     )
 
-    weights_dtype = ttnn.bfloat8_b
+    weights_dtype = ttnn.bfloat16
     tt_weight_tensor = ttnn.from_torch(
-        torch_weight_tensor, weights_dtype if weights_dtype != ttnn.bfloat8_b else ttnn.float32
+        torch_weight_tensor, weights_dtype if weights_dtype != ttnn.bfloat8_b else ttnn.bfloat16
     )
     tt_bias_tensor = ttnn.from_torch(
-        torch_bias_tensor, weights_dtype if weights_dtype != ttnn.bfloat8_b else ttnn.float32
+        torch_bias_tensor, weights_dtype if weights_dtype != ttnn.bfloat8_b else ttnn.bfloat16
     )
 
     tt_input_tensor = ttnn.from_torch(torch_input_tensor, ttnn.bfloat16)
 
-    ds_out, _, _, ds_conv_weight_tensor, ds_conv_bias_tensor = ttnn.conv2d(
+    [tt_output_tensor_on_device, out_height, out_width, weights_device, bias_device] = ttnn.conv2d(
         input_tensor=tt_input_tensor,
         weight_tensor=tt_weight_tensor,
         in_channels=conv_p[0],
@@ -122,6 +124,14 @@ def test_vgg_conv(
         batch_size=batch_size,
         input_height=conv_p[2],
         input_width=conv_p[3],
-        conv_config=conv1_config,
+        conv_config=conv_config,
         conv_op_cache={},
     )
+
+    tt_output_tensor = ttnn.from_device(tt_output_tensor_on_device)
+    torch_output_tensor = ttnn.to_torch(tt_output_tensor)
+    torch_output_tensor = torch.permute(torch_output_tensor, (0, 3, 1, 2))
+    torch_output_tensor = torch_output_tensor.reshape(batch_size, output_channels, out_height, out_width)
+
+    passing, pcc_msg = check_with_pcc_without_tensor_printout(torch_output_tensor, torch_out_golden_tensor, pcc=0.99)
+    logger.info(f"PCC: {pcc_msg}")
