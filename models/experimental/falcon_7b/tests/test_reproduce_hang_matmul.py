23926665bacfe83d8e089fbdea990ecd8d952e07
Brian Liu <bliu@tenstorrent.com>
2024-07-18 16:56:40 -0400

    #0: Update test_reproduce_hang_matmul.py for blackhole - Make test parameters dynamic (values set for blackhole grid) - Update test to use ttnn APIs instead of broken tt_lib APIs

--- models/experimental/falcon_7b/tests/test_reproduce_hang_matmul.py
+++ models/experimental/falcon_7b/tests/test_reproduce_hang_matmul.py
@@ -6,15 +6,50 @@ import pytest
 
 import time
 
-import tt_lib as ttl
+import ttnn
 from models.utility_functions import torch2tt_tensor
 import torch
 
 
 # Used to reproduce issue #8665 with matmul 2D (Falcon 7b matmuls)
+# ------- SPECS per core -------
+# input: 128, 576
+# output: 128, 2304 (ie. 4 x 72 tiles)
+INPUT_PER_CORE_HEIGHT = 128
+INPUT_PER_CORE_WIDTH = 576
+OUTPUT_PER_CORE_WIDTH = 2304  # (ie. dense_h_to_4h)
+BLACKHOLE_GRID_Y = 9
+BLACKHOLE_GRID_X = 14
+GRID_Y = BLACKHOLE_GRID_Y
+GRID_X = BLACKHOLE_GRID_X
+
+
 @pytest.mark.parametrize(
     "seq_len, inner_dim, weights_n, per_core_M, per_core_N, in_block_w, out_subblock_h, out_subblock_w, loop_count",
-    ((1024, 4608, 18432, 4, 72, 3, 1, 8, 20000), (1024, 4608, 18432, 4, 72, 3, 1, 1, 20000)),
+    (
+        (
+            INPUT_PER_CORE_HEIGHT * GRID_Y,
+            INPUT_PER_CORE_WIDTH * GRID_X,
+            OUTPUT_PER_CORE_WIDTH * GRID_X,
+            INPUT_PER_CORE_HEIGHT // 32,
+            OUTPUT_PER_CORE_WIDTH // 32,
+            3,
+            1,
+            8,
+            20000,
+        ),
+        (
+            INPUT_PER_CORE_HEIGHT * GRID_Y,
+            INPUT_PER_CORE_WIDTH * GRID_X,
+            OUTPUT_PER_CORE_WIDTH * GRID_X,
+            INPUT_PER_CORE_HEIGHT // 32,
+            OUTPUT_PER_CORE_WIDTH // 32,
+            3,
+            1,
+            1,
+            20000,
+        ),
+    ),
     ids=["ff1-hang", "ff1-pass"],
 )
 def test_reproduce_matmul_2d_hang(
@@ -32,35 +67,35 @@ def test_reproduce_matmul_2d_hang(
 ):
     torch.manual_seed(1234)
 
-    in0_mem_config = ttl.tensor.MemoryConfig(
-        ttl.tensor.TensorMemoryLayout.BLOCK_SHARDED,
-        ttl.tensor.BufferType.L1,
-        ttl.tensor.ShardSpec(
-            ttl.tensor.CoreRangeSet(
+    in0_mem_config = ttnn.MemoryConfig(
+        ttnn.TensorMemoryLayout.BLOCK_SHARDED,
+        ttnn.BufferType.L1,
+        ttnn.ShardSpec(
+            ttnn.CoreRangeSet(
                 {
-                    ttl.tensor.CoreRange(
+                    ttnn.CoreRange(
                         # Volume must match batch size
-                        ttl.tensor.CoreCoord(0, 0),
-                        ttl.tensor.CoreCoord(7, 7),
+                        ttnn.CoreCoord(0, 0),
+                        ttnn.CoreCoord(GRID_X - 1, GRID_Y - 1),
                     ),
                 }
             ),
             [
-                128,
-                576,
+                INPUT_PER_CORE_HEIGHT,
+                INPUT_PER_CORE_WIDTH,
             ],
-            ttl.tensor.ShardOrientation.ROW_MAJOR,
+            ttnn.ShardOrientation.ROW_MAJOR,
             False,
         ),
     )
 
-    in1_mem_config = ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM)
+    in1_mem_config = ttnn.DRAM_MEMORY_CONFIG
 
-    out_mem_config = ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.BLOCK_SHARDED, ttl.tensor.BufferType.L1)
+    out_mem_config = ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG
 
-    in0_dtype = ttl.tensor.DataType.BFLOAT16
-    in1_dtype = ttl.tensor.DataType.BFLOAT8_B
-    out_dtype = ttl.tensor.DataType.BFLOAT16
+    in0_dtype = ttnn.bfloat16
+    in1_dtype = ttnn.bfloat8_b
+    out_dtype = ttnn.bfloat16
 
     a_shape = [1, 1, seq_len, inner_dim]
     b_shape = [1, 1, inner_dim, weights_n]
@@ -68,11 +103,11 @@ def test_reproduce_matmul_2d_hang(
     A = torch.randn(a_shape)
     B = torch.randn(b_shape)
 
-    a_t = torch2tt_tensor(A, device, ttl.tensor.Layout.TILE, in0_mem_config, in0_dtype)
-    b_t = torch2tt_tensor(B, device, ttl.tensor.Layout.TILE, in1_mem_config, in1_dtype)
+    a_t = torch2tt_tensor(A, device, ttnn.Layout.TILE, in0_mem_config, in0_dtype)
+    b_t = torch2tt_tensor(B, device, ttnn.Layout.TILE, in1_mem_config, in1_dtype)
 
-    program_config = ttl.operations.primary.MatmulMultiCoreReuseMultiCastProgramConfig(
-        compute_with_storage_grid_size=(8, 8),
+    program_config = ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+        compute_with_storage_grid_size=(GRID_X, GRID_Y),
         in0_block_w=in_block_w,
         out_subblock_h=out_subblock_h,
         out_subblock_w=out_subblock_w,
@@ -82,36 +117,34 @@ def test_reproduce_matmul_2d_hang(
         fused_activation=None,
     )
 
-    compute_config = ttl.tensor.WormholeComputeKernelConfig(
-        math_fidelity=ttl.tensor.MathFidelity.LoFi,
+    compute_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.LoFi,
         math_approx_mode=False,
         fp32_dest_acc_en=False,
         packer_l1_acc=True,
     )
 
     # First run for a reference output
-    out = ttl.operations.primary.matmul(
+    out = ttnn.matmul(
         a_t,
         b_t,
         program_config=program_config,
-        output_mem_config=out_mem_config,
-        output_dtype=out_dtype,
+        memory_config=out_mem_config,
+        dtype=out_dtype,
         compute_kernel_config=compute_config,
     )
-
-    nd_output_count = 0
 
     start_time = time.time()
 
     # loop_count iterations to test determinism/hang
     for i in range(loop_count):
         out.deallocate(True)
-        out = ttl.operations.primary.matmul(
+        out = ttnn.matmul(
             a_t,
             b_t,
             program_config=program_config,
-            output_mem_config=out_mem_config,
-            output_dtype=out_dtype,
+            memory_config=out_mem_config,
+            dtype=out_dtype,
             compute_kernel_config=compute_config,
         )
 
@@ -120,7 +153,3 @@ def test_reproduce_matmul_2d_hang(
             print(f"Iteration {i} done, time elapsed from the beginning: {seconds:.2f} seconds")
 
     out.deallocate(True)
-
-    print(f"Iterations with nd output: {nd_output_count}")
-
-    assert nd_output_count != 0
