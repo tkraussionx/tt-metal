8f8e55e8401ca4729b45ce3bc561eca1a0e21c1c
Paul Keller <pkeller@tenstorrent.com>
2024-07-29 21:01:35 +0000

    #4984: Reclaim some mem map space->ring buffer
    Turns out ZEROS is still used, so couldn't reclaim that
    This reduces the dead space at low memory and fixes a hack in watcher for
    determining hex value of the jump insn at address 0

--- tt_metal/hostdevcommon/common_runtime_address_map.h
+++ tt_metal/hostdevcommon/common_runtime_address_map.h
@@ -51,7 +51,7 @@ constexpr static std::uint32_t PROFILER_RISC_COUNT = 5;
 static_assert (PROFILER_FULL_HOST_BUFFER_SIZE_PER_RISC > PROFILER_L1_BUFFER_SIZE);
 
 constexpr static std::uint32_t L1_KERNEL_CONFIG_BASE = PROFILER_L1_END_ADDRESS;
-constexpr static std::uint32_t L1_KERNEL_CONFIG_SIZE = 3 * 1024 + L1_ALIGNMENT; // alignment between unique&common RTAs
+constexpr static std::uint32_t L1_KERNEL_CONFIG_SIZE = 4 * 1024 + 256;
 
 constexpr static std::uint32_t IDLE_ERISC_L1_KERNEL_CONFIG_BASE = 32 * 1024;
 
