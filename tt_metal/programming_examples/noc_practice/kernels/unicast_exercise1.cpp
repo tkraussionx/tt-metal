3ea214b4a68a808c8b59887e96befa8cc113e0b5
Dongjin Na <dongjin.na@moreh.io>
2024-08-05 23:58:36 +0900

    #0: Revise unicast_exercise1 kernel

--- tt_metal/programming_examples/noc_practice/kernels/unicast_exercise1.cpp
+++ tt_metal/programming_examples/noc_practice/kernels/unicast_exercise1.cpp
@@ -8,23 +8,23 @@
 void kernel_main() {
     std::uint32_t l1_buffer_addr        = get_arg_val<uint32_t>(0);
 
-    std::uint32_t dram_input_buffer_addr  = get_arg_val<uint32_t>(1);
-    std::uint32_t dram_input_noc_x        = get_arg_val<uint32_t>(2);
-    std::uint32_t dram_input_noc_y        = get_arg_val<uint32_t>(3);
+    std::uint32_t input_dram_buffer_addr  = get_arg_val<uint32_t>(1);
+    std::uint32_t input_dram_noc_x        = get_arg_val<uint32_t>(2);
+    std::uint32_t input_dram_noc_y        = get_arg_val<uint32_t>(3);
 
-    std::uint32_t dram_buffer_output_addr  = get_arg_val<uint32_t>(4);
-    std::uint32_t dram_output_noc_x        = get_arg_val<uint32_t>(5);
-    std::uint32_t dram_output_noc_y        = get_arg_val<uint32_t>(6);
+    std::uint32_t output_dram_buffer_addr  = get_arg_val<uint32_t>(4);
+    std::uint32_t output_dram_noc_x        = get_arg_val<uint32_t>(5);
+    std::uint32_t output_dram_noc_y        = get_arg_val<uint32_t>(6);
 
     std::uint32_t dram_buffer_size      = get_arg_val<uint32_t>(7);
 
-    // DRAM NOC input address
-    std::uint64_t dram_input_buffer_noc_addr = get_noc_addr(dram_input_noc_x, dram_input_noc_y, dram_input_buffer_addr);
-    noc_async_read(dram_input_buffer_noc_addr, l1_buffer_addr, dram_buffer_size);
+    // Read data from DRAM to L1
+    std::uint64_t input_dram_buffer_noc_addr = get_noc_addr(input_dram_noc_x, input_dram_noc_y, input_dram_buffer_addr);
+    noc_async_read(input_dram_buffer_noc_addr, l1_buffer_addr, dram_buffer_size);
     noc_async_read_barrier();
 
-    // DRAM NOC output address
-    std::uint64_t dram_buffer_output_noc_addr = get_noc_addr(dram_output_noc_x, dram_output_noc_y, dram_buffer_output_addr);
-    noc_async_write(l1_buffer_addr, dram_buffer_output_noc_addr, dram_buffer_size);
+    // Write data from L1 to DRAM
+    std::uint64_t output_dram_buffer_noc_addr = get_noc_addr(output_dram_noc_x, output_dram_noc_y, output_dram_buffer_addr);
+    noc_async_write(l1_buffer_addr, output_dram_buffer_noc_addr, dram_buffer_size);
     noc_async_write_barrier();
 }
