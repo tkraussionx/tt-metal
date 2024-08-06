2e5b8c1b41f43905766d6d693b7f88a7d5d8687a
Dongjin Na <dongjin.na@moreh.io>
2024-08-06 16:56:34 +0900

    #0: Revise multicast2 exercise

--- tt_metal/programming_examples/noc_practice/kernels/sender_multicast_exercise2.cpp
+++ tt_metal/programming_examples/noc_practice/kernels/sender_multicast_exercise2.cpp
@@ -15,9 +15,9 @@
 
 void kernel_main() {
     uint32_t l1_buffer_src_addr = get_arg_val<uint32_t>(0);
-    uint32_t dram_input_buffer_addr = get_arg_val<uint32_t>(1);
-    uint32_t dram_input_noc_x = get_arg_val<uint32_t>(2);
-    uint32_t dram_input_noc_y = get_arg_val<uint32_t>(3);
+    uint32_t input_dram_buffer_addr = get_arg_val<uint32_t>(1);
+    uint32_t input_dram_noc_x = get_arg_val<uint32_t>(2);
+    uint32_t input_dram_noc_y = get_arg_val<uint32_t>(3);
     uint32_t dram_buffer_size      = get_arg_val<uint32_t>(4);
     uint32_t l1_buffer_dest_addr = get_arg_val<uint32_t>(5);
     uint32_t dest_noc_x_start = get_arg_val<uint32_t>(6);
@@ -32,9 +32,9 @@ void kernel_main() {
     *(mcast_receiver_semaphore_addr_ptr) = VALID;
     volatile tt_l1_ptr uint32_t* mcast_sender_semaphore_addr_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(mcast_sender_semaphore_addr);
 
-    // read dram input buffer into local L1 buffer
-    uint64_t dram_buffer_input_noc_addr = get_noc_addr(dram_input_noc_x, dram_input_noc_y, dram_input_buffer_addr);
-    noc_async_read(dram_buffer_input_noc_addr, l1_buffer_src_addr, dram_buffer_size);
+    // read input dram buffer into local L1 buffer
+    uint64_t input_dram_buffer_noc_addr = get_noc_addr(input_dram_noc_x, input_dram_noc_y, input_dram_buffer_addr);
+    noc_async_read(input_dram_buffer_noc_addr, l1_buffer_src_addr, dram_buffer_size);
     noc_async_read_barrier();
 
     // This section is reserved for watcher tool practice session.
@@ -56,7 +56,6 @@ void kernel_main() {
     #endif
 
     noc_semaphore_wait(mcast_sender_semaphore_addr_ptr, num_dests);
-    noc_semaphore_set(mcast_sender_semaphore_addr_ptr, 0);
 
     // multicast local L1 buffer to all destination cores
     uint64_t dest_noc_multicast_addr =
