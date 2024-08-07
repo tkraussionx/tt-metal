26ce5a7222358ad9b3d9af8548b31c7eba609743
Dongjin Na <dongjin.na@moreh.io>
2024-08-07 23:28:28 +0900

    #0: Refactor matmul_multicore_reuse kernel

--- tt_metal/programming_examples/matmul_exercise/kernels/compute/matmul_multicore_reuse.cpp
+++ tt_metal/programming_examples/matmul_exercise/kernels/compute/matmul_multicore_reuse.cpp
@@ -22,6 +22,11 @@ void MAIN {
     uint32_t out_subblock_w = get_compile_time_arg_val(9); // inner column block size in tiles
     uint32_t out_subblock_num_tiles = get_compile_time_arg_val(10); // out_subblock_h * out_subblock_w;
 
+    constexpr uint32_t in0_cb_id = tt::CB::c_in0;
+    constexpr uint32_t in1_cb_id = tt::CB::c_in1;
+    constexpr uint32_t out_cb_id = tt::CB::c_out0;
+    constexpr uint32_t mm_partials_cb_id = tt::CB::c_intermed0;
+
     mm_init();
 
     bool spill = num_blocks > 1;
@@ -32,22 +37,21 @@ void MAIN {
     {
         bool last_out = block == (num_blocks-1);
 
-        cb_wait_front(tt::CB::c_in0, in0_block_num_tiles);
-        cb_wait_front(tt::CB::c_in1, in1_block_num_tiles);
+        cb_wait_front(in0_cb_id, in0_block_num_tiles);
+        cb_wait_front(in1_cb_id, in1_block_num_tiles);
         int in0_index_subblock_offset = 0;
         for (uint32_t in0_subblock = 0; in0_subblock < in0_num_subblocks; in0_subblock++) {
             int in1_index_subblock_offset = 0;
             for (uint32_t in1_subblock = 0; in1_subblock < in1_num_subblocks; in1_subblock++) {
 
-                acquire_dst(tt::DstMode::Half);
-
+                tile_regs_acquire();
                 if (enable_reload) {
                     copy_tile_to_dst_init_short();
-                    cb_wait_front(tt::CB::c_intermed0, out_subblock_num_tiles);
+                    cb_wait_front(mm_partials_cb_id, out_subblock_num_tiles);
                     for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
-                        copy_tile(tt::CB::c_intermed0, i, i);
+                        copy_tile(mm_partials_cb_id, i, i);
                     }
-                    cb_pop_front(tt::CB::c_intermed0, out_subblock_num_tiles);
+                    cb_pop_front(mm_partials_cb_id, out_subblock_num_tiles);
                     mm_init_short();
                 }
 
@@ -60,7 +64,7 @@ void MAIN {
                         for (uint32_t inner_dim = 0; inner_dim < in0_block_w; inner_dim++) {
                             int in0_index = in0_index_subblock_offset + in0_index_h_offset + inner_dim;
                             int in1_index = in1_index_subblock_offset + in1_index_inner_dim_offset + w;
-                            matmul_tiles(tt::CB::c_in0, tt::CB::c_in1, in0_index, in1_index, dst_index, false /* transpose */);
+                            matmul_tiles(in0_cb_id, in1_cb_id, in0_index, in1_index, dst_index, false /* transpose */);
                             in1_index_inner_dim_offset += in1_per_core_w;
                         }
                         dst_index++;
@@ -68,28 +72,31 @@ void MAIN {
                     in0_index_h_offset += in0_block_w;
                 }
 
+                tile_regs_commit();
+
+                tile_regs_wait();
                 if (last_out) {
                     // Pack out to output buffer
-                    cb_reserve_back(tt::CB::c_out0, out_subblock_num_tiles);
+                    cb_reserve_back(out_cb_id, out_subblock_num_tiles);
                     for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
-                        pack_tile(i, tt::CB::c_out0);
+                        pack_tile(i, out_cb_id);
                     }
-                    cb_push_back(tt::CB::c_out0, out_subblock_num_tiles);
+                    cb_push_back(out_cb_id, out_subblock_num_tiles);
                 } else {
                     // Wait for tiles in output buffer to be written out since interm and output share memory
                     if (block == 0) {
-                        cb_reserve_back(tt::CB::c_out0, out_num_tiles_to_wait);
+                        cb_reserve_back(out_cb_id, out_num_tiles_to_wait);
                         out_num_tiles_to_wait += out_subblock_num_tiles;
                     }
                     // Move partial result to interm buffer
-                    cb_reserve_back(tt::CB::c_intermed0, out_subblock_num_tiles);
+                    cb_reserve_back(mm_partials_cb_id, out_subblock_num_tiles);
                     for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
-                        pack_tile(i, tt::CB::c_intermed0);
+                        pack_tile(i, mm_partials_cb_id);
                     }
-                    cb_push_back(tt::CB::c_intermed0, out_subblock_num_tiles);
+                    cb_push_back(mm_partials_cb_id, out_subblock_num_tiles);
                 }
 
-                release_dst(tt::DstMode::Half);
+                tile_regs_release();
                 in1_index_subblock_offset += out_subblock_w;
             }
             in0_index_subblock_offset += in0_subblock_num_tiles;
@@ -97,8 +104,8 @@ void MAIN {
 
         if (spill) enable_reload = true;
 
-        cb_pop_front(tt::CB::c_in0, in0_block_num_tiles);
-        cb_pop_front(tt::CB::c_in1, in1_block_num_tiles);
+        cb_pop_front(in0_cb_id, in0_block_num_tiles);
+        cb_pop_front(in1_cb_id, in1_block_num_tiles);
 
     }
 }
