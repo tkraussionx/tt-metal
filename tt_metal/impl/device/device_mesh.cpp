1441749ef4ed007e67ce0232871f52f63ccfed19
Joseph Chu <jchu@sjc-snva-t3019.local.tenstorrent.com>
2024-08-09 00:46:02 -0700

    #0: wip

--- tt_metal/impl/device/device_mesh.cpp
+++ tt_metal/impl/device/device_mesh.cpp
@@ -22,42 +22,41 @@ DeviceMesh::DeviceMesh(const DeviceGrid& device_grid, const DeviceIds &device_id
     TT_ASSERT(num_requested_devices <= device_ids.size(), "User provided insufficient number of device_ids for DeviceMesh");
 
     this->is_galaxy_ = tt::Cluster::instance().is_galaxy_cluster();
-    if (this->is_galaxy_) {
-        // Temp solution until we add algorithmic way to determine chip connectivity
-        // Map col to tunnel depth and row to tunnel count
-        int cluster_tunnel_depth = tt::Cluster::instance().get_mmio_device_max_tunnel_depth(0);
-        int cluster_tunnel_count = tt::Cluster::instance().get_mmio_device_tunnel_count(0);
-        int num_mmio_devices = tt::Cluster::instance().number_of_pci_devices();
-        TT_FATAL(num_cols <= cluster_tunnel_depth and num_rows <= cluster_tunnel_count * num_mmio_devices, "Unsupported Galaxy mesh shape");
+    DeviceIds sorted_ids = device_ids;
+    std::sort(sorted_ids.begin(), sorted_ids.end());
 
-        DeviceIds galaxy_device_ids;
-        for (int mmio_device_id = 0; mmio_device_id < num_mmio_devices; mmio_device_id++) {
-            auto tunnels_from_mmio = tt::Cluster::instance().get_tunnels_from_mmio_device(mmio_device_id);
-            for (uint32_t t = 0; t < tunnels_from_mmio.size(); t++) {
-                if (galaxy_device_ids.size() == num_requested_devices) {
+    // Temp solution until we add algorithmic way to determine chip connectivity
+    // Map col to tunnel depth and row to tunnel count
+    int cluster_tunnel_depth = tt::Cluster::instance().get_mmio_device_max_tunnel_depth(0);
+    int cluster_tunnel_count = tt::Cluster::instance().get_mmio_device_tunnel_count(0);
+    int num_mmio_devices = tt::Cluster::instance().number_of_pci_devices();
+    std::cout << "cluster_tunnel_depth: " << cluster_tunnel_depth << " cluster_tunnel_count: " << cluster_tunnel_count << " num_mmio_devices: " << num_mmio_devices << std::endl;
+    TT_FATAL(num_cols <= cluster_tunnel_depth and num_rows <= cluster_tunnel_count * num_mmio_devices, "Unsupported Galaxy mesh shape");
+
+    DeviceIds galaxy_device_ids;
+    for (int mmio_device_id = 0; mmio_device_id < num_mmio_devices; mmio_device_id++) {
+        auto tunnels_from_mmio = tt::Cluster::instance().get_tunnels_from_mmio_device(mmio_device_id);
+        for (uint32_t t = 0; t < tunnels_from_mmio.size(); t++) {
+            if (galaxy_device_ids.size() == num_requested_devices) {
+                break;
+            }
+            int col_idx = 0;
+            std::size_t start = (this->is_galaxy_) ? 1 : 0; // this is because we skip the gateway wormhole device.
+            for (uint32_t ts = start; ts < tunnels_from_mmio[t].size(); ts++) {
+                galaxy_device_ids.push_back(tunnels_from_mmio[t][ts]);
+                col_idx ++;
+                if (col_idx == num_cols) {
                     break;
-                }
-                int col_idx = 0;
-                for (uint32_t ts = 1; ts < tunnels_from_mmio[t].size(); ts++) {
-                    galaxy_device_ids.push_back(tunnels_from_mmio[t][ts]);
-                    col_idx ++;
-                    if (col_idx == num_cols) {
-                        break;
-                    }
                 }
             }
         }
-        managed_devices = tt::tt_metal::detail::CreateDevices(galaxy_device_ids, num_command_queues, l1_small_size, trace_region_size);
-        for (int i = 0; i < num_requested_devices; i++) {
-            mesh_devices.emplace_back(device_ids[i], managed_devices.at(galaxy_device_ids[i]));
-        }
-        this->view = std::make_unique<tt::tt_metal::DeviceMeshView>(*this);
-    } else {
-        managed_devices = tt::tt_metal::detail::CreateDevices(device_ids, num_command_queues, l1_small_size, trace_region_size);
-        for (int i = 0; i < num_requested_devices; i++) {
-            mesh_devices.emplace_back(device_ids[i], managed_devices.at(device_ids[i]));
-        }
     }
+    tt::log_info("galaxy_device_ids: {}", galaxy_device_ids);
+    managed_devices = tt::tt_metal::detail::CreateDevices(galaxy_device_ids, num_command_queues, l1_small_size, trace_region_size);
+    for (int i = 0; i < num_requested_devices; i++) {
+        mesh_devices.emplace_back(i, managed_devices.at(galaxy_device_ids[i]));
+    }
+    this->view = std::make_unique<tt::tt_metal::DeviceMeshView>(*this);
 
     for (const auto& [dev_id, dev]: mesh_devices) {
         log_debug(tt::LogMetal, "TTNN Dev {}: Metal Dev {}", dev_id, dev->id());
@@ -90,10 +89,6 @@ std::vector<Device*> DeviceMesh::get_devices() const
 }
 
 Device* DeviceMesh::get_device(int row_idx, int col_idx) const {
-    if (not is_galaxy_) {
-        TT_THROW("#10419: Current device mesh does not support indexing by row or col indices.");
-    }
-
     TT_FATAL(
         this->num_rows() != 0 and this->num_cols() != 0,
         "#10419, Current device mesh does not support indexing by row or col indices.");
@@ -104,16 +99,10 @@ Device* DeviceMesh::get_device(int row_idx, int col_idx) const {
 }
 
 std::vector<Device*> DeviceMesh::get_devices_on_row(int row_idx) const {
-    if (not is_galaxy_) {
-        TT_THROW("#10419: Current device mesh does not support indexing by row or col indices.");
-    }
     return this->view->get_devices_on_row(row_idx);
 }
 
 std::vector<Device*> DeviceMesh::get_devices_on_column(int col_idx) const {
-    if (not is_galaxy_) {
-        TT_THROW("#10419: Current device mesh does not support indexing by row or col indices.");
-    }
     return this->view->get_devices_on_column(col_idx);
 }
 
@@ -172,6 +161,12 @@ void DeviceMesh::close_devices() {
     tt::tt_metal::detail::CloseDevices(managed_devices);
     mesh_devices.clear();
     managed_devices.clear();
+}
+
+void DeviceMesh::enable_async(bool enable) {
+    for (auto& [device_id, device] : mesh_devices) {
+        device->enable_async(enable);
+    }
 }
 
 bool validate_worker_modes(const std::vector<Device*>& workers) {
--- tt_metal/impl/device/device_mesh.hpp
+++ tt_metal/impl/device/device_mesh.hpp
@@ -4,11 +4,13 @@
 
 #pragma once
 
-#include <memory>
-#include <vector>
+#include <cstddef>
 #include <map>
+#include <memory>
 #include <optional>
+#include <vector>
 
+#include "tt_metal/hostdevcommon/common_values.hpp"
 #include "tt_metal/impl/device/device.hpp"
 #include "tt_metal/impl/device/device_mesh_view.hpp"
 
@@ -17,24 +19,26 @@ namespace tt::tt_metal {
 using DeviceGrid = std::pair<int, int>;
 using DeviceIds = std::vector<int>;
 
-class DeviceMesh
-{
-public:
-    DeviceGrid device_grid;
-    std::map<chip_id_t, Device *> managed_devices;
-    std::vector<std::pair<int, Device *>> mesh_devices;
-    std::unique_ptr<DeviceMeshView> view;
+constexpr size_t DEFAULT_NUM_COMMAND_QUEUES = 1;
 
-    DeviceMesh(const DeviceGrid &device_grid, const DeviceIds &device_ids, size_t l1_small_size, size_t trace_region_size, size_t num_command_queues);
+class DeviceMesh {
+   public:
+    DeviceMesh(
+        const DeviceGrid &device_grid,
+        const DeviceIds &device_ids,
+        std::size_t l1_small_size = DEFAULT_L1_SMALL_SIZE,
+        std::size_t trace_region_size = DEFAULT_TRACE_REGION_SIZE,
+        std::size_t num_command_queues = DEFAULT_NUM_COMMAND_QUEUES);
     ~DeviceMesh();
 
+    // Deleted copy and move constructors/assignments
     DeviceMesh(const DeviceMesh &) = delete;
     DeviceMesh &operator=(const DeviceMesh &) = delete;
-
     DeviceMesh(DeviceMesh &&) = delete;
     DeviceMesh &operator=(DeviceMesh &&) = delete;
 
-    std::vector<Device*> get_devices() const;
+    // Get device grid information
+    std::vector<Device *> get_devices() const;
     Device *get_device(int logical_device_id) const;
     Device *get_device(int row_idx, int col_idx) const;
     std::vector<Device *> get_devices_on_row(int row_idx) const;
@@ -44,23 +48,29 @@ public:
 
     const DeviceIds get_device_ids() const;
 
+    // Get device grid information
     int num_devices() const;
     int num_rows() const;
     int num_cols() const;
     DeviceGrid shape() const;
 
+    // Convenience methods (assume homogeneous devices)
     CoreCoord compute_with_storage_grid_size() const;
-
     CoreCoord dram_grid_size() const;
-
     tt::ARCH arch() const;
 
+    // Device-specific operations
     void close_devices();
+    void enable_async(bool enable);
 
    private:
-    bool is_galaxy_;
+    DeviceGrid device_grid;
+    DeviceMeshView view;
+
+    std::map<chip_id_t, Device *> managed_devices;
+    std::vector<std::pair<int, Device *>> mesh_devices;
 };
 
-bool validate_worker_modes(const std::vector<Device*>& workers);
+bool validate_worker_modes(const std::vector<Device *> &workers);
 
-} // namespace tt::tt_metal
+}  // namespace tt::tt_metal
