704e671875ce7537dee503ab23f44c3d2a9d2295
Mohamed Bahnas <mbahnas@tenstorrent.com>
2024-08-27 00:54:46 +0000

    #0: Segformer GS Core grid utilization

--- models/experimental/functional_segformer/tt/ttnn_segformer_efficient_selfattention_v2.py
+++ models/experimental/functional_segformer/tt/ttnn_segformer_efficient_selfattention_v2.py
@@ -82,74 +82,31 @@ class TtSegformerEfficientSelfAttention:
             mm_1_x = 4
         elif hidden_states.shape[1] == 1024:
             mm_1_y = 8
-            mm_1_x = 5
+            mm_1_x = 4
         elif hidden_states.shape[1] == 256:
             mm_1_y = 8
-            mm_1_x = 4
+            mm_1_x = 1
 
         hidden_states = ttnn.to_layout(hidden_states, ttnn.TILE_LAYOUT)
-
-        if hidden_states.shape[1] >= 2048:
-            hidden_states = ttnn.to_memory_config(
-                hidden_states,
-                memory_config=ttnn.create_sharded_memory_config(
-                    hidden_states.shape,
-                    core_grid=ttnn.CoreGrid(y=mm_1_y, x=mm_1_x),
-                    strategy=ttnn.ShardStrategy.HEIGHT,
-                    orientation=ttnn.ShardOrientation.ROW_MAJOR,
-                ),
-                dtype=ttnn.bfloat8_b,
-            )
-
-            query = ttnn.linear(
-                hidden_states,
-                parameters.query.weight,
-                bias=parameters.query.bias,
-                memory_config=ttnn.L1_HEIGHT_SHARDED_MEMORY_CONFIG,
+        hidden_states = ttnn.to_memory_config(
+            hidden_states,
+            memory_config=ttnn.create_sharded_memory_config(
+                hidden_states.shape,
                 core_grid=ttnn.CoreGrid(y=mm_1_y, x=mm_1_x),
-                dtype=ttnn.bfloat8_b,
-            )
-        elif hidden_states.shape[1] == 1024:
-            hidden_states = ttnn.to_memory_config(
-                hidden_states,
-                memory_config=ttnn.create_sharded_memory_config(
-                    hidden_states.shape,
-                    core_grid=ttnn.CoreGrid(y=mm_1_y, x=mm_1_x),
-                    strategy=ttnn.ShardStrategy.BLOCK,
-                    orientation=ttnn.ShardOrientation.ROW_MAJOR,
-                ),
-                dtype=ttnn.bfloat8_b,
-            )
+                strategy=ttnn.ShardStrategy.HEIGHT,
+                orientation=ttnn.ShardOrientation.ROW_MAJOR,
+            ),
+            dtype=ttnn.bfloat8_b,
+        )
 
-            query = ttnn.linear(
-                hidden_states,
-                parameters.query.weight,
-                bias=parameters.query.bias,
-                memory_config=ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG,
-                core_grid=ttnn.CoreGrid(y=mm_1_y, x=mm_1_x),
-                dtype=ttnn.bfloat8_b,
-            )
-        elif hidden_states.shape[1] == 256:
-            hidden_states = ttnn.to_memory_config(
-                hidden_states,
-                memory_config=ttnn.create_sharded_memory_config(
-                    hidden_states.shape,
-                    core_grid=ttnn.CoreGrid(y=mm_1_y, x=mm_1_x),
-                    strategy=ttnn.ShardStrategy.BLOCK,
-                    orientation=ttnn.ShardOrientation.ROW_MAJOR,
-                ),
-                dtype=ttnn.bfloat8_b,
-            )
-
-            query = ttnn.linear(
-                hidden_states,
-                parameters.query.weight,
-                bias=parameters.query.bias,
-                memory_config=ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG,
-                core_grid=ttnn.CoreGrid(y=mm_1_y, x=mm_1_x),
-                dtype=ttnn.bfloat8_b,
-            )
-
+        query = ttnn.linear(
+            hidden_states,
+            parameters.query.weight,
+            bias=parameters.query.bias,
+            memory_config=ttnn.L1_HEIGHT_SHARDED_MEMORY_CONFIG,
+            core_grid=ttnn.CoreGrid(y=mm_1_y, x=mm_1_x),
+            dtype=ttnn.bfloat8_b,
+        )
         hidden_states = ttnn.to_memory_config(hidden_states, ttnn.L1_MEMORY_CONFIG, dtype=ttnn.bfloat16)
         query = ttnn.to_memory_config(query, ttnn.L1_MEMORY_CONFIG, dtype=ttnn.bfloat16)
         query_layer = self.transpose_for_scores(query)
