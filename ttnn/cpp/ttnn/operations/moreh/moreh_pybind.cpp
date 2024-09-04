6bf0de2c5ecd6154b99466197b38f6d589c033cc
thanhnguyen-moreh <wally.nguyensub@gmail.com>
2024-09-04 14:30:02 +0000

    #12207: Port moreh_dot to ttnn

--- ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
@@ -6,10 +6,12 @@
 
 #include "ttnn/operations/moreh/moreh_adam/moreh_adam_pybind.hpp"
 #include "ttnn/operations/moreh/moreh_arange/moreh_arange_pybind.hpp"
+#include "ttnn/operations/moreh/moreh_dot_op/moreh_dot_pybind.hpp"
 
 namespace ttnn::operations::moreh {
 void bind_moreh_operations(py::module &module) {
     moreh_arange::bind_moreh_arange_operation(module);
     moreh_adam::bind_moreh_adam_operation(module);
+    moreh_dot::bind_moreh_dot_operation(module);
 }
 }  // namespace ttnn::operations::moreh
