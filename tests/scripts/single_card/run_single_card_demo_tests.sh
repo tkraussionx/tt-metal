0d563f1c42884738634806a7c1565106080ea114
Austin Ho <aho@tenstorrent.com>
2024-09-06 13:16:46 +0000

    #0: Refactor Resnet files to match other model's folder/file structure and add tg, tgg tests/results

--- tests/scripts/single_card/run_single_card_demo_tests.sh
+++ tests/scripts/single_card/run_single_card_demo_tests.sh
@@ -17,7 +17,7 @@ run_common_func_tests() {
   WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto --disable-warnings models/demos/metal_BERT_large_11/demo/demo.py -k batch_8
 
   # Resnet
-  WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto --disable-warnings models/demos/ttnn_resnet/demo/demo.py
+  WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto --disable-warnings models/demos/wormhole/resnet50/demo/demo.py
 }
 
 run_common_perf_tests(){
