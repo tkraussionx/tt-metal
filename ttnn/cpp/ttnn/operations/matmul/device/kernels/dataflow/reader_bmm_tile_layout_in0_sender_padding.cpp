889f11fe6cc18a969181060bd6d6841076acc3f9
Milos Trajkovic <mtrajkovic@lab-100.yyz1.tenstorrent.com>
2024-09-09 22:23:30 -0400

    #0: Added some dbg prints along L1

--- ttnn/cpp/ttnn/operations/matmul/device/kernels/dataflow/reader_bmm_tile_layout_in0_sender_padding.cpp
+++ ttnn/cpp/ttnn/operations/matmul/device/kernels/dataflow/reader_bmm_tile_layout_in0_sender_padding.cpp
@@ -7,6 +7,8 @@
 #include "dataflow_api.h"
 #include "hostdevcommon/common_values.hpp"
 
+volatile uint32_t* dbg_dump_ncrisc = (volatile uint32_t*)0x15240;
+
 void kernel_main() {
     // in0 tensor args
     const uint32_t in0_tensor_addr = get_arg_val<uint32_t>(0);
@@ -98,6 +100,9 @@ void kernel_main() {
     for (uint32_t b = 0; b < batch; ++b) {
         uint32_t in0_tensor_current_block_start_tile_id = in0_tensor_start_tile_id;
         for (uint32_t block = 0; block < num_blocks; ++block) {
+
+            *(dbg_dump_ncrisc+0) = block;
+
 #ifndef IN0_SHARDED
             // Operand 0
             cb_reserve_back(cb_id_in0, in0_block_num_tiles);
@@ -106,6 +111,9 @@ void kernel_main() {
 #ifndef SKIP_MCAST
             uint32_t in0_start_address = l1_write_addr_in0;  // copy start address of block, to be used for mcasting
 #endif
+
+            *(dbg_dump_ncrisc+1) = l1_write_addr_in0;
+            *(dbg_dump_ncrisc+2) = 0x00baba01;
 
             // Copy in0 block into CB, as the default kernel
             uint32_t in0_tensor_row_start_tile_id = in0_tensor_current_block_start_tile_id;
@@ -122,8 +130,12 @@ void kernel_main() {
             }
             in0_tensor_current_block_start_tile_id += in0_tensor_next_block_stride;
 
+            *(dbg_dump_ncrisc+2) = 0x04baba01;
+
             // Barrier! make sure the reads are done
             noc_async_read_barrier();
+
+            *(dbg_dump_ncrisc+2) = 0x08baba01;
 #else
             if constexpr (extract_shard_sub_blocks) {
                 // Operand 0
@@ -149,10 +161,17 @@ void kernel_main() {
 #endif
 
 #ifndef SKIP_MCAST
+
+            *(dbg_dump_ncrisc+2) = 0x0Cbaba01;
+            *(dbg_dump_ncrisc+3) = (uint32_t)in0_mcast_sender_semaphore_addr_ptr;
+            *(dbg_dump_ncrisc+4) = in0_mcast_num_dests;
+
             // wait until all in0 mcast destinations have atomically incremented the in0 semaphore_addr (i.e. its value
             // should be in0_mcast_num_dests), then reset the semaphore_addr value back to zero for the next block
             noc_semaphore_wait(in0_mcast_sender_semaphore_addr_ptr, in0_mcast_num_dests);
             noc_semaphore_set(in0_mcast_sender_semaphore_addr_ptr, 0);
+
+            *(dbg_dump_ncrisc+2) = 0x0Dbaba01;
 
             // Now we have the block in the CB address, we can mcast to dests!
             uint64_t in0_multicast_data_addr = in0_multicast_data_noc | in0_start_address;
@@ -160,6 +179,8 @@ void kernel_main() {
             // num_dests must not include source, since we are NOT really doing a local copy!
             noc_async_write_multicast(
                 in0_start_address, in0_multicast_data_addr, in0_block_size_bytes, in0_mcast_num_cores, false, false);
+
+            *(dbg_dump_ncrisc+2) = 0x0Ebaba01;
 
             // Note: no need for write barrier, since these two multicasts are done on the same noc id, same vc, same
             // cmd_buf Also, this only works because we are setting VCs statically (using NOC_CMD_STATIC_VC).
@@ -172,10 +193,13 @@ void kernel_main() {
                 in0_mcast_num_cores,
                 false,
                 false);
+
+            *(dbg_dump_ncrisc+2) = 0x0Fbaba01;
 #endif
 
 #ifndef IN0_SHARDED
             cb_push_back(cb_id_in0, in0_block_num_tiles);
+            *(dbg_dump_ncrisc+2) = 0x08baba02;
 #else
             if constexpr (extract_shard_sub_blocks) {
                 cb_push_back(cb_id_in0, in0_block_num_tiles);
