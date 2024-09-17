19278889cb75d8be1b7b7e26788ba8d81ebbc48d
Jay Kruer <jkruer@tenstorrent.com>
2024-09-17 03:07:47 +0000

    #0: split for bert: front/back padding version

--- ttnn/cpp/ttnn/operations/data_movement/split/split.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/split/split.cpp
@@ -4,6 +4,7 @@
 
 
 #include "ttnn/common/constants.hpp"
+#include "ttnn/operations/data_movement/pad/pad.hpp"
 #include "ttnn/run_operation.hpp"
 #include "device/split_op.hpp"
 #include "ttnn/cpp/ttnn/operations/data_movement/reshape/reshape.hpp"
@@ -58,30 +59,76 @@ std::vector<Tensor> split_dim_two_chunks_tiled(
     results.reserve(2); // two chunks
 
     Tensor preprocessed_tensor = Tensor(input_tensor);
+    const bool needs_batch_dim = input_shape.size() < 4;
+    const bool needs_transpose = dim != 3;
+    const bool needs_bilateral_padding = input_shape.without_padding()[dim] < tt::constants::TILE_WIDTH;
 
-    if (input_shape.size() < 4) {
+    if (needs_batch_dim) {
         const int C = input_shape[0], H = input_shape[1], W = input_shape[2];
         std::cout << "Adding batch dim" << std::endl;
-        preprocessed_tensor = ttnn::operations::core::reshape<4>(input_tensor, {1, C, H, W});
+        input_shape = {1, C, H, W};
+        preprocessed_tensor = input_tensor.reshape(input_shape);
         dim += 1; // since we added added a dim to the front
     }
 
-    if (dim != 3) {
+    if (needs_transpose) {
         preprocessed_tensor = ttnn::transpose(preprocessed_tensor, dim, 3, mem_config);
+        std::swap(input_shape[dim], input_shape[3]);
+    }
+
+    uint32_t pad_amount;
+    if (needs_bilateral_padding) {
+        auto unpadded_shape = preprocessed_tensor.get_legacy_shape().without_padding();
+        pad_amount = tt::constants::TILE_WIDTH - (unpadded_shape[3] / 2);
+        TT_FATAL(unpadded_shape[3] % 2 == 0, "Split dim must be divisble by 2.");
+        uint32_t pad_amount = tt::constants::TILE_WIDTH - (unpadded_shape[3] / 2);
+        preprocessed_tensor = preprocessed_tensor.to(Layout::ROW_MAJOR);
+        preprocessed_tensor = preprocessed_tensor.unpad_from_tile(unpadded_shape);
+
+        uint32_t queue_id = 0;
+        std::vector<std::pair<uint32_t, uint32_t>> pad_spec = {{pad_amount, pad_amount}};
+        preprocessed_tensor = ttnn::pad(queue_id,
+                                        preprocessed_tensor,
+                                        pad_spec,
+                                        0.0,
+                                        true,
+                                        std::nullopt); // pad last dim bilaterally by pad amount
+
+        TT_FATAL(preprocessed_tensor.get_shape()[3] / tt::constants::TILE_WIDTH == 2, "Should have two splits here.");
     }
 
     std::vector<Tensor> splits = split_last_dim_two_chunks_tiled(preprocessed_tensor, mem_config);
 
-    auto post_proc = [&dim,
-                      &mem_config,
-                      &input_shape](const Tensor &split) {
+    auto post_proc = [&](const Tensor &split, int split_idx) {
         Tensor res = Tensor(split);
 
-        if (dim != 3) {
+        if (needs_bilateral_padding) {
+            auto split_shape = split.get_shape();
+            const int N = split_shape[0], C = split_shape[1], H = split_shape[2], W = split_shape[3];
+            std::vector<uint32_t> split_start = {0,0,0,0};
+            std::vector<uint32_t> split_end = {0,0,0,0};
+
+            Tensor rm_split = split.to(Layout::ROW_MAJOR); // convert to row major for unpad
+            if (split_idx == 0) {
+                // padding on the left
+                split_start = {0,0,0,pad_amount};
+                split_end = {N, C, H, W+pad_amount-1};
+            } else if (split_idx == 1) {
+                // padding on the right
+                split_start = {0,0,0,0};
+                split_end = {N,C,H,W-1};
+            } else {
+                TT_FATAL(false, "Too many splits!");
+            }
+            res = rm_split.unpad(tt::tt_metal::Shape(split_start), tt::tt_metal::Shape(split_end));
+        }
+
+
+        if (needs_transpose) {
             res = ttnn::transpose(res, dim, 3, mem_config);
         }
 
-        if (input_shape.size() < 4) {
+        if (needs_batch_dim) {
             auto s_shape = split.get_shape();
             const int C = s_shape[1], H = s_shape[2], W = s_shape[3];
 
@@ -89,10 +136,14 @@ std::vector<Tensor> split_dim_two_chunks_tiled(
             res = sans_batch_dim;
         }
 
+        res = res.pad_to_tile(0.0); // ensure we return a tiled tensor.
+
         return res;
     };
 
-    std::transform(splits.begin(), splits.end(), splits.begin(), post_proc);
+    for (int i = 0; i < splits.size(); i++) {
+        results.push_back(post_proc(splits[i], i));
+    }
 
     return splits;
 }
