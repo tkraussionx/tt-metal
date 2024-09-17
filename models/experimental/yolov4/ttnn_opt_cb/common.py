4924d2bb196dfa8e195cd5ee34b477394b7404db
punith.sekar <punith.sekar@multicorewareinc.com>
2024-09-17 05:52:33 +0000

    #0: Initial implementation

---
+++ models/experimental/yolov4/ttnn_opt_cb/common.py
@@ -0,0 +1,97 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import ttnn
+
+
+def fold_bn_to_conv_weights_bias(model, path):
+    bn_weight = model[path + ".conv.1.weight"].unsqueeze(1).unsqueeze(1).unsqueeze(1)
+    bn_running_var = model[path + ".conv.1.running_var"].unsqueeze(1).unsqueeze(1).unsqueeze(1)
+
+    weight = model[path + ".conv.0.weight"]
+    weight = (weight / torch.sqrt(bn_running_var)) * bn_weight
+
+    bn_running_mean = model[path + ".conv.1.running_mean"].unsqueeze(1).unsqueeze(1).unsqueeze(1)
+    bn_bias = model[path + ".conv.1.bias"].unsqueeze(1).unsqueeze(1).unsqueeze(1)
+
+    bias = -(bn_weight) * (bn_running_mean / torch.sqrt(bn_running_var)) + bn_bias
+
+    bias = bias.reshape(1, 1, 1, -1)
+    return (
+        ttnn.from_torch(weight, dtype=ttnn.bfloat16),
+        ttnn.from_torch(bias, dtype=ttnn.bfloat16),
+    )
+
+
+class Conv:
+    def __init__(
+        self,
+        model,
+        path,
+        input_params,
+        conv_params,
+        *,
+        act_block_h=None,
+        reshard=False,
+        deallocate=False,
+        height_sharding=True,
+        activation="",
+        fused_op=True,
+    ) -> None:
+        if fused_op:
+            self.weights, self.bias = fold_bn_to_conv_weights_bias(model, path)
+        else:
+            weight = model[path + ".conv.0.weight"]
+            bias = model[path + ".conv.0.bias"]
+            self.weights = ttnn.from_torch(weight, dtype=ttnn.bfloat16)
+            bias = bias.reshape(1, 1, 1, -1)
+            self.bias = ttnn.from_torch(bias, dtype=ttnn.bfloat16)
+        self.input_params = input_params
+        self.kernel_size = (self.weights.shape[2], self.weights.shape[3])
+        self.conv_params = conv_params
+        self.out_channels = self.weights.shape[0]
+        self.act_block_h = act_block_h
+        self.reshard = reshard
+        self.shard_layout = (
+            ttnn.TensorMemoryLayout.HEIGHT_SHARDED if height_sharding else ttnn.TensorMemoryLayout.BLOCK_SHARDED
+        )
+        self.deallocate = deallocate
+        self.activation = activation
+
+    def __call__(self, device, input_tensor):
+        conv_config = ttnn.Conv2dConfig(
+            dtype=ttnn.bfloat16,
+            weights_dtype=ttnn.bfloat16,
+            math_fidelity=ttnn.MathFidelity.HiFi4,
+            activation=self.activation,
+            shard_layout=self.shard_layout,
+            math_approx_mode_enabled=True,
+            fp32_dest_acc_enabled=False,
+            packer_l1_accum_enabled=False,
+            input_channels_alignment=16 if self.input_params[3] < 16 else 32,
+            transpose_shards=False,
+            reshard_if_not_optimal=self.reshard,
+            deallocate_activation=self.deallocate,
+            reallocate_halo_output=False,
+        )
+        if self.act_block_h is not None:
+            conv_config.act_block_h_override = self.act_block_h
+
+        [output_tensor, _out_height, _out_width, self.weights, self.bias] = ttnn.conv2d(
+            input_tensor=input_tensor,
+            weight_tensor=self.weights,
+            bias_tensor=self.bias,
+            in_channels=self.input_params[3],
+            out_channels=self.out_channels,
+            device=device,
+            kernel_size=self.kernel_size,
+            stride=(self.conv_params[0], self.conv_params[1]),
+            padding=(self.conv_params[2], self.conv_params[3]),
+            batch_size=self.input_params[0],
+            input_height=self.input_params[1],
+            input_width=self.input_params[2],
+            conv_config=conv_config,
+        )
+        return output_tensor
