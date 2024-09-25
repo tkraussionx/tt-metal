cf5a53a317674c4555e9b24d7ac3c0e565cc27a5
Tom Stesco <tstesco@tenstorrent.com>
2024-09-25 01:57:45 +0000

    add loglikelihood_continuous for MMLU, clean up code formatting and imports

--- models/demos/t3000/llama3_70b/demo/lm_eval_llama3.py
+++ models/demos/t3000/llama3_70b/demo/lm_eval_llama3.py
@@ -5,16 +5,10 @@ import os
 from unittest.mock import patch
 
 import lm_eval.evaluator as evaluator
-import lm_eval.tasks as tasks
 from lm_eval.loggers import EvaluationTracker
-from lm_eval.evaluator_utils import print_writeout
 
 from lm_eval.utils import (
-    eval_logger,
     handle_non_serializable,
-    hash_string,
-    positional_deprecated,
-    simple_parse_args_string,
 )
 
 from models.demos.t3000.llama2_70b.reference.llama.llama.tokenizer3 import Tokenizer3
@@ -23,7 +17,7 @@ from models.demos.t3000.llama2_70b.tt.llama_common import (
     setup_llama_env,
 )
 from models.demos.utils.tenstorrent_lm import TenstorrentLM
-from models.demos.t3000.llama3_70b.demo.lm_backend import PrefillDecodeBackend, build_generator
+from models.demos.t3000.llama3_70b.demo.lm_backend import PrefillDecodeBackend
 
 """
 This script uses the as a library (see https://github.com/EleutherAI/lm-evaluation-harness)
@@ -42,14 +36,22 @@ def mock_build_generator(model_args, tt_args, *args, **kwargs):
             self.max_seq_len = max_seq_len
 
         def prefill_forward_single_user(
-            self, tokens: torch.Tensor, start_pos: int, user_id: int, last_token_idx=None, page_table=None, kv_cache=None,
+            self,
+            tokens: torch.Tensor,
+            start_pos: int,
+            user_id: int,
+            last_token_idx=None,
+            page_table=None,
+            kv_cache=None,
         ):
-            return self.decode_forward(
-                tokens=tokens, start_pos=start_pos
-            )
+            return self.decode_forward(tokens=tokens, start_pos=start_pos)
 
         def decode_forward(
-            self, tokens: torch.Tensor, start_pos: int, page_table=None, kv_cache=None,
+            self,
+            tokens: torch.Tensor,
+            start_pos: int,
+            page_table=None,
+            kv_cache=None,
         ):
             assert len(tokens.shape) == 2
             batch, seqlen = tokens.shape
@@ -62,7 +64,7 @@ def mock_build_generator(model_args, tt_args, *args, **kwargs):
             logits = torch.randn((batch, seqlen, 128256))
             # send a token every period loops
             EOT_ID = 128009
-            EOS_ID = 128001
+            # EOS_ID = 128001
             send_index = 200
             send_token = EOT_ID
             if start_pos is not None:
@@ -78,7 +80,6 @@ def mock_build_generator(model_args, tt_args, *args, **kwargs):
             # simulate forward latency
             time.sleep(max(simulated_duration - actual_duration, 0))
             return logits
-
 
     class MockGenerator:
         def __init__(self, tokenizer, model_args):
@@ -98,12 +99,10 @@ def get_model_backend(mock_model=False):
     model_config, ckpt_dir, tokenizer_path, cache_path = setup_llama_env(
         llama_version=llama_version,
     )
-    
+
     if mock_model:
         with patch.object(PrefillDecodeBackend, "init_tt_metal_device", return_value=None):
-            with patch(
-                "models.demos.t3000.llama3_70b.demo.lm_backend.build_generator", new=mock_build_generator
-            ):
+            with patch("models.demos.t3000.llama3_70b.demo.lm_backend.build_generator", new=mock_build_generator):
                 model_backend = PrefillDecodeBackend(
                     model_version="meta-llama/Meta-Llama-3.1-70B-Instruct",
                     batch_size=32,
@@ -127,15 +126,15 @@ def main():
     # -----------------------------------
     # configuration:
     # -----------------------------------
-    # tasks = ["mmlu_econometrics", "mmlu_high_school_statistics"]
+    # tasks = ["mmlu_econometrics"]
     # tasks = ["mmlu"]
-    # tasks = ["gpqa_main_cot_zeroshot"]
     # tasks = ["ifeval"]
+    # tasks = ["gpqa_main_cot_zeroshot"]
     tasks = ["ifeval", "gpqa_main_cot_zeroshot"]
     eval_output_fname = "eval_output"
-    limit = None        # limit the number of samples per task
+    limit = None  # limit the number of samples per task
     log_samples = True  # log samples and outputs to file
-    mock_model = False   # use random logits model for testing
+    mock_model = False  # use random logits model for testing
     num_fewshot = None  # number of fewshot samples (task dependent)
     # -----------------------------------
     cache_path = os.environ.get("CACHE_ROOT")
@@ -144,17 +143,17 @@ def main():
     if "ifeval" in tasks:
         # download nltk punkt tokenizer if not available
         import nltk
+
         try:
-            nltk.data.find('tokenizers/punkt')
+            nltk.data.find("tokenizers/punkt")
         except LookupError:
-            nltk.download('punkt')
+            nltk.download("punkt")
 
     evaluation_tracker = EvaluationTracker(output_path=eval_output_fpath)
     model_backend, tokenizer = get_model_backend(mock_model=mock_model)
     # pretrained must be the hugginface pretrained model name
     # see: https://huggingface.co/meta-llama/Meta-Llama-3.1-70B-Instruct
     pretrained = "meta-llama/Meta-Llama-3.1-70B-Instruct"
-    model = None
     lm = TenstorrentLM(
         model_backend=model_backend,
         pretrained=pretrained,
@@ -178,18 +177,13 @@ def main():
     if results is not None:
         if log_samples:
             samples = results.pop("samples")
-        dumped = json.dumps(
-            results, indent=2, default=handle_non_serializable, ensure_ascii=False
-        )
-        # print(dumped)
-        evaluation_tracker.save_results_aggregated(
-            results=results, samples=samples if log_samples else None
-        )
+        evaluation_tracker.save_results_aggregated(results=results, samples=samples if log_samples else None)
         if log_samples:
             for task_name, config in results["configs"].items():
-                evaluation_tracker.save_results_samples(
-                    task_name=task_name, samples=samples[task_name]
-                )
+                evaluation_tracker.save_results_samples(task_name=task_name, samples=samples[task_name])
+
+        # dumped = json.dumps(results, indent=2, default=handle_non_serializable, ensure_ascii=False)
+        # print(dumped)
 
 
 if __name__ == "__main__":
