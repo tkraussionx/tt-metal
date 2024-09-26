2ffd3579b699e9095d9fd016626012862251a1bf
Almeet Bhullar <abhullar@tenstorrent.com>
2024-09-26 03:01:08 +0000

    #0: Modify matmul workload to run on 11x10 grid

--- models/experimental/falcon_40b/tests/test_reproduce_nd_matmul.py
+++ models/experimental/falcon_40b/tests/test_reproduce_nd_matmul.py
@@ -4,6 +4,7 @@
 
 from loguru import logger
 import pytest
+import time
 
 import ttnn
 from models.utility_functions import comp_pcc, tt2torch_tensor, torch2tt_tensor
@@ -109,19 +110,57 @@ def test_reproduce_matmul_1d(
     assert nd_output_count != 0
 
 
+# Used to reproduce issue #8665 with matmul 2D (Falcon 7b matmuls)
+# ------- SPECS per core -------
+# input: 128, 576
+# output: 128, 2304 (ie. 4 x 72 tiles)
+INPUT_PER_CORE_HEIGHT = 128
+INPUT_PER_CORE_WIDTH = 576
+OUTPUT_PER_CORE_WIDTH = 2304  # (ie. dense_h_to_4h)
+BLACKHOLE_GRID_Y = 10
+BLACKHOLE_GRID_X = 11
+GRID_Y = BLACKHOLE_GRID_Y
+GRID_X = BLACKHOLE_GRID_X
+
+
 # Used to reproduce issue #7066 with matmul 2D (Falcon 40b matmuls)
 @pytest.mark.parametrize(
     "seq_len, inner_dim, weights_n, per_core_M, per_core_N, in_block_w, out_subblock_h, out_subblock_w, loop_count",
     (
-        (2048, 8192, 4096, 8, 16, 8, 1, 4, 100),
-        (2048, 32768, 1024, 8, 4, 16, 1, 4, 100),
-        (2048, 8192, 1024, 8, 4, 16, 1, 4, 100),
+        (
+            INPUT_PER_CORE_HEIGHT * GRID_Y,
+            INPUT_PER_CORE_WIDTH * GRID_X,
+            OUTPUT_PER_CORE_WIDTH * GRID_X,
+            INPUT_PER_CORE_HEIGHT // 32,
+            OUTPUT_PER_CORE_WIDTH // 32,
+            3,
+            1,
+            8,
+            20000,
+        ),
+        (
+            INPUT_PER_CORE_HEIGHT * GRID_Y,
+            INPUT_PER_CORE_WIDTH * GRID_X,
+            OUTPUT_PER_CORE_WIDTH * GRID_X,
+            INPUT_PER_CORE_HEIGHT // 32,
+            OUTPUT_PER_CORE_WIDTH // 32,
+            3,
+            1,
+            1,
+            20000,
+        ),
     ),
-    ids=[
-        "mlp_4h_seq_len2048",
-        "mlp_h_seq_len2048",
-        "attn_seq_len2048",
-    ],
+    ids=["ff1-hang", "ff1-pass"],
+    # (
+    #     (2048, 8192, 4096, 8, 16, 8, 1, 4, 100),
+    #     (2048, 32768, 1024, 8, 4, 16, 1, 4, 100),
+    #     (2048, 8192, 1024, 8, 4, 16, 1, 4, 100),
+    # ),
+    # ids=[
+    #     "mlp_4h_seq_len2048",
+    #     "mlp_h_seq_len2048",
+    #     "attn_seq_len2048",
+    # ],
 )
 def test_reproduce_matmul_2d(
     device,
@@ -137,13 +176,35 @@ def test_reproduce_matmul_2d(
 ):
     torch.manual_seed(1234)
 
-    in0_mem_config = ttnn.DRAM_MEMORY_CONFIG
-    in1_mem_config = ttnn.DRAM_MEMORY_CONFIG
-    out_mem_config = ttnn.DRAM_MEMORY_CONFIG
+    in0_mem_config = ttnn.MemoryConfig(
+        ttnn.TensorMemoryLayout.BLOCK_SHARDED,
+        ttnn.BufferType.L1,
+        ttnn.ShardSpec(
+            ttnn.CoreRangeSet(
+                {
+                    ttnn.CoreRange(
+                        # Volume must match batch size
+                        ttnn.CoreCoord(0, 0),
+                        ttnn.CoreCoord(GRID_X - 1, GRID_Y - 1),
+                    ),
+                }
+            ),
+            [
+                INPUT_PER_CORE_HEIGHT,
+                INPUT_PER_CORE_WIDTH,
+            ],
+            ttnn.ShardOrientation.ROW_MAJOR,
+            False,
+        ),
+    )
 
-    in0_dtype = ttnn.bfloat8_b
+    in1_mem_config = ttnn.DRAM_MEMORY_CONFIG
+
+    out_mem_config = ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG
+
+    in0_dtype = ttnn.bfloat16
     in1_dtype = ttnn.bfloat8_b
-    out_dtype = ttnn.bfloat8_b
+    out_dtype = ttnn.bfloat16
 
     a_shape = [1, 1, seq_len, inner_dim]
     b_shape = [1, 1, inner_dim, weights_n]
@@ -151,11 +212,11 @@ def test_reproduce_matmul_2d(
     A = torch.randn(a_shape)
     B = torch.randn(b_shape)
 
-    a_t = torch2tt_tensor(A, device, ttnn.TILE_LAYOUT, in0_mem_config, in0_dtype)
-    b_t = torch2tt_tensor(B, device, ttnn.TILE_LAYOUT, in1_mem_config, in1_dtype)
+    a_t = torch2tt_tensor(A, device, ttnn.Layout.TILE, in0_mem_config, in0_dtype)
+    b_t = torch2tt_tensor(B, device, ttnn.Layout.TILE, in1_mem_config, in1_dtype)
 
     program_config = ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
-        compute_with_storage_grid_size=(8, 8),
+        compute_with_storage_grid_size=(GRID_X, GRID_Y),
         in0_block_w=in_block_w,
         out_subblock_h=out_subblock_h,
         out_subblock_w=out_subblock_w,
@@ -165,10 +226,10 @@ def test_reproduce_matmul_2d(
         fused_activation=None,
     )
 
-    compute_config = ttnn.WormholeComputeKernelConfig(
+    compute_config = ttnn.types.BlackholeComputeKernelConfig(
         math_fidelity=ttnn.MathFidelity.LoFi,
-        math_approx_mode=True,
-        fp32_dest_acc_en=True,
+        math_approx_mode=False,
+        fp32_dest_acc_en=False,
         packer_l1_acc=True,
     )
 
@@ -181,14 +242,12 @@ def test_reproduce_matmul_2d(
         dtype=out_dtype,
         compute_kernel_config=compute_config,
     )
-    ref_out = tt2torch_tensor(out)
 
-    nd_output_count = 0
+    start_time = time.time()
 
     # loop_count iterations to test determinism/hang
-    for _ in range(loop_count):
+    for i in range(loop_count):
         out.deallocate(True)
-
         out = ttnn.matmul(
             a_t,
             b_t,
@@ -198,13 +257,8 @@ def test_reproduce_matmul_2d(
             compute_kernel_config=compute_config,
         )
 
-        pt_out = tt2torch_tensor(out)
+        if i % 100 == 0:
+            seconds = time.time() - start_time
+            print(f"Iteration {i} done, time elapsed from the beginning: {seconds:.2f} seconds")
 
-        _, output_pcc = comp_pcc(ref_out, pt_out, 1)
-        nd_output_count += 1 if output_pcc != 1 else 0
-
-        logger.debug(f"Output pcc={output_pcc}")
-
-    print(f"Iterations with nd output: {nd_output_count}")
-
-    assert nd_output_count != 0
+    out.deallocate(True)
