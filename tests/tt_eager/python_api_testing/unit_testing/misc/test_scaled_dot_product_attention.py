83307bb145fd8d566da266c2cf31cbadd1eea854
Anil Mahmud <amahmudTT@tenstorrent.com>
2024-09-26 15:32:20 +0000

    #12855: Solution verification branch

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_scaled_dot_product_attention.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_scaled_dot_product_attention.py
@@ -20,9 +20,7 @@ def is_watcher_enabled():
     return os.environ.get("TT_METAL_WATCHER") is not None
 
 
-def run_test_sdpa_tt_ND(
-    device, b, nh, nkv, s, d, q_chunk_size, k_chunk_size, dtype, buggy_tensor_1, buggy_tensor_2, buggy_tensor_3
-):
+def run_test_sdpa_tt_ND(device, b, nh, nkv, s, d, q_chunk_size, k_chunk_size, dtype):
     program_config = tt_lib.operations.primary.transformers.SDPAMultiCoreProgramConfig(
         # compute_with_storage_grid_size=device.compute_with_storage_grid_size(),
         compute_with_storage_grid_size=[1, 1],
@@ -60,23 +58,15 @@ def run_test_sdpa_tt_ND(
     )
 
     torch.set_printoptions(profile="full")
-    for idx in tqdm(range(1)):
+    for idx in tqdm(range(30)):
         tt_back = tt_lib.operations.primary.transformers.scaled_dot_product_attention(
             tt_Q, tt_K, tt_V, tt_attn_mask, is_causal=True, program_config=program_config
         )
         tt_back = tt_back.cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
         if torch.all(E.eq(tt_back)):
             print("Matched Expected")
-        elif torch.all(buggy_tensor_1.eq(tt_back)):
-            print("Matched Buggy 1")
-        elif torch.all(buggy_tensor_2.eq(tt_back)):
-            print("Matched Buggy 2")
-        elif torch.all(buggy_tensor_3.eq(tt_back)):
-            print("Matched Buggy 3")
         else:
-            print("Unknown Error")
-            print(tt_back)
-            torch.save(tt_back, "buggy_tensor_4.pt")
+            print("Error")
 
 
 @skip_for_grayskull("Unsupported in GS since L1 runs OOM with most configs")
@@ -94,22 +84,20 @@ def run_test_sdpa_tt_ND(
     ),
 )
 def test_sdpa_nd(device, b, nh, nkv, s, d, q_chunk_size, k_chunk_size, dtype):
-    torch.set_printoptions(profile="full")
-    buggy_tensor_1 = torch.load("buggy_tensor_1.pt")
-    buggy_tensor_2 = torch.load("buggy_tensor_2.pt")
-    buggy_tensor_3 = torch.load("buggy_tensor_3.pt")
-    print(buggy_tensor_1)
-    print(buggy_tensor_2)
-    print(buggy_tensor_3)
+    # torch.set_printoptions(profile="full")
+    # buggy_tensor_1 = torch.load("buggy_tensor_1.pt")
+    # buggy_tensor_2 = torch.load("buggy_tensor_2.pt")
+    # buggy_tensor_3 = torch.load("buggy_tensor_3.pt")
+    # print(buggy_tensor_1)
+    # print(buggy_tensor_2)
+    # print(buggy_tensor_3)
 
     if (s % q_chunk_size != 0) or (s % k_chunk_size != 0):
         pytest.skip("s must be divisible by q_chunk_size and k_chunk_size")
 
     # NOP 27-32 gives buggy tensor 1-3, 5 produces mixed errors.
-    os.environ["TT_NOP_INSERT"] = str(5)
     for idx in tqdm(range(100)):
+        os.environ["TT_NOP_INSERT"] = str(idx)
         tt_lib.device.DisablePersistentKernelCache()
-        run_test_sdpa_tt_ND(
-            device, b, nh, nkv, s, d, q_chunk_size, k_chunk_size, dtype, buggy_tensor_1, buggy_tensor_2, buggy_tensor_3
-        )
+        run_test_sdpa_tt_ND(device, b, nh, nkv, s, d, q_chunk_size, k_chunk_size, dtype)
         # assert device.num_program_cache_entries() == 1
