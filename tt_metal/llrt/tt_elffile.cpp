c6c32aff2c4f8fe93769492ea50c624a2ed3d3c9
Nathan Sidwell <nsidwell@tenstorrent.com>
2024-10-25 21:13:36 +0000

    #0: fix tt_elffile zero-length sections

--- tt_metal/llrt/tt_elffile.cpp
+++ tt_metal/llrt/tt_elffile.cpp
@@ -100,10 +100,12 @@ class ElfFile::Impl {
         // section is at the end of a segment, it is considered in
         // that segment. Fortunately, we do not have abutting
         // segments, so do not have to consider the case of a zero
-        // length section sitting at that boundary.
-        return shdr.sh_flags & SHF_ALLOC && shdr.sh_addr >= segment.address &&
-	    shdr.sh_addr + shdr.sh_size <=
-	    segment.address + (segment.contents.size() + segment.bss) * sizeof (word_t) + !shdr.sh_size;
+        // length section sitting at that boundary. We also take
+        // advantage of the (a) fact that sections cannot straddle
+        // segment boundaries -- they're either wholey inside or
+        // wholey outside, and (b) unsigned arithmetic.
+        if (shdr.sh_flags & SHF_ALLOC &&
+            sh.sh_addr + shdr.sh_size - segment.address <= (segment.contents.size() + segment.bss) * sizeof (word_t);
     }
     [[nodiscard]] bool IsInSegment(unsigned _ix, Elf32_Shdr const &shdr) const {
         return IsInSegment(GetSegments()[_ix], shdr);
